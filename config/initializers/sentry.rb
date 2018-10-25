d89718e1c76072aefc7d9c7d338273dda7dc9ccd
Kath Pobee-Norris <kpobeenorris@gmail.com>
2018-10-25 08:50:04 +0100

    Set up Sentry initialiser

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,8 @@
-Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+sentry_dsn = Rails.configuration.sentry_dsn
+if sentry_dsn
+  Raven.configure do |config|
+    config.dsn = sentry_dsn
+  end
+else
+  STDOUT.puts '[WARN] Sentry is not configured (SENTRY_DSN)'
 end
