a38ac39b991ba248e64bc20500a178ddd0c5a768
Toby Retallick <toby.retallick@gmail.com>
2018-11-28 14:16:21 +0000

    Create Nomis::Oauth::TokenService
    - It checks whether the 'active' Nomis::Oauth::Token is expired. If the
    token has expired it fetches a new token.
    - Add unit tests

---
+++ app/services/nomis/oauth/token_service.rb
@@ -0,0 +1,32 @@
+require 'byebug'
+module Nomis
+  module Oauth
+    class TokenService
+      include Singleton
+
+      class << self
+        delegate :valid_token, to: :instance
+      end
+
+      def valid_token
+        # byebug
+        set_new_token if token.expired?
+        token
+      end
+
+    private
+
+      def set_new_token
+        @token = fetch_token
+      end
+
+      def token
+        @token ||= fetch_token
+      end
+
+      def fetch_token
+        Nomis::Oauth::Api.fetch_new_auth_token
+      end
+    end
+  end
+end
