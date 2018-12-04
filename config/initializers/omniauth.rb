06dff66aabd86b9263e9438b87d20d6fbccbed00
Toby Retallick <toby.retallick@gmail.com>
2018-12-04 14:20:35 +0000

    Create initialiser for omniauth-oauth2 library

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,21 @@
+require 'hmpps_sso'
+
+sso_client_secret = Rails.configuration.nomis_oauth_client_secret
+sso_client_id = Rails.configuration.nomis_oauth_client_id
+sso_host = Rails.configuration.nomis_oauth_host
+
+unless sso_client_secret && sso_client_id && sso_host
+  STDOUT.puts '[WARN] NOMIS_OAUTH_CLIENT_ID, SECRET or HOST not configured'
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider(
+    :hmpps_sso,
+    sso_client_id,
+    sso_client_secret,
+    client_options: {
+      site: sso_host,
+      authorize_url: '/auth/oauth/authorize',
+      token_url: '/auth/oauth/token'
+    })
+end
