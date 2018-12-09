5250b6daa7a559c94b62423e01d3ec4d44e4b688
Toby Retallick <toby.retallick@gmail.com>
2018-12-09 20:27:06 +0000

    [MOIC-79] - Create Nomis::Oauth::ClientHelper

---
+++ app/services/nomis/oauth/client_helper.rb
@@ -0,0 +1,17 @@
+require 'base64'
+
+#:nocov:
+module Nomis
+  module Oauth
+    module ClientHelper
+      # rubocop:disable Metrics/LineLength
+      def authorisation
+        'Basic ' + Base64.urlsafe_encode64(
+          "#{Rails.configuration.nomis_oauth_client_id}:#{Rails.configuration.nomis_oauth_client_secret}"
+        )
+      end
+      # rubocop:enable Metrics/LineLength
+    end
+  end
+end
+#:nocov:
