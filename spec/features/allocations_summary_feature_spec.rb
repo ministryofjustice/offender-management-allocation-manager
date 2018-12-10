14bca3db92ecdbaafbb8d5191f1eba1b15810ece
Toby Retallick <toby.retallick@gmail.com>
2018-12-10 15:12:46 +0000

    [MOICM-67] - Get prisoner information by location
    - remove hard-coded data in the AllocationsController
    - temporarily adjust views to pull in Custody API response data
    - update spec helpers

--- spec/features/allocations_summary_feature_spec.rb
+++ spec/features/allocations_summary_feature_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 feature 'allcations summary feature' do
+  around do |example|
+    travel_to Date.new(2018, 11, 10, 15) do
+      example.run
+    end
+  end
+
   describe 'awaiting tiering' do
-    it 'displays offenders awaiting tiering' do
+    it 'displays offenders awaiting tiering', vcr: { cassette_name: :awaiting_tiering_feature } do
       signin_user
 
       visit 'allocations/#awaiting_tiering'
