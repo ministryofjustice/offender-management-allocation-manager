3c1159fa5fb9f12481dc63be81808d943e81230f
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-01-07 11:34:07 +0000

    Provides a partial for pagination
    Currently there is no way to navigate through pages of data that are
    returned from API calls that make lists, although it is possible to
    manually change the URL to replicate this.
    
    This PR adds a partial that can be added to templates to show the
    current paging state, and to navigate through the pages available (as
    long as the action provides a PageMeta object). This PR doesn't
    introduce any more dependencies as our needs are quite simple.
    
    It may be that at a later stage we will need to replace the links to
    ?page= with a helper that replaces the parameter, for instance if there
    is also likely to be a sort parameter.

---
+++ app/models/page_meta.rb
@@ -0,0 +1,40 @@
+class PageMeta
+  include MemoryModel
+
+  attribute :size, :integer
+  attribute :total_elements, :integer
+  attribute :total_pages, :integer
+  attribute :number, :integer
+  attribute :items_on_page, :integer
+
+  def record_range
+    return '0 - 0' if total_elements == 0
+
+    start = size * number + 1
+    "#{start} - #{start + items_on_page - 1}"
+  end
+
+  def current_page
+    number + 1
+  end
+
+  def page_numbers
+    page = current_page
+
+    return [] if total_pages == 0
+
+    return 1..[total_pages, 5].min if page <= 3
+
+    return total_pages - 5..total_pages if page >= total_pages - 5
+
+    page - 2..page + 2
+  end
+
+  def previous?
+    current_page > 1
+  end
+
+  def next?
+    current_page < total_pages
+  end
+end
