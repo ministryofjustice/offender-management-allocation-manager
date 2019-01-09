b2d7664a42268cc1904c7be44b17f1f5b2d5e1fe
Toby Retallick <toby.retallick@gmail.com>
2019-01-09 17:05:18 +0000

    Add configuration for exporting puma server metrics to prometheus_exporter

--- config/puma_prod.rb
+++ config/puma_prod.rb
@@ -8,3 +8,13 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'production'
+
+after_worker_boot do
+  require 'prometheus_exporter/instrumentation'
+  PrometheusExporter::Instrumentation::Puma.start
+end
+
+after_fork do
+  require 'prometheus_exporter/instrumentation'
+  PrometheusExporter::Instrumentation::process.start(type: 'web')
+end
