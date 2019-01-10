1714bdf4d31243dd8563cb20ea928ad51da989ff
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-01-10 16:15:29 +0000

    Fix the cassettes for the allocation page@

--- spec/support/helpers/features_helper.rb
+++ spec/support/helpers/features_helper.rb
@@ -2,7 +2,7 @@ module FeaturesHelper
   def signin_user(name = 'Fred')
     hmpps_sso_response = {
       'info' => double('user_info', username: name, caseload: 'LEI'),
-      'credentials' => double('credentials', expires_at: (Time.zone.now + 3600).to_i)
+      'credentials' => double('credentials', expires_at: Time.zone.local(2030, 1, 1).to_i)
     }
 
     OmniAuth.config.add_mock(:hmpps_sso, hmpps_sso_response)
