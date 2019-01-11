77cab4c8325daad67957a0a7d32eae8c3bb3799b
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-01-11 09:32:56 +0000

    Ugly debugging

--- spec/support/helpers/features_helper.rb
+++ spec/support/helpers/features_helper.rb
@@ -5,6 +5,10 @@ module FeaturesHelper
       'credentials' => double('credentials', expires_at: Time.zone.local(2030, 1, 1).to_i)
     }
 
+    p '---------------------------------------------'
+    p 'Mocking AUTH'
+    p '---------------------------------------------'
+
     OmniAuth.config.add_mock(:hmpps_sso, hmpps_sso_response)
   end
 end
