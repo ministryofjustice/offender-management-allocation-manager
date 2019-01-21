3efa4f8af1600d547e2f0deeed30e327c0e5b82e
Toby Retallick <toby.retallick@gmail.com>
2019-01-21 11:37:13 +0000

    Fetch multiple NDelius records from the fake server
    Return hash of fake NDelius records

--- app/services/ndelius/api.rb
+++ app/services/ndelius/api.rb
@@ -4,10 +4,17 @@ module Ndelius
 
     class << self
       delegate :get_record, to: :instance
+      delegate :get_records, to: :instance
     end
 
     def get_record(nomis_id)
       FakeRecord.generate(nomis_id)
+    end
+
+    def get_records(nomis_ids)
+      nomis_ids.each_with_object({}) do |id, hash|
+        hash[id] = FakeRecord.generate(id)
+      end
     end
   end
 end
