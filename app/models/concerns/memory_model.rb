60b3ae8ce70530318c623e183eae4e8aaa55bc79
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-01-22 11:04:19 +0000

    Handle embedded objects in API response
    Most of the values related to the hash are strings or integers, but
    occassionally we encounter an embedded object.  This object is currently
    stored as a string, and decoded from the JSON on first use.
    
    This PR changes how embedded objects are made available in the memory
    model.  If we remove the type from the attribute and add a serialize
    field like
    
    ```
    serialise :field, JSON
    ```
    
    the `:field` will be available as a hash (or list) based on the JSON
    attached to that key.

--- app/models/concerns/memory_model.rb
+++ app/models/concerns/memory_model.rb
@@ -1,3 +1,7 @@
+require 'active_record'
+require 'active_record/model_schema'
+require 'active_record/attribute_decorators'
+
 module MemoryModel
   extend ActiveSupport::Concern
 
@@ -5,5 +9,8 @@ module MemoryModel
     include ActiveModel::Model
     include ActiveModel::Attributes
     include ActiveModel::Validations::Callbacks
+    include ActiveRecord::AttributeDecorators
+    include ActiveRecord::ModelSchema
+    include ActiveRecord::AttributeMethods::Serialization
   end
 end
