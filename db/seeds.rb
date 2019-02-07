ca0245edab5b5c8d9ecd487b06ce473c5d3668ce
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-02-07 12:01:19 +0000

    Re-enable seeds using the AllocationService

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,51 @@
 # Jay Heal
 PrisonOffenderManager.find_or_create_by!(
-  nomis_staff_id: 485737,
+  nomis_staff_id: 485_737,
   status: 'active',
   working_pattern: 1
 )
 
 # Ross Jones
 PrisonOffenderManager.find_or_create_by!(
-  nomis_staff_id: 485752,
+  nomis_staff_id: 485_752,
   status: 'active',
   working_pattern: 0.5
 )
 
 # Dom Bull
 PrisonOffenderManager.find_or_create_by!(
-  nomis_staff_id: 485572,
+  nomis_staff_id: 485_572,
   status: 'active',
   working_pattern: 1
 )
 
 # Jenny Ducket
 PrisonOffenderManager.find_or_create_by!(
-  nomis_staff_id: 485636,
+  nomis_staff_id: 485_636,
   status: 'active',
   working_pattern: 0.5
 )
 
 # Toby Retallick
 PrisonOffenderManager.find_or_create_by!(
-  nomis_staff_id: 485595,
+  nomis_staff_id: 485_595,
   status: 'active',
   working_pattern: 1
 )
-#TODO reinstate when AllocationService created
-# AllocationService.create_allocation(
-#   {
-#     nomis_offender_id: 'G4273GI',
-#     nomis_booking_id: 1153753,
-#     prison: 'LEI',
-#     allocated_at_tier: 'C',
-#     created_by: 'user@username.com',
-#     nomis_staff_id: 485595
-#   })
-#
-# AllocationService.create_allocation(
-#   {
-#     nomis_offender_id: 'G4273GI',
-#     nomis_booking_id: 1153753,
-#     prison: 'LEI',
-#     allocated_at_tier: 'A',
-#     created_by: 'user@username.com',
-#     nomis_staff_id: 485595
-#   })
+AllocationService.create_allocation(
+  nomis_offender_id: 'G4273GI',
+  nomis_booking_id: 1_153_753,
+  prison: 'LEI',
+  allocated_at_tier: 'C',
+  created_by: 'user@username.com',
+  nomis_staff_id: 485_595
+  )
+
+AllocationService.create_allocation(
+  nomis_offender_id: 'G4273GI',
+  nomis_booking_id: 1_153_753,
+  prison: 'LEI',
+  allocated_at_tier: 'A',
+  created_by: 'user@username.com',
+  nomis_staff_id: 485_595
+  )
