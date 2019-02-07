687f29a472ca9587f35b127aecb9045e336eec45
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-02-07 12:04:37 +0000

    Integrate imported services and fix the tests

--- app/controllers/allocate_controller.rb
+++ app/controllers/allocate_controller.rb
@@ -2,13 +2,11 @@ class AllocateController < ApplicationController
   before_action :authenticate_user
 
   def show
-    response = OffenderService.new.get_offender(nomis_offender_id)
-    @prisoner = response.data
-
+    @prisoner = prisoner
     @recommended_pom = @prisoner.current_responsibility
 
-    pom_response = StaffService.new.get_prisoner_offender_managers(caseload)
-    @recommended_poms, @not_recommended_poms = pom_response.data.partition { |pom|
+    pom_response = StaffService.get_prisoner_offender_managers(caseload)
+    @recommended_poms, @not_recommended_poms = pom_response.partition { |pom|
       pom.position_description.include?(@recommended_pom)
     }
   end
@@ -16,12 +14,14 @@ class AllocateController < ApplicationController
   def new
     @prisoner = prisoner
 
-    pom_response = StaffService.new.get_prisoner_offender_managers(caseload)
-    @pom = pom_response.data.select { |p| p.staff_id == nomis_staff_id }.first
+    # TODO: This should not happen here, we should use an amalgamation
+    # of the StaffService and PrisonOffenderManagerService
+    poms_list = Nomis::Elite2::Api.prisoner_offender_manager_list(caseload)
+    @pom = poms_list.data.select { |p| p.staff_id == nomis_staff_id }.first
   end
 
   def create
-    AllocationService::Api.allocate(
+    AllocationService.create_allocation(
       nomis_staff_id: nomis_staff_id.to_i,
       nomis_offender_id: nomis_offender_id,
       created_by: current_user,
@@ -29,14 +29,14 @@ class AllocateController < ApplicationController
       allocated_at_tier: prisoner.tier,
       prison: caseload
     )
+
     redirect_to allocations_path
   end
 
 private
 
   def prisoner
-    response = OffenderService.new.get_offender(nomis_offender_id)
-    @prisoner = response.data
+    OffenderService.new.get_offender(nomis_offender_id).data
   end
 
   def nomis_offender_id
