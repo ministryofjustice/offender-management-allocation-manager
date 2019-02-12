3e2e010f8a9201dbe7f7939abd268a16245e686b
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-02-12 14:23:19 +0000

    Formatting fix

--- spec/services/allocation_summary_service_spec.rb
+++ spec/services/allocation_summary_service_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 describe AllocationSummaryService do
-  it "will generate a summary", vcr: {cassette_name: :allocation_summary_service_summary} do
+  it "will generate a summary", vcr: { cassette_name: :allocation_summary_service_summary } do
     summary = described_class.summary(1, 48, 15, 'LEI')
 
     expect(summary.unallocated_page_count).to eq(48)
     expect(summary.missing_page_count).to eq(15)
   end
-
 end
