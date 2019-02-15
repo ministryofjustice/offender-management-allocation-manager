227c0d219c5fcd25265502331bcba64b90700c76
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-02-15 08:15:10 +0000

    Fix migrations
    update_overrides_column_type was trying to rename a field in the
    allocations table that was already renamed in
    
    ```
    db/migrate/20190210132134_rename_column_allocations_table.rb
      3,33:     rename_column :allocations, :reason, :override_reasons
      9,52:     rename_column :allocations, :override_reasons, :reason
    ```

--- db/migrate/20190210105233_add_overrides_table.rb
+++ db/migrate/20190210105233_add_overrides_table.rb
@@ -2,7 +2,7 @@ class AddOverridesTable < ActiveRecord::Migration[5.2]
   create_table "overrides", force: :cascade do |t|
     t.integer "nomis_staff_id"
     t.string "nomis_offender_id"
-    t.string "override_reasons"
+    t.string "override_reason"
     t.string "more_detail"
   end
 end
