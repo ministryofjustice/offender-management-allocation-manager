b413158fdcdace8685119154938fe79e0369e955
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-02-18 13:37:47 +0000

    Show validation errors when creating case information
    When creating a new case information this PR will perform validation. If
    validation fails then the :new action if performed and validation errors
    are shown on the form.
    
    Unfortunately because the :create call goes to /case_information/ it is
    not possible to reload that form (it will instead raise a 404 because it
    is looking for :index).
    
    The messages in the error summary at the top of the page cannot link to
    the relevant part of the page as a new request is made which fails with
    a 404 because of the issue described above.

--- spec/features/case_information_feature_spec.rb
+++ spec/features/case_information_feature_spec.rb
@@ -29,4 +29,67 @@ feature 'case information feature' do
       expect(page).to have_css('.offender_row_0', count: 1)
     end
   end
+
+  it 'complains if allocation data is missing', :raven_intercept_exception, vcr: { cassette_name: :case_information_missing_case_feature } do
+    nomis_offender_id = 'G4273GI'
+
+    signin_user
+    visit summary_path
+
+    within('#awaiting-information') do
+      within('.offender_row_0') do
+        click_link 'Edit'
+      end
+    end
+
+    expect(page).to have_current_path new_case_information_path(nomis_offender_id)
+
+    choose('case_information_tier_A')
+    click_button 'Save'
+
+    expect(CaseInformation.count).to eq(0)
+    expect(page).to have_content("Case allocation must be provided")
+  end
+
+  it 'complains if all data is missing', :raven_intercept_exception, vcr: { cassette_name: :case_information_missing_all_feature } do
+    nomis_offender_id = 'G4273GI'
+
+    signin_user
+    visit summary_path
+
+    within('#awaiting-information') do
+      within('.offender_row_0') do
+        click_link 'Edit'
+      end
+    end
+
+    expect(page).to have_current_path new_case_information_path(nomis_offender_id)
+
+    click_button 'Save'
+
+    expect(CaseInformation.count).to eq(0)
+    expect(page).to have_content("Case allocation must be provided")
+    expect(page).to have_content("Tier must be provided")
+  end
+
+  it 'complains if tier data is missing', :raven_intercept_exception, vcr: { cassette_name: :case_information_missing_tier_feature } do
+    nomis_offender_id = 'G4273GI'
+
+    signin_user
+    visit summary_path
+
+    within('#awaiting-information') do
+      within('.offender_row_0') do
+        click_link 'Edit'
+      end
+    end
+
+    expect(page).to have_current_path new_case_information_path(nomis_offender_id)
+
+    choose('case_information_case_allocation_NPS')
+    click_button 'Save'
+
+    expect(CaseInformation.count).to eq(0)
+    expect(page).to have_content("Tier must be provided")
+  end
 end
