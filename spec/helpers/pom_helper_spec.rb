a8c4c4d8392ffd124b6c3e812124ef41fc05bdbc
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-02-19 09:19:27 +0000

    Add helper to display POM working pattern as a formatted string

---
+++ spec/helpers/pom_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe PomHelper do
+  describe 'format_working_pattern' do
+    it "formats a POM's working pattern" do
+      expect(format_working_pattern('1.0')).to eq('Full time')
+    end
+  end
+end
