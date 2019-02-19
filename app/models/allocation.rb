5aa27d84e620bde45f195557e92c871b50ddc080
Toby Retallick <toby.retallick@gmail.com>
2019-02-19 11:47:15 +0000

    Add POM grade

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -1,6 +1,6 @@
 class Allocation < ApplicationRecord
   belongs_to :pom_detail
 
-  validates :nomis_offender_id, :nomis_booking_id, :prison, :allocated_at_tier,
+  validates :nomis_offender_id, :nomis_staff_id, :nomis_booking_id, :prison, :allocated_at_tier,
     :created_by, presence: true
 end
