df6b219a8297aad414695a755f3b18d85df9df83
Toby Retallick <toby.retallick@gmail.com>
2019-02-19 08:46:33 +0000

    Refactor 'config/routes.rb'
    - Convert more routes to 'resources'

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,22 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
 
-  get '/status', to: 'status#index'
-  get('/health' => 'health#index')
-
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
-
   get('/summary' => 'summary#index')
   get('/prisoners/:id' => 'prisoners#show', as: 'prisoners_show')
   get('/allocations/confirm/:nomis_offender_id/:nomis_staff_id' => 'allocations#confirm', as: 'confirm_allocations')
   get('/poms/:nomis_staff_id/my_caseload' => 'poms#my_caseload', as: 'my_caseload')
   get('/poms/:nomis_staff_id/new_cases' => 'poms#new_cases', as: 'new_cases')
-
+  
+  resources :health, only: %i[ index ], controller: 'health'
+  resources :status, only: %i[ index ], controller: 'status'
+  resources :poms, only: %i[ index show edit ], param: :nomis_staff_id
+  resource :overrides,  only: %i[ new create ], path_names: { new: 'new/:nomis_offender_id/:nomis_staff_id'}
   resource :allocations, only: %i[ new create ], path_names: {
     new: 'new/:nomis_offender_id',
     confirm: 'confirm/:nomis_offender_id/:nomis_staff_id'
   }
-  resources :poms, only: %i[ index show edit ], param: :nomis_staff_id
-  resource :summary, only: %i[ index ], controller: 'summary'
-  resource :overrides,  only: %i[ new create ], path_names: { new: 'new/:nomis_offender_id/:nomis_staff_id'}
   resource :case_information, only: %i[new create edit update], controller: 'case_information', path_names: {
     new: 'new/:nomis_offender_id',
     edit: 'edit/:nomis_offender_id'
