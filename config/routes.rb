0194923c50d7d271fe333a9b557c45f71304fa44
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-02-19 09:18:37 +0000

    Update routes to enable editing a POM

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,19 @@ Rails.application.routes.draw do
   get('/summary' => 'summary#index')
   get('/prisoners/:id' => 'prisoners#show', as: 'prisoners_show')
   get('/allocations/confirm/:nomis_offender_id/:nomis_staff_id' => 'allocations#confirm', as: 'confirm_allocations')
+  put('/poms/:nomis_staff_id' => 'poms#update', as: 'poms_update')
   get('/poms/:nomis_staff_id/my_caseload' => 'poms#my_caseload', as: 'my_caseload')
   get('/poms/:nomis_staff_id/new_cases' => 'poms#new_cases', as: 'new_cases')
 
   resource :allocations, only: %i[ new create ], path_names: {
-    new: 'new/:nomis_offender_id',
-    confirm: 'confirm/:nomis_offender_id/:nomis_staff_id'
+      new: 'new/:nomis_offender_id',
+      confirm: 'confirm/:nomis_offender_id/:nomis_staff_id'
   }
   resources :poms, only: %i[ index show edit ], param: :nomis_staff_id
   resource :summary, only: %i[ index ], controller: 'summary'
   resource :overrides,  only: %i[ new create ], path_names: { new: 'new/:nomis_offender_id/:nomis_staff_id'}
   resource :case_information, only: %i[new create edit update], controller: 'case_information', path_names: {
-    new: 'new/:nomis_offender_id',
-    edit: 'edit/:nomis_offender_id'
+      new: 'new/:nomis_offender_id',
+      edit: 'edit/:nomis_offender_id'
   }
 end
