76821d9167c0e3785739298f866186f5d4dc6375
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-02-21 15:08:44 +0000

    Fix the GA bug that stops form submission
    The gtag function defined on the page, isn't available in the included
    json so we will temporarily just include the block in the page.

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -9,18 +9,3 @@ function createFunctionWithTimeout(callback, opt_timeout) {
     setTimeout(fn, opt_timeout || 1000);
     return fn;
   }
-
-function analytics_form_event(form_id, event_name, event_action) {
-    var form = document.getElementById(form_id);
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      gtag('send', 'event', event_name, event_action, {
-        hitCallback: createFunctionWithTimeout(function() {
-          form.submit();
-        })
-      });
-    });
-}
-
-
-
