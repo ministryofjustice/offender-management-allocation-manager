b8265c32da426df6bc53c81476a199bc3e762bf3
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-02-21 10:48:46 +0000

    Simplify the movement service
    Simplifies the code to do the select inline as we don't need to perform
    the select once for each filter, we can just check whether the current
    field is a member of the list of filters.

--- app/services/movement_service.rb
+++ app/services/movement_service.rb
@@ -4,30 +4,18 @@ class MovementService
     movements = Nomis::Elite2::Api.movements_on_date(date)
 
     if direction_filters.any?
-      movements = filter_movements(movements, direction_filters, proc { |m|
+      movements = movements.select { |m|
         direction_filters.include?(m.direction_code)
-      })
+      }
     end
 
     if type_filters.any?
-      movements = filter_movements(movements, type_filters, proc { |m|
+      movements = movements.select { |m|
         type_filters.include?(m.movement_type)
-      })
+      }
     end
 
     movements
   end
-# rubocop:enable Metrics/MethodLength
-
-private
-
-  def self.filter_movements(movements, filters, filter_func)
-    return movements if filters.empty?
-
-    filters.each do
-      movements = movements.select(&filter_func)
-    end
-
-    movements
-  end
+  # rubocop:enable Metrics/MethodLength
 end
