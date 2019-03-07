09cae4aae1e4ca31920fa89695401af5cb55e5fc
Toby Retallick <toby.retallick@gmail.com>
2019-03-07 14:25:19 +0000

    Remove Elite2::UserApi and rename Custody::StaffApi to Custody::UserApi

--- app/services/nomis/custody/user_api.rb
+++ app/services/nomis/custody/user_api.rb
@@ -9,6 +9,13 @@ module Nomis
         response = custody_client.get(route)
         response['nomisCaseloads'].keys
       end
+
+      def self.user_details(username)
+        route = "/custodyapi/api/nomis-staff-users/#{username}"
+        response = custody_client.get(route)
+
+        api_deserialiser.deserialise(Nomis::Models::UserDetails, response)
+      end
     end
   end
 end
