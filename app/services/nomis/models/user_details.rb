f45303f6d27ee414f1eb290a26f49e6e7a1a64f1
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-03-08 13:11:32 +0000

    Remove type specification from 'emails'

--- app/services/nomis/models/user_details.rb
+++ app/services/nomis/models/user_details.rb
@@ -13,7 +13,7 @@ module Nomis
       attribute :status, :string
       attribute :thumbnail_id, :string
       attribute :username, :string
-      attribute :emails, :string
+      attribute :emails
     end
   end
 end
