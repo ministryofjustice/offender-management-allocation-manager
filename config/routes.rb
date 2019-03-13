3e3c463c64f9cfca0862e65a40298986f9a1c857
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-03-13 11:08:40 +0000

    Add sidekiq web dashboard into routes

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,7 @@ Rails.application.routes.draw do
       new: 'new/:nomis_offender_id',
       edit: 'edit/:nomis_offender_id'
   }
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 end
