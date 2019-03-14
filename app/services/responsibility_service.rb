38f7bd11fe7a917e458525b1090ed202d41f23df
Toby Retallick <toby.retallick@gmail.com>
2019-03-14 12:50:45 +0000

    Calculate POM responsibility
    - Rename 'calculate_responsibility' class method to 'calculate_case_owner'

--- app/services/responsibility_service.rb
+++ app/services/responsibility_service.rb
@@ -1,5 +1,5 @@
 class ResponsibilityService
-  def self.calculate_responsibility(offender)
+  def self.calculate_case_owner(offender)
     case offender.case_allocation
     when 'NPS'
       nps_calculation(offender)
@@ -10,9 +10,19 @@ class ResponsibilityService
     end
   end
 
+  def self.calculate_pom_responsibility(offender)
+    return 'Unknown' if offender.release_date.nil?
+    return 'Responsible' if offender.welsh_address == true && release_date_gt_10_months(offender)
+    'Supporting'
+  end
+
   def self.nps_calculation(offender)
     return 'No release date' if offender.release_date.nil?
 
     offender.tier == 'A' || offender.tier == 'B' ? 'Probation' : 'Prison'
+  end
+
+  def self.release_date_gt_10_months(offender)
+    offender.release_date > DateTime.now.utc.to_date + 10.months
   end
 end
