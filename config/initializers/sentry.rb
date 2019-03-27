154b3e66ca3338ec14107fc67474e57c3be8abff
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-03-27 10:24:16 +0000

    Ignore JWT::ExpiredSignature when reporting to Sentry
    We get a lot of ExpiredSignature messages, and they're not really
    exceptions that we can do much about, so let's ignore them.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,7 @@ sentry_dsn = Rails.configuration.sentry_dsn
 if sentry_dsn
   Raven.configure do |config|
     config.dsn = sentry_dsn
+    config.excluded_exceptions << 'JWT::ExpiredSignature'
   end
 else
   Rails.logger.warn '[WARN] Sentry is not configured (SENTRY_DSN)'
