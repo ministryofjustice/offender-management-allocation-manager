5340ef045ae121996f9505b46eea8c88a10bad2d
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-04-05 14:19:03 +0100

    Refactor offender models
    We currently have two offender memory models as we have to call two
    endpoints from the API to fetch all the data we need for an offender.
    There are also a number of attributes that we don't use and are storing
    needlessly.  This commit removes the deserialiser's logging warning for
    missed attributes and removes those attributes from both offender
    models.

--- app/services/nomis/models/offender_summary.rb
+++ app/services/nomis/models/offender_summary.rb
@@ -5,40 +5,29 @@ module Nomis
     class OffenderSummary
       include MemoryModel
 
-      attribute :offender_no, :string
-      attribute :noms_id, :string
-      attribute :title, :string
-      attribute :first_name, :string
-      attribute :middle_names, :string
-      attribute :last_name, :string
-      attribute :date_of_birth, :date
-      attribute :gender, :string
-      attribute :sex_code, :string
-      attribute :nationalities, :string
-      attribute :currently_in_prison, :string
-      attribute :latest_booking_id, :integer
-      attribute :latest_location_id, :string
-      attribute :latest_location, :string
-      attribute :internal_location, :string
-      attribute :pnc_number, :string
-      attribute :cro_number, :string
-      attribute :ethnicity, :string
-      attribute :birth_country, :string
-      attribute :religion, :string
       attribute :convicted_status, :string
+      attribute :date_of_birth, :date
+      attribute :first_name, :string
+      attribute :gender, :string
       attribute :imprisonment_status, :string
-      attribute :reception_date, :date
-      attribute :marital_status, :string
+      attribute :last_name, :string
+      attribute :latest_booking_id, :integer
       attribute :main_offence, :string
-      attribute :tier, :string
-      attribute :case_allocation, :string
-      attribute :omicable, :boolean
+      attribute :nationalities, :string
+      attribute :noms_id, :string
+      attribute :offender_no, :string
+      attribute :reception_date, :date
+
+      # custom attributes
       attribute :allocated_pom_name, :string
+      attribute :case_allocation, :string
+      attribute :home_detention_curfew_eligibility_date, :date
+      attribute :omicable, :boolean
+      attribute :parole_eligibility_date, :date
       attribute :release_date, :date
       attribute :sentence_start_date, :date
-      attribute :parole_eligibility_date, :date
-      attribute :home_detention_curfew_eligibility_date, :date
       attribute :tariff_date, :date
+      attribute :tier, :string
 
       def sentence_detail=(sentence_detail)
         self.release_date = sentence_detail.release_date
