178b41c14ec755819431a1d096476646692a8d5b
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-04-29 11:31:13 +0100

    Shows the default sort indicator
    WHen visiting one of the summary pages, if no sort order is specified it
    defaults to ordering by last name.  This was not showing an indicator to
    show that it was sorting by last name, now it does.
    
    At the same time the helper has been re-factored to make it simpler and
    to avoid too much parsing of the current URL.

--- app/helpers/sort_helper.rb
+++ app/helpers/sort_helper.rb
@@ -1,45 +1,80 @@
 # frozen_string_literal: true
 
 module SortHelper
-  # rubocop:disable Naming/AccessorMethodName
-  def set_search_param(field_name)
-    uri = URI.parse(request.original_url)
+  class Sorter
+    include ActionView::Helpers::TagHelper
 
-    query = Rack::Utils.parse_query(uri.query)
-    current_sort = query['sort']
+    DEFAULT_SORT = 'last_name'
 
-    if current_sort.present? && current_sort.start_with?(field_name)
-      # Check if name is already in query, and invert the search if it is
-      query['sort'] = "#{field_name} #{invert_sort_parameter(current_sort)}"
-    else
-      # If it isn't, choose the appropriate default
-      query['sort'] = "#{field_name} asc"
+    def initialize(url)
+      @uri = URI.parse(url)
+
+      @query = Rack::Utils.parse_query(@uri.query)
+      @current_sort_field = @query['sort']
     end
 
-    uri.query = Rack::Utils.build_query(query)
-    uri.to_s
-  end
-  # rubocop:enable Naming/AccessorMethodName
+    def link_for_field(field_name)
+      if @current_sort_field.present? && @current_sort_field.start_with?(field_name)
+        # Check if name is already in query, and invert the search if it is
+        @query['sort'] = "#{field_name} #{invert_sort_parameter(@current_sort_field)}"
+      else
+        # If it isn't, choose the appropriate default
+        @query['sort'] = "#{field_name} #{default_direction(field_name)}"
+      end
 
-  def display_arrow(field_name)
-    uri = URI.parse(request.original_url)
+      @uri.query = Rack::Utils.build_query(@query)
+      @uri.to_s
+    end
 
-    query = Rack::Utils.parse_query(uri.query)
-    current_sort = query['sort']
-    return '' unless current_sort.present? && current_sort.start_with?(field_name)
+    def arrow_for_field(field_name)
+      if field_name == DEFAULT_SORT && @current_sort_field.blank?
+        return asc_tag
+      end
 
-    if current_sort.end_with?('asc')
+      return '' if @current_sort_field.blank?
+      return '' unless @current_sort_field.start_with?(field_name)
+
+      if @current_sort_field.end_with?('asc')
+        asc_tag
+      else
+        desc_tag
+      end
+    end
+
+  private
+
+    def default_direction(field_name)
+      return 'desc' if @current_sort_field.blank? && field_name == DEFAULT_SORT
+
+      'asc'
+    end
+
+    def asc_tag
       content_tag(:span, '&#9650'.html_safe, class: 'sort-arrow')
-    else
+    end
+
+    def desc_tag
       content_tag(:span, '&#9660'.html_safe, class: 'sort-arrow')
     end
+
+    def invert_sort_parameter(param)
+      return 'desc' if param.end_with?('asc')
+
+      'asc'
+    end
+  end
+
+  def sort_link(field_name)
+    sorter.link_for_field(field_name)
+  end
+
+  def sort_arrow(field_name)
+    sorter.arrow_for_field(field_name)
   end
 
 private
 
-  def invert_sort_parameter(param)
-    return 'desc' if param.end_with?('asc')
-
-    'asc'
+  def sorter
+    @sorter ||= Sorter.new(request.original_url)
   end
 end
