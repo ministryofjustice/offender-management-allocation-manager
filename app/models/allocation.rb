a0695cf39993999921f02705b23f82a33ae98e5b
Toby Retallick <toby.retallick@gmail.com>
2019-04-29 09:35:54 +0100

    Move database calls to scopes

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: true
 
 class Allocation < ApplicationRecord
-  belongs_to :pom_detail
-
   attr_accessor :responsibility
 
+  scope :active_allocations, lambda { |nomis_offender_ids|
+    where(nomis_offender_id: nomis_offender_ids)
+  }
+  scope :primary_poms, lambda { |nomis_staff_id|
+    where(primary_pom_nomis_id: nomis_staff_id, active: true)
+  }
+  scope :secondary_poms, lambda { |nomis_staff_id|
+    where(secondary_pom_nomis_id: nomis_staff_id)
+  }
+  scope :any_pom, lambda { |nomis_staff_id|
+    where(primary_poms(nomis_staff_id)).or(secondary_poms(nomis_staff_id))
+  }
+  scope :primary_pom_nomis_id, lambda { |nomis_offender_id|
+    active_allocations(nomis_offender_id).first.primary_pom_nomis_id
+  }
+
   validates :nomis_offender_id,
-    :nomis_staff_id,
+    :primary_pom_nomis_id,
     :nomis_booking_id,
     :prison,
     :allocated_at_tier,
