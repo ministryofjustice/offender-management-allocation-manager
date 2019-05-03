e25f1eaa1aadee4e5bb99fe8cb51e70d2d072c6c
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-05-03 13:03:59 +0100

    Update 'new_nomis_host' ENV VAR name
    New Nomis has been renamed to 'Digital Prison Service' and so this PR
    just updates anywhere that refers to New Nomis.

--- app/helpers/offender_helper.rb
+++ app/helpers/offender_helper.rb
@@ -1,5 +1,6 @@
 module OffenderHelper
-  def new_nomis_profile_path(offender_id)
-    "#{Rails.configuration.new_nomis_host}/offenders/#{offender_id}/quick-look"
+  def digital_prison_service_profile_path(offender_id)
+    Rails.configuration.digital_prison_service_host +
+        "/offenders/#{offender_id}/quick-look"
   end
 end
