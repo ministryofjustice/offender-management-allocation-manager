c3afe94e954d3c17611de8566227cda2aa796246
Toby Retallick <toby.retallick@gmail.com>
2019-05-08 21:28:45 +0100

    Remove byebug import

--- app/services/prison_offender_manager_service.rb
+++ app/services/prison_offender_manager_service.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'byebug'
-
 class PrisonOffenderManagerService
   def self.get_pom_detail(nomis_staff_id)
     PomDetail.find_or_create_by!(nomis_staff_id: nomis_staff_id.to_i) { |s|
