ab181c008bc43a065529adcc210e87bedf1ad6b2
Toby Retallick <toby.retallick@gmail.com>
2019-05-20 15:07:49 +0100

    Create Rake task to migrate Allocation 'created_at' dates to thier corresponding AllocationVersion record

---
+++ lib/tasks/date_migrater.rake
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rake'
+require_relative '../../app/services/date_migrater.rb'
+
+namespace :date_migrater do
+  desc "Migrates Allocation 'created_at' values to corresponding AllocationVersion records"
+  task run: :environment do
+    if defined?(Rails) && Rails.env.development?
+      Rails.logger = Logger.new(STDOUT)
+    end
+
+    DateMigrater.run
+  end
+end
---
+++ spec/services/date_migrater_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+require 'byebug'
+
+describe DateMigrater do
+  it "migrates 'created_at' dates from Allocations to AllocationVersions" do
+    nomis_offender_id = 'ABD1234'
+    another_nomis_offender_id = 'ZZZ1234'
+    created_at_date = DateTime.new(2019, 05, 03).utc
+    primary_pom_allocated_at = DateTime.new(2019, 05, 19).utc
+
+    create(
+      :allocation,
+      nomis_offender_id: nomis_offender_id,
+      created_at: created_at_date,
+      active: true
+    )
+
+    create(
+      :allocation,
+      nomis_offender_id: another_nomis_offender_id,
+      created_at: created_at_date,
+      active: false
+    )
+
+    create(
+      :allocation,
+      nomis_offender_id: 'No corresponding AllocationVersion',
+      created_at: created_at_date,
+      active: true
+    )
+
+    create(
+      :allocation_version,
+      nomis_offender_id: nomis_offender_id
+    )
+
+    create(
+      :allocation_version,
+      nomis_offender_id: another_nomis_offender_id,
+      primary_pom_allocated_at: primary_pom_allocated_at
+    )
+
+    described_class.run
+
+    changed_allocation_version = AllocationVersion.find_by(nomis_offender_id: nomis_offender_id)
+    unchanged_allocation_version = AllocationVersion.find_by(nomis_offender_id: another_nomis_offender_id)
+
+    # Only active Allocations should be migrated
+    # Only Allocations with a corresponding AllocationVersion should be migrated
+
+    expect(changed_allocation_version.primary_pom_allocated_at).to eq(created_at_date)
+    expect(unchanged_allocation_version.primary_pom_allocated_at).to eq(primary_pom_allocated_at)
+  end
+end
