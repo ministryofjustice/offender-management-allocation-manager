874e15c4e77dda2df8a1b09615cb2137c6d1528f
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-06-05 16:08:44 +0100

    Appease the linter!

--- lib/onboard_prison.rb
+++ lib/onboard_prison.rb
@@ -6,9 +6,12 @@ class OnboardPrison
     @delius_records = delius_records
   end
 
-  private
+private
+
+  # rubocop:disable Metrics/LineLength
   def filter_existing_records(offender_ids)
     existing = CaseInformation.where(nomis_offender_id: offender_ids).pluck(:nomis_offender_id)
     offender_ids - existing
   end
+  # rubocop:enable Metrics/LineLength
 end
--- spec/lib/onboard_prison_spec.rb
+++ spec/lib/onboard_prison_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 require 'onboard_prison.rb'
 
 describe OnboardPrison do
-  context  'can de;termine which offenders need a CaseInformation record' do
-    let!(:offender_ids)  { [ 'G9468UN', 'G5054VN', 'G1895GH' ] }
+  context 'when we need to find out which offenders need a CaseInformation record' do
+    let!(:offender_ids)  { %w[G9468UN G5054VN G1895GH] }
 
     it 'returns a list of offenders that are missing two records' do
       CaseInformation.find_or_create_by!(
-          nomis_offender_id: 'G5054VN',
-          tier: 'A',
-          case_allocation: 'NPS',
-          omicable: 'Yes',
-          prison: 'PVI'
+        nomis_offender_id: 'G5054VN',
+        tier: 'A',
+        case_allocation: 'NPS',
+        omicable: 'Yes',
+        prison: 'PVI'
       )
       CaseInformation.find_or_create_by!(
-          nomis_offender_id: 'G9468UN',
-          tier: 'C',
-          case_allocation: 'CRC',
-          omicable: 'Yes',
-          prison: 'PVI'
+        nomis_offender_id: 'G9468UN',
+        tier: 'C',
+        case_allocation: 'CRC',
+        omicable: 'Yes',
+        prison: 'PVI'
       )
 
       op = described_class.new(offender_ids, nil)
