59f5d33d7d845a0e0b6ff778562660d73c8e04f3
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-06-07 11:14:38 +0100

    Loads data from CSV rather than XLSX
    The previous attempt at extracting the delius data from an XLSX file,
    whilst functional, used excessive amounts of RAM which exceeded that
    available in the production environment. This was the case with 2 of the
    most popular libraries for reading XLSX in Ruby, most likely as a direct
    result of parsing 10s of Mb of XML.
    
    This PR instead switches to reading the delius data from a CSV file with
    the same structure.  The results are much faster execution, and lower
    memory requirements, although still high because of the need to include
    the Rails environment in the ETL task.
    
    In addition to this change, we're now also reducing the data we hold in
    the delius lookup structure and we might find further savings by
    inverting the processing, so iterating through every line of the CSV and
    instead looking up whether it matches an offender.

--- lib/delius/extractor.rb
+++ lib/delius/extractor.rb
@@ -1,4 +1,4 @@
-require 'roo'
+# frozen_string_literal: true
 
 module Delius
   class Extractor
@@ -14,64 +14,49 @@ module Delius
       @errors = []
     end
 
+    # rubocop:disable Metrics/CyclomaticComplexity
     # rubocop:disable Metrics/MethodLength
     def fetch_records
-      xlsx = Roo::Spreadsheet.open(@filename)
       records = []
 
       validated_header = false
-      xlsx.each_row_streaming(pad_cells: true) do |row|
+      CSV.foreach(@filename, skip_blanks: true) do |row|
         unless validated_header
           validated_header = validate_header(row)
           next
         end
 
+        next if row.count == 0 || row[2].blank?
+
         record = {
-          'timestamp' => Time.zone.now.getutc
+          noms_no: row[2],
+          tier: row[4].present? ? row[4][0] : '',
+          provider_cd: row[10][0] == 'C' ? 'CRC' : 'NPS',
+          omicable: row[12].start_with?('WPT')
         }
-
-        next if row.count == 0
-
-        row.each_with_index do |cell, i|
-          next if cell.nil?
-
-          k = MAPPED_CELLS[i]
-          v = cell.value.to_s.strip
-          record[k.to_sym] = v
-        end
 
         records << record if clean_record(record)
       end
       records
     end
+  # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/MethodLength
 
   private
 
     def validate_header(row)
-      row.map { |c| c.value.strip } == HEADER_CELLS
+      row.map(&:strip) == HEADER_CELLS
     end
 
-    # rubocop:disable Metrics/MethodLength
     def clean_record(record)
-      if record[:tier].nil?
+      if record[:tier].blank?
         # Fill in @errors with a proper error
         @errors << 'Bad record passed, missing tier'
         return false
       end
 
-      required_fields = [:noms_no, :tier, :provider_cd, :ldu_cd]
-      record.keys.each do |key|
-        record.delete(key) unless required_fields.include?(key)
-      end
-
-      record[:tier] = record[:tier][0]
-      record[:provider_cd] = record[:provider_cd] == 'C' ? 'CRC' : 'NPS'
-      record[:omicable] = record[:ldu_cd].start_with?('WPT')
-
       true
     end
-    # rubocop:enable Metrics/MethodLength
 
     def values_for_row(row, escape_func)
       MAPPED_CELLS.each_with_object([]) do |cell_label, lst|
