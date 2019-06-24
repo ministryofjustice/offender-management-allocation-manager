538769db6ec3a0bd8cdc74446c2b25d23817797a
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-06-24 15:57:05 +0100

    Additional spec for prisoner profile page
    One of the new flows is that a POM can also view the prisoner profile
    page for any of their allocated offenders.  An additional feature spec
    has been added to test this flow is possible from the POM caseload
    pages.
    
    I've also updated the caseload new & index views to make use of the
    'each_with_index' method so we can more appropriately, and easily, track
    down specific rows within the tables displayed on these pages.

--- spec/features/pom_caseload_spec.rb
+++ spec/features/pom_caseload_spec.rb
@@ -22,6 +22,25 @@ feature "view POM's caseload" do
     expect(page).to have_content("Abbella, Ozullirn")
   end
 
+  it 'allows a POM to view the prisoner profile page for a specific offender',  vcr: { cassette_name: :show_poms_caseload_prisoner_profile } do
+    signin_user('PK000223')
+
+    visit confirm_allocation_path(nomis_offender_id, nomis_staff_id)
+
+    click_button 'Complete allocation'
+
+    visit caseload_index_path
+
+    within('.offender_row_0') do
+      click_link 'View'
+    end
+
+    expect(page).to have_css('h2', text: 'Abbella, Ozullirn')
+    expect(page).to have_content('15/08/1980')
+    cat_code = find('h3#category-code').text
+    expect(cat_code).to eq('C')
+  end
+
   it 'displays all cases that have been allocated to a specific POM in the last week', vcr: { cassette_name: :show_new_cases } do
     signin_user('PK000223')
 
