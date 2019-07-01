2d907e01d36fb0215772f5307193782c513c5c13
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-07-01 07:22:07 +0100

    Remove duplicated code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  include ActiveModel::Validations
-
-  validates :more_detail, presence: true
-
   def help; end
 
   def contact
@@ -13,7 +9,6 @@ class PagesController < ApplicationController
 
   def create_contact
     @contact = Contact.new(contact_params[:more_detail])
-    @contact.valid?
 
     if @contact.valid?
       redirect_to contact_path
