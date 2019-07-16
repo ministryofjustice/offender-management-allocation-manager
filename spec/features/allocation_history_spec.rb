7bfeada33692fced08ea12fd7485c5297b960e7d
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-07-16 11:24:04 +0100

    secondary allocations on history page

--- spec/features/allocation_history_spec.rb
+++ spec/features/allocation_history_spec.rb
@@ -45,58 +45,68 @@ feature 'Allocation History' do
       suitability_detail: "Too high risk",
       created_at: Time.zone.now - 10.days,
       updated_at: Time.zone.now - 10.days,
-      primary_pom_allocated_at: Time.zone.now - 10.days
+      primary_pom_allocated_at: Time.zone.now - 12.days
     )
 
     allocation = AllocationVersion.find_by(nomis_offender_id: nomis_offender_id)
 
-    allocation.update(event: AllocationVersion::REALLOCATE_PRIMARY_POM,
-                      primary_pom_nomis_id: probation_pom_2[:primary_pom_nomis_id],
-                      primary_pom_name: probation_pom_2[:primary_pom_name],
-                      recommended_pom_type: 'probation',
-                      updated_at: Time.zone.now - 8.days
+    allocation.update!(event: AllocationVersion::REALLOCATE_PRIMARY_POM,
+                       primary_pom_nomis_id: probation_pom_2[:primary_pom_nomis_id],
+                       primary_pom_name: probation_pom_2[:primary_pom_name],
+                       recommended_pom_type: 'probation',
+                       updated_at: Time.zone.now - 10.days
+    )
+    allocation.update!(event: AllocationVersion::ALLOCATE_SECONDARY_POM,
+                       secondary_pom_nomis_id: probation_pom[:primary_pom_nomis_id],
+                       secondary_pom_name: probation_pom[:primary_pom_name],
+                       updated_at: Time.zone.now - 8.days
+    )
+    allocation.update!(event: AllocationVersion::DEALLOCATE_SECONDARY_POM,
+                       secondary_pom_nomis_id: nil,
+                       secondary_pom_name: nil,
+                       recommended_pom_type: nil,
+                       updated_at: Time.zone.now - 7.days
     )
 
-    allocation.update(event: AllocationVersion::DEALLOCATE_PRIMARY_POM,
-                      event_trigger: AllocationVersion::USER,
-                      primary_pom_nomis_id: nil,
-                      primary_pom_name: nil,
-                      recommended_pom_type: nil,
-                      updated_at: Time.zone.now - 6.days,
-                      primary_pom_allocated_at: nil
+    allocation.update!(event: AllocationVersion::DEALLOCATE_PRIMARY_POM,
+                       event_trigger: AllocationVersion::USER,
+                       primary_pom_nomis_id: nil,
+                       primary_pom_name: nil,
+                       recommended_pom_type: nil,
+                       updated_at: Time.zone.now - 6.days,
+                       primary_pom_allocated_at: nil
     )
 
     deallocate_date = allocation.updated_at.strftime("#{allocation.updated_at.day.ordinalize} %B %Y")
 
-    allocation.update(event: AllocationVersion::ALLOCATE_PRIMARY_POM,
-                      prison: 'PVI',
-                      primary_pom_nomis_id: prison_pom[:primary_pom_nomis_id],
-                      primary_pom_name: prison_pom[:primary_pom_name],
-                      recommended_pom_type: 'prison',
-                      updated_at: Time.zone.now - 4.days)
+    allocation.update!(event: AllocationVersion::ALLOCATE_PRIMARY_POM,
+                       prison: 'PVI',
+                       primary_pom_nomis_id: prison_pom[:primary_pom_nomis_id],
+                       primary_pom_name: prison_pom[:primary_pom_name],
+                       recommended_pom_type: 'prison',
+                       updated_at: Time.zone.now - 4.days)
 
-    allocation.update(event: AllocationVersion::REALLOCATE_PRIMARY_POM,
-                      primary_pom_nomis_id: pom_without_email[:primary_pom_nomis_id],
-                      primary_pom_name: pom_without_email[:primary_pom_name],
-                      recommended_pom_type: 'probation',
-                      updated_at: Time.zone.now - 2.days)
+    allocation.update!(event: AllocationVersion::REALLOCATE_PRIMARY_POM,
+                       primary_pom_nomis_id: pom_without_email[:primary_pom_nomis_id],
+                       primary_pom_name: pom_without_email[:primary_pom_name],
+                       recommended_pom_type: 'probation',
+                       updated_at: Time.zone.now - 2.days)
 
-    allocation.update(event: AllocationVersion::DEALLOCATE_PRIMARY_POM,
-                      event_trigger: AllocationVersion::OFFENDER_TRANSFERRED,
-                      primary_pom_nomis_id: nil,
-                      primary_pom_name: nil,
-                      recommended_pom_type: nil,
-                      updated_at: Time.zone.now - 1.day,
-                      primary_pom_allocated_at: nil)
+    allocation.update!(event: AllocationVersion::DEALLOCATE_PRIMARY_POM,
+                       event_trigger: AllocationVersion::OFFENDER_TRANSFERRED,
+                       primary_pom_nomis_id: nil,
+                       primary_pom_name: nil,
+                       recommended_pom_type: nil,
+                       updated_at: Time.zone.now - 1.day,
+                       primary_pom_allocated_at: nil)
 
     transfer_date = allocation.updated_at.strftime("#{allocation.updated_at.day.ordinalize} %B %Y") + " (" + allocation.updated_at.strftime("%R") + ")"
 
     history = AllocationService.offender_allocation_history(nomis_offender_id)
-
-    previous_formatted_date = history[1].updated_at.strftime("#{history[1].updated_at.day.ordinalize} %B %Y") + " (" + history[1].updated_at.strftime("%R") + ")"
-    pvi_allocation_date = history[2].updated_at.strftime("#{history[2].updated_at.day.ordinalize} %B %Y") + " (" + history[2].updated_at.strftime("%R") + ")"
-    old_formatted_date = history[4].updated_at.strftime("#{history[4].updated_at.day.ordinalize} %B %Y") + " (" + history[4].updated_at.strftime("%R") + ")"
-    initial_allocated_date = history.last.updated_at.strftime("#{history.last.updated_at.day.ordinalize} %B %Y") + " (" + history.last.updated_at.strftime("%R") + ")"
+    history1 = history[1]
+    history2 = history[2]
+    hist_allocate_secondary = history[5]
+    history6 = history[6]
 
     signin_user
     visit prison_allocation_history_path('LEI', nomis_offender_id)
@@ -107,25 +117,32 @@ feature 'Allocation History' do
         ['.govuk-heading-s', "Prisoner unallocated (transfer)"],
         ['.time', transfer_date.to_s],
         ['.govuk-heading-s', "Prisoner reallocated"],
-        ['p', "Prisoner reallocated to #{history[1].primary_pom_name} Tier: #{history[1].allocated_at_tier}"],
-        ['.time', "#{previous_formatted_date} by #{history[1].created_by_name.titleize}"],
+        ['p', "Prisoner reallocated to #{history1.primary_pom_name} Tier: #{history1.allocated_at_tier}"],
+        ['.time', "#{formatted_date_for(history1)} by #{history1.created_by_name.titleize}"],
         ['.govuk-heading-s', "Prisoner allocation"],
-        ['p', "Prisoner allocated to #{history[2].primary_pom_name.titleize} - #{prison_pom[:email]} Tier: #{history[2].allocated_at_tier}"],
-        ['.time', "#{pvi_allocation_date} by #{history[2].created_by_name.titleize}"],
+        ['p', "Prisoner allocated to #{history2.primary_pom_name.titleize} - #{prison_pom[:email]} Tier: #{history2.allocated_at_tier}"],
+        ['.time', "#{formatted_date_for(history2)} by #{history2.created_by_name.titleize}"],
+        ['p', "Prisoner allocated to #{hist_allocate_secondary.secondary_pom_name.titleize} - #{probation_pom[:email]} Tier: #{hist_allocate_secondary.allocated_at_tier}"],
+        ['.time', "#{formatted_date_for(hist_allocate_secondary)} by #{hist_allocate_secondary.created_by_name.titleize}"],
         ['.govuk-heading-m', "HMP Leeds"],
         ['.govuk-heading-s', "Prisoner unallocated"],
+        ['.govuk-heading-s', "Co-working unallocated"],
         ['.time', deallocate_date.to_s],
         ['.govuk-heading-s', "Prisoner reallocated"],
-        ['p', "Prisoner reallocated to #{history[4].primary_pom_name} - #{probation_pom_2[:email]} Tier: #{history[4].allocated_at_tier}"],
-        ['.time', "#{old_formatted_date} by #{history[4].created_by_name.titleize}"],
+        ['p', "Prisoner reallocated to #{history6.primary_pom_name} - #{probation_pom_2[:email]} Tier: #{history6.allocated_at_tier}"],
+        ['.time', "#{formatted_date_for(history6)} by #{history6.created_by_name.titleize}"],
         ['.govuk-heading-s', "Prisoner allocation"],
         ['p', "Prisoner allocated to #{history.last.primary_pom_name.titleize} - #{probation_pom[:email]} Tier: #{history.last.allocated_at_tier}"],
         ['p', "Probation POM allocated instead of recommended Prison POM", "Reason(s):", "- Prisoner assessed as suitable for a prison POM despite tiering calculation", "Too high risk"],
-        ['.time', "#{initial_allocated_date} by #{history.last.created_by_name.titleize}"]
+        ['.time', "#{formatted_date_for(history.last)} by #{history.last.created_by_name.titleize}"]
     ]
 
     TESTS.each do |key, val|
       expect(page).to have_css(key, text: val)
     end
+  end
+
+  def formatted_date_for(history)
+    history.updated_at.strftime("#{history.updated_at.day.ordinalize} %B %Y") + " (" + history.updated_at.strftime("%R") + ")"
   end
 end
