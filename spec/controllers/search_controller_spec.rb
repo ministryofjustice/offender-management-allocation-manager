890ed1d9646e7b52c26736caca68292192076c51
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-08-02 09:22:32 +0100

    Added comments about spike

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
+# Note - this is a rubbish test. It doesn't actually test the search controller
+# it's part of a spike to demonstrate that we can write controller tests pretty easily
+# by hard-wiring the sso_data in the session.
+# It might be better to mock slightly higher up than done here - the normal flow of a controller
+# test is to setup models and then call the action - the current code doesn't really have a
+# nice clean 'data layer' where mocking could be introduced without being a source of defects.
 RSpec.describe SearchController, type: :controller do
-  let(:prison) { 'RSI' }
+  # Need to use a different prison than LEI to prevent filling Rails cache with mock data
+  let(:prison) { 'WEI' }
   let(:booking_id) { 'booking3' }
   let(:elite2api) { 'https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api' }
+  let(:elite2listapi) { "#{elite2api}/locations/description/#{prison}/inmates?convictedStatus=Convicted&returnCategory=true" }
 
   before do
     allow(Nomis::Oauth::TokenService).to receive(:valid_token).and_return(OpenStruct.new(access_token: 'token'))
@@ -13,7 +21,7 @@ RSpec.describe SearchController, type: :controller do
   end
 
   it 'can search' do
-    stub_request(:get, "#{elite2api}/locations/description/#{prison}/inmates?convictedStatus=Convicted&returnCategory=true").
+    stub_request(:get, elite2listapi).
       with(
         headers: {
           'Authorization' => 'Bearer token',
@@ -21,9 +29,9 @@ RSpec.describe SearchController, type: :controller do
           'Page-Limit' => '1',
           'Page-Offset' => '1'
         }).
-      to_return(status: 200, body: {}.to_json, headers: { 'Total-Records' => '931' })
+      to_return(status: 200, body: {}.to_json, headers: { 'Total-Records' => '31' })
 
-    stub_request(:get, "#{elite2api}/locations/description/#{prison}/inmates?convictedStatus=Convicted&returnCategory=true").
+    stub_request(:get, elite2listapi).
       with(
         headers: {
           'Authorization' => 'Bearer token',
@@ -32,7 +40,7 @@ RSpec.describe SearchController, type: :controller do
           'Page-Offset' => '0'
         }).
       to_return(status: 200, body: {}.to_json, headers: {})
-    stub_request(:get, "#{elite2api}/locations/description/#{prison}/inmates?convictedStatus=Convicted&returnCategory=true").
+    stub_request(:get, elite2listapi).
       with(
         headers: {
           'Authorization' => 'Bearer token',
@@ -54,6 +62,7 @@ RSpec.describe SearchController, type: :controller do
     get :search, params: { prison_id: prison, q: 'Cal' }
     expect(response.status).to eq(200)
     expect(response).to be_successful
+
     expect(assigns(:q)).to eq('Cal')
     expect(assigns(:offenders).size).to eq(0)
 
