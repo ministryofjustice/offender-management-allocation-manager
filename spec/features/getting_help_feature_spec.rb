26d7c775343d4d6752f6a5b88302bc224c70e4e8
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-08-07 12:09:23 +0100

    Add content for help page

--- spec/features/getting_help_feature_spec.rb
+++ spec/features/getting_help_feature_spec.rb
@@ -4,6 +4,9 @@ feature 'Getting help' do
   it "can show the help page", vcr: { cassette_name: :getting_help_text } do
     visit "help"
     expect(page).to have_content('Help')
+    expect(page).to have_link('Back')
+    expect(page).to have_link('Read guidance', href: 'guidance')
+    expect(page).to have_link('moic@digital.justice.gov.uk')
   end
 
   it "can show the contact page", vcr: { cassette_name: :getting_help_contact } do
