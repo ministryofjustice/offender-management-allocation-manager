c7c6e451d2e557af12d197b18ffe7ee96e002873
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-08-09 15:08:22 +0100

    add unique index to prevent duplicate CaseInformation records

---
+++ db/migrate/20190809140516_make_case_information_nomis_id_unique.rb
@@ -0,0 +1,12 @@
+class MakeCaseInformationNomisIdUnique < ActiveRecord::Migration[5.2]
+  # prevent race conditions on creating CaseInformation records
+  def up
+    remove_index :case_information, :nomis_offender_id
+    add_index :case_information, :nomis_offender_id, unique: true
+  end
+
+  def down
+    remove_index :case_information, :nomis_offender_id
+    add_index :case_information, :nomis_offender_id
+  end
+end
