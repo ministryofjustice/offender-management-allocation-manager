dbefb0567a99d16c1a3958c34de96352af27d05e
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-09-05 11:37:26 +0100

    Update content

--- config/initializers/turnout.rb
+++ config/initializers/turnout.rb
@@ -4,7 +4,7 @@ Turnout.configure do |config|
   config.named_maintenance_file_paths = { default: config.app_root.join('tmp', 'maintenance.yml').to_s }
   config.maintenance_pages_path = config.app_root.join('public').to_s
   config.default_maintenance_page = Turnout::MaintenancePage::HTML
-  config.default_reason = 'We are working to fix the problem. Try again soon.'
+  config.default_reason = 'We are carrying out essential maintenance. Try again soon.'
   config.default_allowed_paths = ['/assets']
   config.default_response_code = 503
   config.default_retry_after = 7200
