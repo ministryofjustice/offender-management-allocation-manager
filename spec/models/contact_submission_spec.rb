4cfeeb01f39dde9c84ba3f2e0b5bdf9620cce53a
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-09-09 12:01:02 +0100

    Update spec to use job_type rather than role

--- spec/models/contact_submission_spec.rb
+++ spec/models/contact_submission_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ContactSubmission, type: :model do
-  it { described_class.validates_presence_of(:role) }
+  it { described_class.validates_presence_of(:job_type) }
   it { described_class.validates_presence_of(:email_address) }
   it { described_class.validates_presence_of(:name) }
   it { described_class.validates_presence_of(:body) }
