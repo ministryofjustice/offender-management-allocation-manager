f9058163b8e2031d35d9b61fc60664dc73676eaf
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-09-09 08:51:35 +0100

    Refactor help feature spec

--- spec/features/getting_help_feature_spec.rb
+++ spec/features/getting_help_feature_spec.rb
@@ -21,48 +21,8 @@ feature 'Getting help' do
     visit '/help'
 
     expect(page.find("#prison").value).to eq('HMP Leeds')
+    expect(page.find("#name").value).to eq('Kath Pobee-Norris')
 
     expect(page).to have_button('Submit')
-  end
-
-  it 'shows an error when empty form submitted' do
-    visit '/help'
-
-    click_button('Submit')
-
-    expect(page).to have_content('There is a problem')
-  end
-
-  it 'redirects a signed in user to the dashboard', :raven_intercept_exception, vcr: { cassette_name: :help_logged_in_submit_form } do
-    allow(Rails.configuration).to receive(:zendesk_enabled).and_return(false)
-    allow_any_instance_of(SSOIdentity).to receive(:sso_identity).and_return(
-      'username' => 'PK000223',
-      'caseload' => ['LEI'],
-      'active_caseload' => 'LEI'
-                                                                            )
-
-    signin_user('PK000223')
-    visit '/help'
-
-    fill_in 'job_type', with: 'SPO'
-    fill_in 'message', with: 'This is a test'
-    click_button('Submit')
-
-    expect(page).to have_css('.notification', text: 'Your form has been submitted')
-    expect(page).to have_current_path prison_dashboard_index_path('LEI')
-  end
-
-  it 'redirects a logged out user to the help page' do
-    visit '/help'
-
-    fill_in 'name', with: 'Kath'
-    fill_in 'email_address', with: 'Kath@example.com'
-    fill_in 'prison', with: 'Leeds'
-    fill_in 'job_type', with: 'SPO'
-    fill_in 'message', with: 'This is a test'
-    click_on('Submit')
-
-    expect(page).to have_css('.notification', text: 'Your form has been submitted')
-    expect(page).to have_current_path help_path
   end
 end
