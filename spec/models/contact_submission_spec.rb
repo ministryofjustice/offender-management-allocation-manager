ef864cfa26dbfd40f4f7eb4da4a6c4c1ff518c44
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-09-09 13:57:02 +0100

    Fix contact_submission model spec

--- spec/models/contact_submission_spec.rb
+++ spec/models/contact_submission_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe ContactSubmission, type: :model do
   it { described_class.validates_presence_of(:job_type) }
   it { described_class.validates_presence_of(:email_address) }
   it { described_class.validates_presence_of(:name) }
-  it { described_class.validates_presence_of(:body) }
+  it { described_class.validates_presence_of(:message) }
   it { described_class.validates_presence_of(:prison) }
 end
