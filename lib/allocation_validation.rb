1b4696e16b2db5553052a3cc6c77df6d9a0d6fd3
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-09-11 11:24:09 +0100

    Unallocate anyone who is allocated but not sentenced
    If the offender is released (and maybe we missed it) it's possible
    they'll come back on a different sentence, but will still be allocated
    when they are not sentenced.

--- lib/allocation_validation.rb
+++ lib/allocation_validation.rb
@@ -28,6 +28,14 @@ class AllocationValidation
         next
       end
 
+      if offender.sentenced? == false
+        # This offender should not have any case information, and should not
+        # be allocated to anybody We will de-allocate the offender so they can
+        # be re-allocated against a new offense.
+        puts "#{offender.offender_no} is not sentenced - deallocating"
+        AllocationVersion.deallocate_offender(offender.offender_no, 'offender_released')
+      end
+
       # If the offender is at this prison, we're good .
       next if offender.latest_location_id == prison
 
