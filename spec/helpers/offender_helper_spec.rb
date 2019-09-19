85e12dd542985f09f3fb66368aee10e36b597aea
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-09-19 09:03:52 +0100

    move case_owner_label method

--- spec/helpers/offender_helper_spec.rb
+++ spec/helpers/offender_helper_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe OffenderHelper do
       expect(digital_prison_service_profile_path('AB1234A')).to eq("#{Rails.configuration.digital_prison_service_host}/offenders/AB1234A/quick-look")
     end
   end
+
+  describe 'generates labels for case owner ' do
+    it 'can show Custody for Prison' do
+      off = Nomis::Offender.new
+      off.sentence = Nomis::SentenceDetail.new
+
+      expect(case_owner_label(off)).to eq('Custody')
+    end
+
+    it 'can show Community for Probation' do
+      off = Nomis::Offender.new
+      off.sentence = Nomis::SentenceDetail.new
+      off.sentence.release_date = Time.zone.today
+
+      expect(case_owner_label(off)).to eq('Community')
+    end
+  end
 end
