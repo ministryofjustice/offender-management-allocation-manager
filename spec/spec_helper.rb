4467c77b6386a17a4d53822ce9859c79ac1c4292
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-09-20 14:49:11 +0100

    disable API cache in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 
-SimpleCov.minimum_coverage 95
+# Try to set this to current coverage levels so that it never goes down after a PR
+SimpleCov.minimum_coverage 96.39
 
 SimpleCov.start 'rails' do
   add_filter '/gems/'
