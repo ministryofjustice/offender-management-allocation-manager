2009152e5e2c1b640d6b1997c3200cbccdf20cd5
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-09-23 15:37:10 +0100

    fix up after rebase

--- app/models/offender_presenter.rb
+++ app/models/offender_presenter.rb
@@ -4,12 +4,13 @@ class OffenderPresenter
   delegate :offender_no, :first_name, :last_name, :latest_booking_id,
            :indeterminate_sentence?, :sentence_type_code, :describe_sentence,
            :full_name_ordered, :full_name, :main_offence,
+           :sentence_start_date, :team,
            :home_detention_curfew_eligibility_date, :tariff_date,
            :date_of_birth, :release_date, :parole_eligibility_date,
-           :pom_responsibility, :welsh_offender, :case_allocation, :earliest_release_date,
+           :welsh_offender, :case_allocation, :earliest_release_date,
            :category_code, :conditional_release_date, :automatic_release_date,
            :awaiting_allocation_for, :allocated_pom_name, :allocation_date,
-           :tier, :crn, :convicted_status, :convicted?,
+           :tier, :crn, :convicted_status, :convicted?, :ldu,
            :over_18?, :recalled?, :sentenced?, to: :@offender
 
   def initialize(offender, responsibility)
@@ -17,16 +18,15 @@ class OffenderPresenter
     @responsibility = responsibility
   end
 
-  def case_owner
+  def pom_responsibility
     if @responsibility
-      @responsibility.value
-    else
-      pom_responsibility = ResponsibilityService.calculate_pom_responsibility(@offender)
-      if pom_responsibility == ResponsibilityService::RESPONSIBLE
-        'Prison'
+      if @responsibility.value == Responsibility::PRISON
+        ResponsibilityService::RESPONSIBLE
       else
-        'Probation'
+        ResponsibilityService::SUPPORTING
       end
+    else
+      @offender.pom_responsibility
     end
   end
 end
