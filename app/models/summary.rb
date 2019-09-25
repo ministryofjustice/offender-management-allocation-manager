65a3a1e04a129d1bddd66fc1637bc65df4f066bd
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-09-25 14:02:59 +0100

    Remove unused method from summary model

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -8,12 +8,4 @@ class Summary
     @summary_type = summary_type
   end
 
-private
-
-  def total_for_summary_type
-    return allocated_total if @summary_type == :allocated
-    return unallocated_total if @summary_type == :unallocated
-
-    pending_total if @summary_type == :pending
-  end
 end
