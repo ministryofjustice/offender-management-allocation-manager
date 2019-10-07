8b7ffc503b7cf19bc1527f363c7fd1fb4a37ee53
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-10-07 09:35:03 +0100

    Appease the linter

--- app/controllers/prisons_application_controller.rb
+++ app/controllers/prisons_application_controller.rb
@@ -14,7 +14,10 @@ protected
 private
 
   def check_prison_access
-    redirect_to '/401' and return unless PrisonService.exists?(active_prison)
+    unless PrisonService.exists?(active_prison)
+      redirect_to('/401')
+      return
+    end
     redirect_to '/401' unless caseloads.include?(active_prison)
 
     @prison = active_prison
