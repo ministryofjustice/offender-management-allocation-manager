eaedef7223c570f2661d8a426a1e9f5e3037c71f
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-10-11 13:44:06 +0100

    revert Nomis namespace removal

--- spec/services/nomis/api_deserialiser_spec.rb
+++ spec/services/nomis/api_deserialiser_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ApiDeserialiser do
+RSpec.describe Nomis::ApiDeserialiser do
   # rubocop:disable RSpec/LeakyConstantDeclaration
   class DeserialiseTest
     attr_accessor :foo, :string
