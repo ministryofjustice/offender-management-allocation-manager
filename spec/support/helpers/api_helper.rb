cb3e6e4c109c9a7d398e8b1efae63a40a530d3a3
Ross Jones <ross.jones@digital.justice.gov.uk>
2019-10-17 16:01:03 +0100

    Case Tasks page
    Starts the page/controller for the case tasks, which are tasks that poms
    need to complete. This is not yet complete.
    
    We are missing an action for case info that need a delius record check,
    and we are waiting on other PRs that will allow us to get early
    allocation records that are pending a community response.

--- spec/support/helpers/api_helper.rb
+++ spec/support/helpers/api_helper.rb
@@ -1,9 +1,7 @@
 module ApiHelper
-  def stub_offender(nomis_id)
-    booking_number = 754_165
-
+  def stub_offender(nomis_id, booking_number: 754_165, imprisonment_status: 'SENT03')
     stub_request(:get, "https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api/prisoners/#{nomis_id}").
-      to_return(status: 200, body: [{ offenderNo: nomis_id, gender: 'Male', latestBookingId: booking_number }].to_json)
+      to_return(status: 200, body: [{ offenderNo: nomis_id, gender: 'Male', latestBookingId: booking_number, imprisonmentStatus: imprisonment_status }].to_json)
 
     stub_request(:post, "https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api/offender-sentences/bookings").
       with(
@@ -16,6 +14,7 @@ module ApiHelper
         body: [nomis_id].to_json
       ).
       to_return(status: 200, body: {}.to_json)
+
     stub_request(:get, "https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api/bookings/#{booking_number}/mainOffence").
       to_return(status: 200, body: {}.to_json)
   end
@@ -32,5 +31,39 @@ module ApiHelper
       stub_request(:get, "https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api/staff/#{pom[:staffId]}/emails").
         to_return(status: 200, body: pom[:emails].to_json)
     end
+  end
+
+  def stub_signed_in_pom(staff_id, username)
+    stub_request(:get, "https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api/users/#{username}").
+      to_return(status: 200, body: { 'staffId': staff_id }.to_json)
+  end
+
+  def stub_offenders_for_prison(prison, offenders, bookings)
+    # Stub the call to get_offenders_for_prison. Takes a list of offender hashes (in nomis camelCase format) and
+    # a list of bookings (same key format). It it your responsibility to make sure they contain the data you want
+    # and if you provide a booking, that the id matches between the offender and booking hashes.
+    elite2api = 'https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api'
+    elite2listapi = "#{elite2api}/locations/description/#{prison}/inmates?convictedStatus=Convicted&returnCategory=true"
+    elite2bookingsapi = "#{elite2api}/offender-sentences/bookings"
+
+    # Stub the call that will get the total number of records
+    stub_request(:get, elite2listapi).to_return(
+      status: 200,
+      body: {}.to_json,
+      headers: { 'Total-Records' => offenders.count.to_s }
+    )
+
+    # Return the actual offenders from the call to /locations/description/PRISON/inmates
+    stub_request(:get, elite2listapi).with(
+      headers: {
+        'Page-Limit' => '200',
+        'Page-Offset' => '0'
+      }).to_return(status: 200, body: offenders.to_json)
+
+    # Get the booking ids provided and add a non-existent booking in
+    # case none were provided
+    booking_ids = bookings.map{ |h| h[:bookingId] }.compact + [1]
+    stub_request(:post, elite2bookingsapi).with(body: booking_ids.to_json).
+      to_return(status: 200, body: bookings.to_json, headers: {})
   end
 end
