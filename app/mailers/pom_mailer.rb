6d1a7b9dc0136eb15fe924d0d12fbf85e65ffd40
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-10-17 09:17:19 +0100

    Different dead-end text for early allocations

--- app/mailers/pom_mailer.rb
+++ app/mailers/pom_mailer.rb
@@ -44,7 +44,6 @@ class PomMailer < GovukNotifyRails::Mailer
     mail(to: email)
   end
 
-  # rubocop:disable Metrics/ParameterLists
   def responsibility_override_open_prison(
     prisoner_name:,
     prisoner_number:,
