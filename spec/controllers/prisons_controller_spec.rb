7ab28d7ebe1b20ca0ec290f930be5d69ef999064
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-10-21 08:55:17 +0100

    This bug occurs when a POM's Nomis caseload has not been set up properly, resulting in their caseload being nil.
    Currently when we check the POM's prison access, the app
    crashes because we are not expecting the POM's caseload to be
    nil.
    
    This commit fixes this by redirecting to /401 if the POM's
    caseload is nil.

---
+++ spec/controllers/prisons_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe PrisonsController, type: :controller do
+  describe '#index' do
+    let(:prison) { 'LEI' }
+
+    context 'when caseloads is not empty' do
+      before do
+        stub_sso_data(prison)
+      end
+
+      it 'renders the dashboard page when caseload contains the active_prison' do
+        get :index, params: { prison_id: prison }
+        expect(response).to render_template("index")
+      end
+
+      it 'redirects to /401 when caseloads does not contain the active_prison' do
+        get :index, params: { prison_id: 'WEI' }
+        expect(response.status).to eq(302)
+      end
+
+      it 'redirects to /401 when the active_prison is not valid' do
+        get :index, params: { prison_id: 'ABC' }
+        expect(response.status).to eq(302)
+      end
+    end
+
+    context 'when caseloads is nil' do
+      it 'redirects to /401' do
+        allow(Nomis::Oauth::TokenService).to receive(:valid_token).and_return(OpenStruct.new(access_token: 'token'))
+        session[:sso_data] = {}
+
+        get :index, params: { prison_id: prison }
+        expect(response.status).to eq(302)
+      end
+    end
+  end
+end
