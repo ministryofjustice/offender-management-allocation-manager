f993279efc5e8462aa1f208824bac9214c56a780
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-11-22 16:36:10 +0000

    remove spurious stub_request for users API

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -20,9 +20,7 @@ RSpec.describe DashboardController, type: :controller do
         stub_auth_token
         stub_poms(prison, poms)
         stub_sso_pom_data(prison, 'alice')
-        stub_signed_in_pom(1, 'Alice')
-        stub_request(:get, "https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/api/users/alice").
-          to_return(status: 200, body: { staffId: 1 }.to_json, headers: {})
+        stub_signed_in_pom(1, 'alice')
       end
 
       it 'shows me only Manage case tasks' do
