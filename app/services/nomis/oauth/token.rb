4021eeb93eb4f7e58483495a74f690961ea8801e
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-11-28 08:50:22 +0000

    de-scope Token class

--- app/services/nomis/oauth/token.rb
+++ app/services/nomis/oauth/token.rb
@@ -7,19 +7,20 @@ module Nomis
     class Token
       include Deserialisable
 
-      attr_accessor :access_token,
-                    :token_type,
-                    :expires_in,
-                    :scope,
-                    :internal_user,
-                    :jti,
-                    :auth_source
+      attr_writer :expires_in,
+                  :internal_user,
+                  :token_type,
+                  :auth_source,
+                  :jti
 
-      def initialize(fields = nil)
+      attr_accessor :access_token,
+                    :scope
+
+      def initialize(fields = {})
         # Allow this object to be reconstituted from a hash, we can't use
         # from_json as the one passed in will already be using the snake case
         # names whereas from_json is expecting the elite2 camelcase names.
-        fields.each { |k, v| instance_variable_set("@#{k}", v) } if fields.present?
+        fields.each { |k, v| instance_variable_set("@#{k}", v) }
       end
 
       def expired?
@@ -55,7 +56,7 @@ module Nomis
         Token.new.tap { |obj|
           obj.access_token = payload['access_token']
           obj.token_type = payload['token_type']
-          obj.expires_in = payload['expires_in']
+          obj.expires_in = payload.fetch('expires_in')
           obj.scope = payload['scope']
           obj.internal_user = payload['internal_user']
           obj.jti = payload['jti']
