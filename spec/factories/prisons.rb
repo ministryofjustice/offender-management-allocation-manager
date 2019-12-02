83390068cf40dd9507e6d9b89e82714f9b58ffa2
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-12-02 16:29:33 +0000

    Appease the linter

--- spec/factories/prisons.rb
+++ spec/factories/prisons.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     attr_accessor :code
   end
 
-  factory :prison, class: Elite2Prison do
+  factory :prison, class: 'Elite2Prison' do
     # rotate around every prison - can't use arbitrary codes as we sometimes lookup names in PrisonService
     sequence(:code) do |c|
       codes = PrisonService.prison_codes
