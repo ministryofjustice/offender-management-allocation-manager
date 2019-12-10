50af5b58a29318262821c81e9a99fed6925fd6d9
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-12-10 14:04:27 +0000

    POM-562 use tier rather than allocated_at_tier for tier groups

--- spec/controllers/poms_controller_spec.rb
+++ spec/controllers/poms_controller_spec.rb
@@ -13,8 +13,30 @@ RSpec.describe PomsController, type: :controller do
       build(:pom, staffId: active.nomis_staff_id),
       build(:pom, staffId: unavailable.nomis_staff_id)
     ])
-    create(:allocation, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
-    create(:allocation, primary_pom_nomis_id: inactive.nomis_staff_id, secondary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+    a1 = create(:case_information)
+    create(:allocation, nomis_offender_id: a1.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+
+    a2 = create(:case_information)
+    create(:allocation, nomis_offender_id: a2.nomis_offender_id, primary_pom_nomis_id: inactive.nomis_staff_id, secondary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+
+    b1 = create(:case_information, tier: 'B')
+    create(:allocation, nomis_offender_id: b1.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+    b2 = create(:case_information, tier: 'B')
+    create(:allocation, nomis_offender_id: b2.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+    b3 = create(:case_information, tier: 'B')
+    create(:allocation, nomis_offender_id: b3.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+    b4 = create(:case_information, tier: 'B')
+    create(:allocation, nomis_offender_id: b4.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+
+    c1 = create(:case_information, tier: 'C')
+    create(:allocation, nomis_offender_id: c1.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+    c2 = create(:case_information, tier: 'C')
+    create(:allocation, nomis_offender_id: c2.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+    c3 = create(:case_information, tier: 'C')
+    create(:allocation, nomis_offender_id: c3.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
+
+    d1 = create(:case_information, tier: 'D')
+    create(:allocation, nomis_offender_id: d1.nomis_offender_id, primary_pom_nomis_id: active.nomis_staff_id, prison: prison.code)
   end
 
   render_views
@@ -28,7 +50,12 @@ RSpec.describe PomsController, type: :controller do
     expect(assigns(:active_poms).count).to eq(2)
 
     active_pom = assigns(:active_poms).detect { |pom| pom.status == 'active' }
-    # one primary, one co-working
-    expect(active_pom.total_cases).to eq(2)
+
+    expect(active_pom.tier_a).to eq(2)
+    expect(active_pom.tier_b).to eq(4)
+    expect(active_pom.tier_c).to eq(3)
+    expect(active_pom.tier_d).to eq(1)
+
+    expect(active_pom.total_cases).to eq(10)
   end
 end
