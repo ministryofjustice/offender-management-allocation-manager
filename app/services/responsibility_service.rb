5e3dc8608bfbd676455cdfad83fdfe432d41d59b
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2019-12-13 11:32:58 +0000

    refactored handover service to return one consistent object

--- app/services/responsibility_service.rb
+++ app/services/responsibility_service.rb
@@ -12,92 +12,109 @@ class ResponsibilityService
   COWORKING = 'Co-Working'
   NPS = 'NPS'
 
-  WELSH_POLICY_START_DATE = DateTime.new(2019, 2, 4).utc.to_date
-  ENGLISH_POLICY_START_DATE = DateTime.new(2019, 10, 1).utc.to_date
-
-  ORIGINAL_ENGLISH_POLICY_START_DATE = DateTime.new(2019, 9, 16).utc.to_date
-
   def self.calculate_pom_responsibility(offender)
-    if offender.immigration_case?
-      SUPPORTING
-    elsif open_prison_nps_offender?(offender)
+    if offender.immigration_case? || open_prison_nps_offender?(offender)
       SUPPORTING
     elsif offender.earliest_release_date.nil?
       RESPONSIBLE
     elsif offender.indeterminate_sentence? && offender.earliest_release_date < Time.zone.today
       RESPONSIBLE
-    elsif welsh_offender?(offender)
-      welsh_rules(offender)
     else
-      english_rules(offender)
+      standard_rules(offender)
     end
   end
 
 private
 
-  def self.welsh_rules(offender)
+  def self.standard_rules(offender)
     if offender.recalled?
       SUPPORTING
     elsif nps_case?(offender)
-      welsh_nps_rules(offender)
+      nps_rules(offender)
     else
       crc_rules(offender)
     end
   end
 
-  def self.welsh_nps_rules(offender)
-    if new_case?(offender)
-      welsh_policy_nps_rules(offender)
-    else
-      welsh_prepolicy_nps_rules(offender)
+  class ResponsibilityRules
+    def initialize(policy_start_date)
+      @policy_start_date = policy_start_date
     end
-  end
 
-  def self.welsh_policy_nps_rules(offender)
-    if release_date_gt_10_mths?(offender)
-      RESPONSIBLE
-    else
-      SUPPORTING
+    def new_case?(offender)
+      if offender.sentenced?
+        offender.sentence_start_date > @policy_start_date
+      else
+        true
+      end
     end
-  end
 
-  def self.welsh_prepolicy_nps_rules(offender)
-    if release_date_gt_15_mths_at_policy_date?(offender)
-      RESPONSIBLE
-    else
-      SUPPORTING
-    end
-  end
-
-  def self.english_rules(offender)
-    if offender.recalled?
-      SUPPORTING
-    elsif new_case?(offender)
-      english_policy_rules(offender)
-    else
-      english_prepolicy_rules(offender)
-    end
-  end
-
-  def self.english_policy_rules(offender)
-    if nps_case?(offender)
+    def policy_nps_rules(offender)
       if release_date_gt_10_mths?(offender)
         RESPONSIBLE
       else
         SUPPORTING
       end
-    else
-      crc_rules(offender)
+    end
+
+  private
+
+    def release_date_gt_10_mths?(offender)
+      offender.earliest_release_date >
+        DateTime.now.utc.to_date + 10.months
     end
   end
 
-  def self.hub_or_private?(offender)
-    PrisonService.english_hub_prison?(offender.prison_id) ||
-      PrisonService.english_private_prison?(offender.prison_id)
+  class WelshNpsResponsibiltyRules < ResponsibilityRules
+    WELSH_POLICY_START_DATE = DateTime.new(2019, 2, 4).utc.to_date
+
+    def initialize
+      super WELSH_POLICY_START_DATE
+    end
+
+    def responsibility(offender)
+      if new_case?(offender)
+        policy_nps_rules(offender)
+      else
+        welsh_prepolicy_nps_rules(offender)
+      end
+    end
+
+  private
+
+    def welsh_prepolicy_nps_rules(offender)
+      if release_date_gt_15_mths_at_policy_date?(offender)
+        RESPONSIBLE
+      else
+        SUPPORTING
+      end
+    end
+
+    def release_date_gt_15_mths_at_policy_date?(offender)
+      offender.earliest_release_date >
+        WELSH_POLICY_START_DATE + 15.months
+    end
   end
 
-  def self.english_prepolicy_rules(offender)
-    if nps_case?(offender)
+  class EnglishNpsResponsibilityRules < ResponsibilityRules
+    ENGLISH_POLICY_START_DATE = DateTime.new(2019, 10, 1).utc.to_date
+    ORIGINAL_ENGLISH_POLICY_START_DATE = DateTime.new(2019, 9, 16).utc.to_date
+
+    def initialize
+      super ENGLISH_POLICY_START_DATE
+    end
+
+    def responsibility(offender)
+      if new_case?(offender)
+        policy_nps_rules(offender)
+      else
+        english_prepolicy_nps_rules(offender)
+      end
+    end
+
+  private
+
+    def english_prepolicy_nps_rules(offender)
       if hub_or_private?(offender)
         threshold = 20.months
       else
@@ -108,8 +125,24 @@ private
       else
         SUPPORTING
       end
+    end
+
+    def hub_or_private?(offender)
+      PrisonService.english_hub_prison?(offender.prison_id) ||
+        PrisonService.english_private_prison?(offender.prison_id)
+    end
+
+    def release_date_gt_17_mths_at_policy_date?(offender, threshold)
+      offender.earliest_release_date >
+        ORIGINAL_ENGLISH_POLICY_START_DATE + threshold
+    end
+  end
+
+  def self.nps_rules(offender)
+    if welsh_offender?(offender)
+      WelshNpsResponsibiltyRules.new.responsibility(offender)
     else
-      crc_rules(offender)
+      EnglishNpsResponsibilityRules.new.responsibility(offender)
     end
   end
 
@@ -133,33 +166,8 @@ private
     offender.case_allocation == NPS
   end
 
-  def self.release_date_gt_10_mths?(offender)
-    offender.earliest_release_date >
-      DateTime.now.utc.to_date + 10.months
-  end
-
-  def self.release_date_gt_15_mths_at_policy_date?(offender)
-    offender.earliest_release_date >
-      WELSH_POLICY_START_DATE + 15.months
-  end
-
-  def self.release_date_gt_17_mths_at_policy_date?(offender, threshold)
-    offender.earliest_release_date >
-      ORIGINAL_ENGLISH_POLICY_START_DATE + threshold
-  end
-
   def self.release_date_gt_12_weeks?(offender)
     offender.earliest_release_date >
       DateTime.now.utc.to_date + 12.weeks
-  end
-
-  def self.new_case?(offender)
-    return true unless offender.sentenced?
-
-    if offender.welsh_offender
-      offender.sentence_start_date > WELSH_POLICY_START_DATE
-    else
-      offender.sentence_start_date > ENGLISH_POLICY_START_DATE
-    end
   end
 end
