574668f58365669a3d853bd8ab7a2e0ab319ada4
Kyle <kjdchapman@gmail.com>
2020-02-10 14:02:37 +0000

    Add failing acceptance spec for missing data
    In the wake of 3a3ec5 (which updates how the recommended
    responsibility is calculated), we discovered issues in production with
    certain offenders. These offenders, when looked up in the NOMIS API,
    were missing certain dates the app always expects to be present.
    
    When this data isn't present, the app 'crashes' (returning a 500 for
    the request, but continuing to serve other requests). This is the case
    for pages with multiple offenders if only a single one is missing data.
    
    This commit does not stop the app crashing - it only adds a spec where
    the absolute minimum data is returned from the API, to start working
    through the errors.
    
    Co-authored-by: Maysa <maysa@madetech.com>

---
+++ spec/features/when_nomis_is_missing_information_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+context 'when NOMIS is missing information' do
+  let(:stub_prison_code) { 'LEI' }
+
+  before do
+    stub_auth_host = 'http://nomis.mock'
+    stub_api_host = 'http://nomis.mock/elite2api/api'
+    stub_user_name = 'example_user'
+    stub_staff_id = 1
+    stub_poms = [{ staffId: 1, position: RecommendationService::PRISON_POM }]
+    stub_offenders = [{ offenderNo: 1 }]
+
+    Rails.configuration.nomis_oauth_host = stub_auth_host
+
+    stub_request(:post, "#{stub_auth_host}/auth/oauth/token").
+      with(query: { grant_type: 'client_credentials' }).
+      to_return(status: 200, body: {}.to_json)
+
+    stub_request(:get, "#{stub_api_host}/users/example_user").
+      to_return(status: 200, body: { staffId: stub_staff_id }.to_json)
+
+    stub_request(:get, "#{stub_api_host}/staff/#{stub_staff_id}/emails").
+      to_return(status: 200, body: [].to_json)
+
+    stub_request(:get, "#{stub_api_host}/staff/roles/#{stub_prison_code}/role/POM").
+      to_return(status: 200, body: stub_poms.to_json)
+
+    stub_request(:get, "#{stub_api_host}/locations/description/#{stub_prison_code}/inmates").
+      with(query: { convictedStatus: 'Convicted', returnCategory: true }).
+      to_return(status: 200, body: stub_offenders.to_json)
+
+    stub_request(:post, "#{stub_api_host}/offender-sentences/bookings").
+      to_return(status: 200, body: [].to_json)
+
+    signin_pom_user(stub_user_name)
+  end
+
+  describe 'the caseload page' do
+    it 'does not error' do
+      visit prison_caseload_index_path(stub_prison_code)
+
+      expect(page).to have_content('Showing 1 - 1 of 1 results')
+    end
+  end
+end
