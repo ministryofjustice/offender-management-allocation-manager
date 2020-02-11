e2f7fa0fb2dc822c71f90809f3c11009500c0784
Kyle <kjdchapman@gmail.com>
2020-02-11 15:33:22 +0000

    Add minimum information to caseload spec
    To better understand which information causes the app to crash, we've
    built out a spec that stubs out the API calls with a very limited set
    of data.
    
    Co-authored-by: Maysa <maysa@madetech.com>

--- spec/features/when_nomis_is_missing_information_spec.rb
+++ spec/features/when_nomis_is_missing_information_spec.rb
@@ -9,7 +9,20 @@ context 'when NOMIS is missing information' do
     stub_user_name = 'example_user'
     stub_staff_id = 1
     stub_poms = [{ staffId: 1, position: RecommendationService::PRISON_POM }]
-    stub_offenders = [{ offenderNo: 1 }]
+    stub_offenders = [{
+      offenderNo: "A1",
+      bookingId: 1,
+      dateOfBirth: '1990-01-01',
+      imprisonmentStatus: 'LIFE'
+    }]
+
+    stub_bookings = [{
+      bookingId: 1,
+      sentenceDetail: {
+        releaseDate: 30.years.from_now.iso8601,
+        sentenceStartDate: Time.zone.now.iso8601
+      }
+    }]
 
     Rails.configuration.nomis_oauth_host = stub_auth_host
 
@@ -31,13 +44,15 @@ context 'when NOMIS is missing information' do
       to_return(status: 200, body: stub_offenders.to_json)
 
     stub_request(:post, "#{stub_api_host}/offender-sentences/bookings").
-      to_return(status: 200, body: [].to_json)
+      to_return(status: 200, body: stub_bookings.to_json)
 
     signin_pom_user(stub_user_name)
   end
 
   describe 'the caseload page' do
     it 'does not error' do
+      create(:allocation, nomis_offender_id: "A1", primary_pom_nomis_id: 1)
+
       visit prison_caseload_index_path(stub_prison_code)
 
       expect(page).to have_content('Showing 1 - 1 of 1 results')
