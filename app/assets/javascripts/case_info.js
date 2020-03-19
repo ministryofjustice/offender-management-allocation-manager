993d00dcabe51fcff75b00f454eb35458f9c34ca
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2020-03-19 14:20:36 +0000

    Add autocomplete text field when adding team in case information missing_info page

--- app/assets/javascripts/case_info.js
+++ app/assets/javascripts/case_info.js
@@ -1,8 +1,15 @@
 $(document).on("turbolinks:load", function(){
-    if ($(".edit_case_information").length > 0){
+    if ($("#team_autocomplete").length > 0) {
+        let element = document.querySelector('#team_autocomplete');
+        let id = 'autocomplete-default';
+
+        accessibleAutocomplete({element: element, id: id, source: team_names()});
+    }
+
+    if ($(".case_information.edit").length > 0){
         if((document.getElementById('case_information_probation_service_scotland').checked) ||
             (document.getElementById('case_information_probation_service_northern_ireland').checked)){
-            hide_element();
+                hide_element();
         }
     }
 });
@@ -15,6 +22,6 @@ function show_element() {
     $('.optional-case-info').show();
 }
 
-
-
-
+function team_names() {
+    return $('.team_information').data('team')
+}
