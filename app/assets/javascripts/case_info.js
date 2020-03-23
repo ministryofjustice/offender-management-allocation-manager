e34c761e06916b463581929ed5e70f33aa8ca7d4
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2020-03-23 08:09:23 +0000

    Add javascript functionality to remove selected team

--- app/assets/javascripts/case_info.js
+++ app/assets/javascripts/case_info.js
@@ -3,7 +3,14 @@ $(document).on("turbolinks:load", function(){
         let element = document.querySelector('#team_autocomplete');
         let id = 'autocomplete-default';
 
-        accessibleAutocomplete({element: element, id: id, source: team_names()});
+        accessibleAutocomplete({
+            element: element,
+            id: id,
+            source: team_names(),
+            onConfirm: (val) => {
+                document.getElementById("chosen_team").innerHTML = val.bold();
+            }
+        });
     }
 
     if ($(".case_information.edit").length > 0){
@@ -24,4 +31,12 @@ function show_element() {
 
 function team_names() {
     return $('.team_information').data('team')
+}
+
+function remove_team() {
+    if (document.getElementById("chosen_team").innerText.length > 0) {
+        document.getElementById("chosen_team").innerText = "";
+        documeent.getElementById("autocomplete-default").innerText = "";
+        // clear the value in id 'autocomplete-default'
+    }
 }
