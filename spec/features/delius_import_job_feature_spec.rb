2e9df84e167a00a1dcd40c74aa79844def0adb58
Maysa <maysa@madetech.com>
2020-04-29 11:28:00 +0100

    Add a feature spec for the Delius import
    This is an end to end test covering how a case information record is
    created. An offender exists in Nomis with some key information including
    the tier of the offender missing. At this point, they exist in the update
    case informations page. This information is required to make a
    CaseInformation entry for the offender. The Delius import retrives this
    from Delius and matches the Nomis offender to the ndelius offender so
    a CaseInformation entry is made.  Once this is updated, the offender
    should no longer show up in this page. This test covers this flow end
    to end.
    
    Co-authored-by: Ollie Treend <ollie@treend.com>

---
+++ spec/features/delius_import_job_feature_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+require 'support/lib/mock_imap'
+require 'support/lib/mock_mail'
+require 'delius_import_job'
+
+feature "Delius import feature" do
+  let(:stub_auth_host) { Rails.configuration.nomis_oauth_host }
+  let(:stub_api_host) { "#{stub_auth_host}/elite2api/api" }
+  let(:offender_no) { "GCA2H2A" }
+  let(:prison) { "LEI" }
+  let(:booking_id) { 754_207 }
+  let(:offenders) {
+    [{
+      "bookingId": booking_id,
+      "offenderNo": offender_no,
+      "dateOfBirth": "1985-03-19",
+      "agencyId": prison,
+      "imprisonmentStatus": "DET"
+    }]
+  }
+  let(:bookings) {
+    [{  "bookingId": booking_id,
+        "offenderNo": offender_no,
+        "agencyLocationId": prison,
+        "sentenceDetail": { "homeDetentionCurfewEligibilityDate": "2011-11-07",
+                            "sentenceStartDate": "2009-02-08"
+                          }
+    }]
+  }
+
+  before do
+    signin_spo_user
+
+    stub_request(:post, "#{stub_auth_host}/auth/oauth/token").
+      with(query: { grant_type: 'client_credentials' }).
+        to_return(status: 200, body: {}.to_json)
+
+    stub_offender(offender_no, booking_number: booking_id, imprisonment_status: 'DET', dob: "1985-03-19")
+
+    stub_offenders_for_prison("LEI", offenders, bookings)
+
+    stub_request(:post, "#{stub_api_host}/movements/offenders?latestOnly=false&movementTypes=TRN").
+      with(body: [offender_no].to_json).
+        to_return(status: 200, body: [{ offenderNo: offender_no, toAgency: prison }].to_json)
+
+    stub_const("Net::IMAP", MockIMAP)
+
+    stub_const("Mail", MockMailMessage)
+
+    ENV['DELIUS_EMAIL_FOLDER'] = 'delius_import_feature'
+    ENV['DELIUS_XLSX_PASSWORD'] = 'secret'
+
+    create(:team, code: 'A')
+  end
+
+  it "imports the Delius spreadsheet and creates case information" do
+    visit prison_summary_pending_path(prison)
+    expect(page).to have_content("Add missing information")
+    expect(page).to have_content(offender_no)
+
+    DeliusImportJob.perform_now
+
+    reload_page
+    expect(page).to have_content("Add missing information")
+    expect(page).not_to have_content(offender_no)
+
+    visit prison_summary_unallocated_path(prison)
+    expect(page).to have_content("Make allocations")
+    expect(page).to have_content(offender_no)
+  end
+end
