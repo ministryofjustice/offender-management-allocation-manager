c60c1a36feac07b5b1f4c5e7c26bb8c24cad4147
Ollie Treend <ollie@treend.com>
2020-04-30 17:54:51 +0100

    Auto-update active team names and LDU associations
    The Delius import spreadsheet is the 'source of truth' for the names of
    active teams and their associations to LDUs. We've updated the Delius
    import job to automatically update the names and associations between
    Teams and LDUs in our database to reflect those received in the Delius
    extract.
    
    We've created a new service object, `UpdateTeamNameAndLduService`, which
    updates the team names and associations to LDUs in our database. It will
    not create new Team or LDU records in our database.
    
    Co-authored-by: Maysa Kanoni <maysa@madetech.com>

--- app/jobs/delius_import_job.rb
+++ app/jobs/delius_import_job.rb
@@ -104,9 +104,35 @@ private
 
   def process_decrypted_file(filename)
     Rails.logger.info('[DELIUS] Processing decrypted file')
-
-    total = 0
     processor = Delius::Processor.new(filename)
+    update_team_names_and_ldus(processor)
+    upsert_delius_data_records(processor)
+  end
+
+  def update_team_names_and_ldus(processor)
+    processor.each_with_index do |row, index|
+      # skip header row in row[0]
+      next if index == 0
+
+      record = {}
+
+      # For each row, map the column to the appropriate column name
+      # as the existing column names are not very hash/symbol friendly
+      row.each_with_index do |val, idx|
+        key = FIELDS[idx]
+        record[key] = val
+      end
+
+      UpdateTeamNameAndLduService.update(
+        team_code: record[:team_code],
+        team_name: record[:team],
+        ldu_code: record[:ldu_code]
+      )
+    end
+  end
+
+  def upsert_delius_data_records(processor)
+    total = 0
     processor.each_with_index do |row, index|
       # skip header row in row[0]
       next if index == 0
