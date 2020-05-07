2ba18a360581f5ae739826cba3e3878176e37915
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2020-05-07 08:32:43 +0100

    Add tests for CaseInformationHelper

--- app/helpers/case_information_helper.rb
+++ app/helpers/case_information_helper.rb
@@ -7,37 +7,46 @@ module CaseInformationHelper
     EMAIL_MESSAGES.fetch(error_type, 'No match could be made.')
   end
 
-  def flash_notice_text(error_type, prisoner)
+  # rubocop:disable Metrics/MethodLength
+  def flash_notice_text(error_type:, prisoner:)
     case error_type
     when DeliusImportError::DUPLICATE_NOMIS_ID
-
-      msg = "There’s more than one nDelius record with this NOMIS number #{prisoner.offender_no} for #{prisoner.full_name}. The community probation team need to update nDelius."
+      msg = "There’s more than one nDelius record with this NOMIS number #{prisoner.offender_no} for "\
+            "#{prisoner.full_name}. The community probation team need to update nDelius."
     when DeliusImportError::MISSING_DELIUS_RECORD
-      msg = "There’s no nDelius match for #{prisoner.full_name}, NOMIS number #{prisoner.offender_no}. The community probation team need to update nDelius."
+      msg = "There’s no nDelius match for #{prisoner.full_name}, NOMIS number #{prisoner.offender_no}. The community "\
+            'probation team need to update nDelius.'
     when DeliusImportError::INVALID_TIER
-      msg = "There’s no tier recorded in nDelius for #{prisoner.full_name}, NOMIS number #{prisoner.offender_no}.The community probation team need to update nDelius."
+      msg = "There’s no tier recorded in nDelius for #{prisoner.full_name}, NOMIS number #{prisoner.offender_no}. "\
+            'The community probation team need to update nDelius.'
     when DeliusImportError::INVALID_CASE_ALLOCATION
-      msg = "There’s no service provider in nDelius for #{prisoner.full_name}, NOMIS number #{prisoner.offender_no}. The community probation team need to update nDelius."
+      msg = "There’s no service provider in nDelius for #{prisoner.full_name}, NOMIS number #{prisoner.offender_no}. "\
+            'The community probation team need to update nDelius.'
     when DeliusImportError::MISMATCHED_DOB
-      msg = "There’s an nDelius record with NOMIS number #{prisoner.offender_no} - #{prisoner.full_name} but a different date of birth. The community probation team need to update nDelius."
+      msg = "There’s an nDelius record with NOMIS number #{prisoner.offender_no} - #{prisoner.full_name} but a "\
+            'different date of birth. The community probation team need to update nDelius.'
     else
-      msg = "#{prisoner.full_name}, NOMIS number #{prisoner.offender_no} must be linked to an nDelius record for handover to the community. The community probation team need to update nDelius."
+      msg = "#{prisoner.full_name}, NOMIS number #{prisoner.offender_no} must be linked to an nDelius record for "\
+            'handover to the community. The community probation team need to update nDelius.'
     end
 
-    msg + " Automatic email sent."
+    msg + ' Automatic email sent.'
   end
+  # rubocop:enable Metrics/MethodLength
 
-  def flash_alert_text(me, ldu, team_name)
+  def flash_alert_text(spo:, ldu:, team_name:)
     msg = ''
 
     if ldu.nil?
-      msg = msg + "An email could not be sent to the LDU for #{team_name} because there is no LDU assigned to the team. "
+      msg += "An email could not be sent to the LDU for #{team_name} because there is no LDU assigned to the team. "
     elsif ldu.email_address.nil?
-      msg = msg + "An email could not be sent to the community probation team - #{ldu.name} because there is no email address saved. You need to find an alternative way to contact them. "
+      msg += "An email could not be sent to the community probation team - #{ldu.name} because there is no "\
+      'email address saved. You need to find an alternative way to contact them. '
     end
 
-    if me.nil?
-      msg = msg + "We could not send you an email because there is no valid email address saved to your account. You need to contact the local system administrator in your prison to update your email address."
+    if spo.nil?
+      msg += 'We could not send you an email because there is no valid email address saved to your account. '\
+      'You need to contact the local system administrator in your prison to update your email address.'
     end
 
     msg
@@ -64,29 +73,26 @@ private
 
   ERROR_MESSAGES = {
     DeliusImportError::DUPLICATE_NOMIS_ID =>
-      'More than one nDelius record found with this prisoner number.
-         You need to update nDelius so there is only one record before you can allocate.',
+      'More than one nDelius record found with this prisoner number. '\
+      'You need to update nDelius so there is only one record before you can allocate.',
     DeliusImportError::INVALID_TIER =>
-      'nDelius record with matching prisoner number but no tiering
-         calculation found. You need to update nDelius with the tiering calculation
-         before you can allocate.',
+      'nDelius record with matching prisoner number but no tiering calculation found. '\
+      'You need to update nDelius with the tiering calculation before you can allocate.',
     DeliusImportError::INVALID_CASE_ALLOCATION =>
-      'nDelius record with matching prisoner number but no service provider
-         information found. You need to update nDelius with the service provider
-         before you can allocate.',
+      'nDelius record with matching prisoner number but no service provider information found. '\
+      'You need to update nDelius with the service provider before you can allocate.',
     DeliusImportError::MISSING_DELIUS_RECORD =>
-      'No nDelius record found with this prisoner number. This may be because
-      the case information has not yet been updated. This prisoner needs to be
-      matched with an nDelius record before you can allocate.',
-    DeliusImportError::MISSING_LDU => 'nDelius record with matching prisoner number
-       but no local divisional unit (LDU) information found. You need to update nDelius
-       with the LDU before you can allocate.',
-    DeliusImportError::MISSING_TEAM => 'nDelius record found with matching prisoner
-       number but no community team information found. You need to update nDelius with the
-       team information before you can allocate.',
-    DeliusImportError::MISMATCHED_DOB => 'nDelius record found with matching prisoner
-       number but a different date of birth. You need to check the data in nDelius and
-       correct before you can allocate.'
+      'No nDelius record found with this prisoner number. This may be because the case information has not yet '\
+      'been updated. This prisoner needs to be matched with an nDelius record before you can allocate.',
+    DeliusImportError::MISSING_LDU =>
+      'nDelius record with matching prisoner number but no local divisional unit (LDU) information found. '\
+      'You need to update nDelius with the LDU before you can allocate.',
+    DeliusImportError::MISSING_TEAM =>
+      'nDelius record found with matching prisoner number but no community team information found. '\
+      'You need to update nDelius with the team information before you can allocate.',
+    DeliusImportError::MISMATCHED_DOB =>
+      'nDelius record found with matching prisoner number but a different date of birth. '\
+      'You need to check the data in nDelius and correct before you can allocate.'
   }.freeze
 
   EMAIL_MESSAGES = {
