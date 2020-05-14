492013fe4759d8a0e9e8a5190e69a1c5388e9773
Maysa <maysa@madetech.com>
2020-05-14 11:58:59 +0100

    Update shadow codes for teams
    This commit builds upon the Delius import job to allow our service to
    automatically 'learn' about shadow team codes.
    
    Our modelling of active and shadow teams is slightly different from the
    way they're modelled in Delius.
    
    In Delius, two types of team exist:
    - Active teams
    - Shadow teams
    
    They're differentiated by the LDU they belong to – if a team's LDU code
    looks like `N01OMIC` (where `01` is any 2 digit number), then it's a
    shadow team. Otherwise, it's an active team.
    
    In our application, however, there's only one type of team. Instead,
    teams have both an 'active' code (field: `code`), and a 'shadow' code
    (field: `shadow_code`).
    
    Delius has no concept of relationships between teams, however there's a
    convention which dictates that shadow teams share the same name as their
    active counterpart, but with 'OMIC' prepended.
    
    Knowledge of the relationship between 'active' and 'shadow' teams is
    something that this service is responsible for.
    
    This commit introduces a new service object called
    `UpdateShadowTeamAssociation`. This new service receives a Delius
    'shadow' team name and code, and attempts to locate the associated
    'active' team in our database. The lookup is performed based on the
    aforementioned shadow team naming convention, which should allow us to
    locate the active team.
    
    If it finds a team with the target name, it'll update that team's shadow
    code to reflect the one seen in the Delius import.
    
    In essence, this will allow us to automatically learn about shadow team
    codes provided we already have the active team in our database.

---
+++ spec/services/update_shadow_team_association_service_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe UpdateShadowTeamAssociationService do
+  let(:team_name) { 'NPS - Team 1' }
+  let(:team_shadow_code) { nil }
+  let!(:team) { create(:team, name: team_name, shadow_code: team_shadow_code) }
+
+  context "when the team doesn't have a shadow code" do
+    it "sets the team's shadow code" do
+      described_class.update(shadow_code: 'SHAD01', shadow_name: 'OMIC NPS - Team 1')
+      expect(team.reload.shadow_code).to eq('SHAD01')
+    end
+  end
+
+  context "when the team has a different shadow code" do
+    let(:team_shadow_code) { 'A_DIFFERENT_SHADOW_CODE' }
+
+    it "updates the team's shadow code" do
+      described_class.update(shadow_code: 'SHAD01', shadow_name: 'OMIC NPS - Team 1')
+      expect(team.reload.shadow_code).to eq('SHAD01')
+    end
+  end
+
+  context "when the team already has the correct shadow code" do
+    let(:team_shadow_code) { 'SHAD01' }
+
+    it "doesn't update the team record" do
+      expect_any_instance_of(Team).not_to receive(:save)
+      described_class.update(shadow_code: 'SHAD01', shadow_name: 'OMIC NPS - Team 1')
+    end
+  end
+
+  describe "shadow to active team name mappings" do
+    example_team_names = {
+      # "Shadow name" => "Active name"
+      "OMIC Some Team" => "Some Team",
+      "OMIC - NPS - Team 1" => "NPS - Team 1",
+      "OMiC - Another team" => "Another team",
+      "omic lowercase team" => "lowercase team"
+    }
+
+    example_team_names.each_with_index do |(shadow_name, active_name), index|
+      context "shadow team '#{shadow_name}'" do
+        let(:team_name) { active_name }
+
+        it "maps to active team '#{active_name}'" do
+          shadow_code = "SC#{index}"
+          described_class.update(shadow_code: shadow_code, shadow_name: shadow_name)
+          expect(team.reload.shadow_code).to eq(shadow_code)
+        end
+      end
+    end
+  end
+
+  context "when the shadow name doesn't begin with 'OMIC'" do
+    it "logs an error" do
+      bad_shadow_name = 'Not a shadow team name'
+      expect_message = "This doesn't look like a shadow team name: '#{bad_shadow_name}'"
+      expect(Rails.logger).to receive(:error).with(expect_message)
+      described_class.update(shadow_code: 'SHAD01', shadow_name: bad_shadow_name)
+    end
+  end
+
+  context "when no active team exists with a matching name" do
+    let(:team_name) { 'Some other team name' }
+
+    it "logs an error" do
+      expect_message = "Couldn't find a team named 'Team 1' to associate with shadow team 'OMIC Team 1'"
+      expect(Rails.logger).to receive(:error).with(expect_message)
+      described_class.update(shadow_code: 'SHAD01', shadow_name: 'OMIC Team 1')
+    end
+  end
+end
