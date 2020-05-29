9a55ead035f6f51764da9b9980c58ff7944951ee
Stephen Dicks <stephen@starswan.com>
2020-05-29 12:19:23 +0100

    Enabled ImplicitExpect cop

--- spec/models/responsibility_spec.rb
+++ spec/models/responsibility_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe Responsibility, type: :model do
   context 'with other reason' do
     subject { build(:responsibility, reason: :other_reason) }
 
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 
   context 'with default factory' do
     subject { build(:responsibility) }
 
-    it { should be_valid }
+    it { is_expected.to be_valid }
   end
 
   context 'with invalid override' do
     subject { build(:responsibility, value: 'wibble') }
 
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 
   context 'with prison override' do
     subject { build(:responsibility, value: 'Prison') }
 
-    it { should be_valid }
+    it { is_expected.to be_valid }
   end
 end
