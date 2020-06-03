b1e96d5308c6e5b21027702e8fc4d34423338dc3
Stephen Dicks <stephen@starswan.com>
2020-06-03 09:19:34 +0100

    added missing test shown up by code change

--- spec/factories/offenders.rb
+++ spec/factories/offenders.rb
@@ -2,10 +2,8 @@ FactoryBot.define do
   factory :offender, class: 'Nomis::Offender' do
     inprisonment_status do 'SENT03' end
     sentence do
-      Nomis::SentenceDetail.new.tap { |s|
-        s.automatic_release_date = Time.zone.today + 1.year
-        s.sentence_start_date = Time.zone.today
-      }
+      Nomis::SentenceDetail.new automatic_release_date: Time.zone.today + 1.year,
+                                sentence_start_date: Time.zone.today
     end
     prison_id { 'LEI' }
     case_allocation { 'NPS' }
