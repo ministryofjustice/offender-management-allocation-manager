04451486f79e990fa6ea53ec7adc0966b1d226c3
Stephen Dicks <stephen@starswan.com>
2020-06-19 07:57:26 +0100

    enable rspec -n to step through test failures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.use_transactional_fixtures = false
+  config.example_status_persistence_file_path = 'tmp/example_status.txt'
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
