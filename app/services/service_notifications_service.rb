7f649e9f7dc9ab8f0cb1a9d7b522db14ec359c5d
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2020-07-29 17:11:29 +0100

    Create a service class to read in the service_notifications yaml file and return an array of notifications to be shown today based on the users role.

---
+++ app/services/service_notifications_service.rb
@@ -0,0 +1,39 @@
+#frozen_string_literal: true
+
+class ServiceNotificationsService
+  def self.notifications(user_roles)
+    return [] if user_roles.empty?
+
+    data = YAML.load(File.read('app/data/service_notifications.yaml'))
+    notification_list = data['notifications']
+    return [] unless notification_list.is_a?(Array)
+
+    role_notifications = alerts_by_role(notification_list, user_roles)
+    alerts_for_today(role_notifications)
+  end
+
+private
+
+  def self.alerts_by_role(notifications, user_roles)
+    alerts = []
+
+    notifications.each do |alert|
+      alerts << alert if user_roles.any? { |role| alert['role'].include?(role) }
+    end
+
+    alerts
+  end
+
+  def self.alerts_for_today(notifications)
+    alerts = []
+
+    notifications.each do |alert|
+      today = Time.zone.now.to_date
+      start_date = alert['start_date'].to_date
+      end_date = start_date + alert['duration'].days
+      alerts << alert if today >= start_date && today <= end_date
+    end
+
+    alerts
+  end
+end
---
+++ spec/services/service_notifications_service_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+describe ServiceNotificationsService do
+  describe 'notifications' do
+    let(:pom_role) { ['POM'] }
+    let(:spo_role) { ['SPO'] }
+    let(:both_roles) { %w(SPO POM) }
+    let(:filename) { 'app/data/service_notifications.yaml' }
+
+    it 'returns an empty array if file is empty' do
+      allow(YAML).to receive(:load).with(File.read(filename)).and_return('')
+      expect(described_class.notifications(pom_role)).to eq([])
+    end
+
+    it 'returns an empty array if YAML does not return an array' do
+      allow(YAML).to receive(:load).with(File.read(filename)).and_return('{"notifications"=>nil}')
+      expect(described_class.notifications(both_roles)).to eq([])
+    end
+
+    it 'returns an empty array if there are no roles' do
+      roles = []
+      start_date = 2.days.ago.strftime("%d/%m/%Y")
+      data =
+        {
+          "notifications" => [
+            {
+              "id" => "my-new-feature",
+              "start_date" => start_date,
+              "role" => ["POM"],
+              "duration" => 14,
+              "text" => "This is a test banner"
+            }
+          ]
+        }
+
+      allow(YAML).to receive(:load).with(File.read(filename)).and_return(data)
+      expect(described_class.notifications(roles)).to eq([])
+    end
+
+    it 'returns an empty array if there are no notifications that match the users role' do
+      start_date = 4.days.ago.strftime("%d/%m/%Y")
+      data =
+        {
+          "notifications" => [
+            {
+              "id" => "my-new-feature",
+              "start_date" => start_date,
+              "role" => ["POM"],
+              "duration" => 14,
+              "text" => "This is a test banner"
+            }
+          ]
+        }
+
+      allow(YAML).to receive(:load).with(File.read(filename)).and_return(data)
+      expect(described_class.notifications(spo_role)).to eq([])
+    end
+
+    it 'returns an empty array if there are no notifications for today' do
+      start_date = 2.months.ago.strftime("%d/%m/%Y")
+      data =
+        {
+          "notifications" => [
+            {
+              "id" => "my-new-feature",
+              "start_date" => start_date,
+              "role" => ["POM"],
+              "duration" => 30,
+              "text" => "This is a test banner"
+            }
+          ]
+        }
+
+      allow(YAML).to receive(:load).with(File.read(filename)).and_return(data)
+      expect(described_class.notifications(pom_role)).to eq([])
+    end
+
+    it 'returns notifications for roles that should be shown today' do
+      start_dates = [
+        3.days.ago.strftime("%d/%m/%Y"),
+        2.weeks.ago.strftime("%d/%m/%Y"),
+        5.months.ago.strftime("%d/%m/%Y")
+      ]
+      data =
+        {
+          "notifications" => [
+            {
+              "id" => "new-spo-feature",
+              "start_date" => start_dates.first,
+              "role" => ["SPO"],
+              "duration" => 14,
+              "text" => "New SPO feature"
+            },
+            {
+              "id" => "new-app-feature",
+              "start_date" => start_dates[1],
+              "role" => ["POM", "SPO"],
+              "duration" => 30,
+              "text" => "New design layout"
+            },
+            {
+              "id" => "old-feature",
+              "start_date" => start_dates.last,
+              "role" => ["POM", "SPO"],
+              "duration" => 30,
+              "text" => "An old feature"
+            }
+          ]
+        }
+
+      allow(YAML).to receive(:load).with(File.read(filename)).and_return(data)
+      expect(described_class.notifications(both_roles)).to eq(data['notifications'].take(2))
+    end
+  end
+end
