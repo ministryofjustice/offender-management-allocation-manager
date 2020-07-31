73033e90690dd9a72b1923a8b0cd252a25f59aa1
Stephen Dicks <stephen@starswan.com>
2020-07-31 08:59:05 +0100

    faster implementation

--- spec/factories/prisons.rb
+++ spec/factories/prisons.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
+  CLOSED_PRISON_CODES = PrisonService.prison_codes.reject { |x|
+    PrisonService::OPEN_PRISON_CODES.include?(x) || PrisonService::ENGLISH_HUB_PRISON_CODES.include?(x)
+  }
   class Elite2Prison
     attr_accessor :code, :name
   end
@@ -6,8 +9,7 @@ FactoryBot.define do
   factory :prison, class: 'Elite2Prison' do
     # rotate around every closed prison - open prisons have different rules
     sequence(:code) do |c|
-      codes = PrisonService.prison_codes.reject { |x| PrisonService::OPEN_PRISON_CODES.include?(x) }
-      codes[c % codes.size]
+      CLOSED_PRISON_CODES[c % CLOSED_PRISON_CODES.size]
     end
 
     name { PrisonService.name_for(code) }
