df7a44debe148a2edc4f54494921d99bcbba5e84
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2020-09-16 15:07:33 +0100

    Modify view
    Their is a fix in the view that displays incorrect data  due to a bug in the Override Controller. Unfortunately this bad data can not be altered. To get around this it has been modified at the view level.

--- spec/views/allocations/allocation_history.html.erb_spec.rb
+++ spec/views/allocations/allocation_history.html.erb_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe "allocations/history", type: :view do
       expect(page.css('#override-reason-reallocation').text).to include 'Continuity'
     end
   end
+
+  # Their is a fix in this view to display incorrect data correctly due to a bug created in the Override Controller. Unfortunately this bad data
+  # can not be altered so to get around this it has been modified at the view level.
+  context 'when a prisoner has moved to another prison' do
+    before do
+      assign(:prisoner, build(:offender))
+      assign(:pom_emails, {})
+      assign(:history, [build(:allocation, :primary, prison: prison_one),
+                        build(:allocation, :transfer, prison: prison_one),
+                        build(:allocation, :reallocation, :override, prison: prison_two)])
+    end
+
+    let(:prison_one) { build(:prison).code }
+    let(:prison_two) { build(:prison).code }
+    let(:page) { Nokogiri::HTML(rendered) }
+
+    it 'displays an allocation label in the allocation history' do
+      render
+      expect(page.css(".moj-timeline__title").map(&:text).map(&:strip)).to eq ["Prisoner allocated", "Prisoner unallocated", "Prisoner allocated"]
+    end
+  end
 end
