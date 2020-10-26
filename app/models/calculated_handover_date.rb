f75dae61e2d762c99ea02bf565edbfae1cb1db9d
Ollie Treend <ollie@treend.com>
2020-10-26 15:05:19 +0000

    Push handover dates to nDelius when they change
    This commit adds an after_save callback to the `CalculatedHandoverDate`
    model which will push the handover dates into nDelius whenever they're
    changed.
    
    This will allow us to run the `#recalculate_for(offender)` method and
    have dates pushed automatically.
    
    Because it only pushes when the dates have changed, it'll avoid us
    needing to re-push the same dates multiple times into nDelius.

--- app/models/calculated_handover_date.rb
+++ app/models/calculated_handover_date.rb
@@ -13,12 +13,27 @@ class CalculatedHandoverDate < ApplicationRecord
   validates :nomis_offender_id, uniqueness: true, presence: true
   validates :reason, presence: true
 
+  after_save :push_to_delius
+
   def self.recalculate_for(offender)
     record = self.find_or_initialize_by(nomis_offender_id: offender.offender_no)
     record.update!(
       start_date: offender.handover_start_date,
       handover_date: offender.responsibility_handover_date,
       reason: offender.handover_reason
+    )
+  end
+
+private
+
+  def push_to_delius
+    # Only push if the dates have changed
+    return unless saved_change_to_start_date? || saved_change_to_handover_date?
+
+    HmppsApi::CommunityApi.set_handover_dates(
+      offender_no: nomis_offender_id,
+      handover_start_date: start_date,
+      responsibility_handover_date: handover_date
     )
   end
 end
