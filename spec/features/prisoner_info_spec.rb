9f26748a9f86714ece5fc276b72a30e7d69fa357
Stephen Dicks <stephen@starswan.com>
2020-12-04 09:04:43 +0000

    tweak test for readability

--- spec/features/prisoner_info_spec.rb
+++ spec/features/prisoner_info_spec.rb
@@ -20,11 +20,10 @@ feature 'View a prisoner profile page', :allocation do
   context 'with an allocation' do
     before do
       create(:case_information, nomis_offender_id: 'G7998GJ', victim_liaison_officers: [build(:victim_liaison_officer)])
+      create(:allocation, nomis_offender_id: 'G7998GJ', primary_pom_nomis_id: '485637', primary_pom_name: 'Pobno, Kath')
     end
 
-    let!(:alloc) {
-      create(:allocation, nomis_offender_id: 'G7998GJ', primary_pom_nomis_id: '485637', primary_pom_name: 'Pobno, Kath')
-    }
+    let(:alloc) { Allocation.last }
 
     let(:initial_vlo) { VictimLiaisonOfficer.last }
 
