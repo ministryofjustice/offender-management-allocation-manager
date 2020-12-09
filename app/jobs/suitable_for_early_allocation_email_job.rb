411d4de9378421235755139e02572e1588472f83
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2020-12-09 16:49:05 +0000

    Create Mailer Job that identifies all previously completed 'eligible' and 'discretionary' Early Allocation forms completed for offenders due to be released in exactly 18 months time and send an email to the POM.

---
+++ app/jobs/suitable_for_early_allocation_email_job.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class SuitableForEarlyAllocationEmailJob < ApplicationJob
+  queue_as :mailer
+
+  EQUIP_URL = 'https://equip-portal.rocstac.com/CtrlWebIsapi.dll/?__id=webDiagram.show&map=0%3A9A63E167DE4B400EA07F81A9271E1944&dgm=4F984B45CBC447B1A304B2FFECABB777'
+
+  def perform
+    offenders = EarlyAllocation.where(created_within_referral_window: false).where.not(outcome: 'ineligible').pluck(:nomis_offender_id).uniq
+
+    offenders.each do |offender_no|
+      allocation = Allocation.where(nomis_offender_id: offender_no).first
+      next if allocation.nil?
+
+      prisoner = OffenderService.get_offender(offender_no)
+      if !prisoner.nil? && eligible_today?(prisoner)
+        pom = PrisonOffenderManagerService.get_pom_at(prisoner.prison_id, allocation.primary_pom_nomis_id)
+        EarlyAllocationMailer.review_early_allocation(
+          email: pom.email_address,
+          prisoner_name: prisoner.full_name,
+          start_page_link: Rails.application.routes.url_helpers.prison_prisoner_early_allocations_path(
+            prison_id: prisoner.prison_id,
+            prisoner_id: prisoner.offender_no),
+          equip_guidance_link: EQUIP_URL).deliver_now
+      end
+    end
+  end
+
+private
+
+  def eligible_today?(offender)
+    [
+      offender.tariff_date,
+      offender.parole_eligibility_date,
+      offender.parole_review_date,
+      offender.automatic_release_date,
+      offender.conditional_release_date
+    ].compact.min == Time.zone.today + 18.months
+  end
+end
