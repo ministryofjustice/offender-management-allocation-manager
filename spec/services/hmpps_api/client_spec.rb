d12bbc1a0184d303d98294b350d028c5deda18d2
Ollie Treend <ollie@treend.com>
2020-12-15 15:35:58 +0000

    Add caching to the HMPPS API client
    This commit adds support for caching responses from any of the HMPPS API
    clients. GET requests will be cached automatically. Optionally, POST
    requests can also be cached (not cached by default).
    
    API responses are cached in the Rails cache store with a 60 minute TTL.
    
    All GET requests will be cached automatically:
    
    ```
    client.get('/some/endpoint')
    ```
    
    Caching can be disabled with:
    
    ```
    client.get('/some/endpoint', cache: false)
    ```
    
    POST requests are *not* cached by default, but caching can be enabled:
    
    ```
    client.post('/some/endpoint', request_body, cache: true)
    ```
    
    **Note:** Caching POST requests is not advisable. However for some
    non-restful API endpoints (in particular with the Prison API), it is
    appropriate to cache them because they're essentially GET requests
    disguised as POSTs.

--- spec/services/hmpps_api/client_spec.rb
+++ spec/services/hmpps_api/client_spec.rb
@@ -114,6 +114,36 @@ describe HmppsApi::Client do
       end
 
       include_examples 'handles JSON response'
+
+      describe 'request caching' do
+        let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+
+        before do
+          # Set up a cache store for these tests
+          allow(Rails).to receive(:cache).and_return(memory_store)
+          Rails.cache.clear
+          WebMock.reset_executed_requests!
+        end
+
+        it 'caches responses by default' do
+          5.times { client.get(route) }
+          expect(a_request(:get, stub_url)).to have_been_made.once
+        end
+
+        context 'with cache: true' do
+          it 'caches responses' do
+            5.times { client.get(route, cache: true) }
+            expect(a_request(:get, stub_url)).to have_been_made.once
+          end
+        end
+
+        context 'with cache: false' do
+          it 'does not cache responses' do
+            5.times { client.get(route, cache: false) }
+            expect(a_request(:get, stub_url)).to have_been_made.times(5)
+          end
+        end
+      end
     end
 
     describe '#post' do
@@ -146,6 +176,36 @@ describe HmppsApi::Client do
       end
 
       include_examples 'handles JSON response'
+
+      describe 'request caching' do
+        let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+
+        before do
+          # Set up a cache store for these tests
+          allow(Rails).to receive(:cache).and_return(memory_store)
+          Rails.cache.clear
+          WebMock.reset_executed_requests!
+        end
+
+        it 'does not cache responses by default' do
+          5.times { client.post(route, request_body) }
+          expect(a_request(:post, stub_url)).to have_been_made.times(5)
+        end
+
+        context 'with cache: true' do
+          it 'caches responses' do
+            5.times { client.post(route, request_body, cache: true) }
+            expect(a_request(:post, stub_url)).to have_been_made.once
+          end
+        end
+
+        context 'with cache: false' do
+          it 'does not cache responses' do
+            5.times { client.post(route, request_body, cache: false) }
+            expect(a_request(:post, stub_url)).to have_been_made.times(5)
+          end
+        end
+      end
     end
 
     describe '#put' do
@@ -191,6 +251,116 @@ describe HmppsApi::Client do
       it 'performs an authenticated DELETE request' do
         expect(WebMock).to have_requested(:delete, stub_url).
           with(auth_header)
+      end
+    end
+  end
+
+  describe 'request caching' do
+    let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+
+    let(:base_request) {
+      {
+        root: 'https://example.com',
+        route: '/some/endpoint',
+        queryparams: {},
+        extra_headers: {},
+        body: nil,
+        # HTTP methods to test caching for – we support caching both GET and POST requests
+        methods: [:get, :post],
+      }
+    }
+
+    # Helper to send a request
+    def send_request(req)
+      client = described_class.new(req[:root])
+
+      if req[:method] == :get
+        client.get(req[:route], queryparams: req[:queryparams], extra_headers: req[:extra_headers])
+      else
+        client.post(req[:route], req[:body],
+                    queryparams: req[:queryparams], extra_headers: req[:extra_headers], cache: true)
+      end
+    end
+
+    before do
+      # Set up a cache store for these tests
+      allow(Rails).to receive(:cache).and_return(memory_store)
+      Rails.cache.clear
+
+      # Stub all HTTP requests and return a unique response for each
+      counter = 0
+      WebMock.stub_request(:any, /.*/)
+             .to_return { |_req| { body: ["Response #{counter += 1}"].to_json } }
+
+      # Warm up the cache by sending each request once
+      requests.each do |req|
+        send_request(req)
+      end
+    end
+
+    context 'when the requests are different' do
+      # An array of requests to send – each one differing slightly
+      let(:requests) do
+        requests = [
+          # A base request
+          base_request,
+
+          # Change the Root URL (e.g. API hostname)
+          base_request.merge(root: 'https://some-other-hostname.com'),
+
+          # Change the route (e.g. API endpoint)
+          base_request.merge(route: '/a/different/endpoint'),
+
+          # Change the query string parameters
+          base_request.merge(queryparams: { movementTypes: %w[one two] }),
+
+          # Send additional headers
+          base_request.merge(extra_headers: { 'Page-Limit' => 100 }),
+
+          # Send something in the request body – this is only supported for POST requests
+          base_request.merge(body: ['a json array', 'in the request body'], methods: [:post]),
+        ]
+
+        # Make one request for each HTTP method (GET, POST)
+        get_requests = requests.select { |req| req[:methods].include?(:get) }.map { |req| req.merge(method: :get) }
+        post_requests = requests.select { |req| req[:methods].include?(:post) }.map { |req| req.merge(method: :post) }
+
+        get_requests + post_requests
+      end
+
+      it 'caches them independently' do
+        # Perform each request and keep the response
+        responses = requests.map do |req|
+          send_request(req)
+        end
+
+        expected_responses = (1..requests.length).map { |n| ["Response #{n}"] }
+
+        # Expect: Response 1, Response 2, Response 3, etc...
+        expect(responses).to eq(expected_responses)
+
+        # Unique responses mean each request was cached independently - they didn't share a cache
+        expect(responses.count).to eq(responses.uniq.count)
+      end
+    end
+
+    context 'when the requests are the same' do
+      # An array of 5 identical GET requests
+      let(:requests) do
+        [base_request.merge(method: :get)] * 5
+      end
+
+      it 'reuses the existing cache' do
+        # Perform each request and keep the response
+        responses = requests.map do |req|
+          send_request(req)
+        end
+
+        # Expect cached "Response 1" to be returned 5 times
+        expect(responses).to eq([['Response 1']] * 5)
+
+        # Expect only 1 unique response – subsequent requests get the cached response
+        expect(responses.uniq.count).to eq(1)
       end
     end
   end
