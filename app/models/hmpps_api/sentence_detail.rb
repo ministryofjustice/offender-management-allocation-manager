7691fb46975d11f94e465039dd0eabc213424234
Stephen Dicks <stephen@starswan.com>
2020-12-16 18:39:20 +0000

    performance - lazy-parse dates

--- app/models/hmpps_api/sentence_detail.rb
+++ app/models/hmpps_api/sentence_detail.rb
@@ -4,42 +4,29 @@ module HmppsApi
   class SentenceDetail
     include Deserialisable
 
-    attr_reader :home_detention_curfew_eligibility_date,
-                :home_detention_curfew_actual_date,
-                :parole_eligibility_date,
-                :release_date,
-                :licence_expiry_date,
-                :sentence_start_date,
-                :tariff_date,
-                :actual_parole_date
-
-    # Note - this is hiding a defect - we never get sentence_expiry_date from NOMIS (but maybe we should?)
+    # Note - this is hiding a defect - we never get sentence_expiry_date from NOMIS (maybe we should?)
     attr_accessor :sentence_expiry_date
 
     def automatic_release_date
-      @automatic_release_override_date.presence || @automatic_release_date
+      automatic_release_override_date.presence || nomis_automatic_release_date
     end
 
     def conditional_release_date
-      @conditional_release_override_date.presence || @conditional_release_date
+      conditional_release_override_date.presence || nomis_conditional_release_date
     end
 
     def post_recall_release_date
       post_recall_release_date = nomis_post_recall_release_date
 
-      return post_recall_release_date if @actual_parole_date.blank?
+      return post_recall_release_date if actual_parole_date.blank?
 
-      return @actual_parole_date if post_recall_release_date.blank?
+      return actual_parole_date if post_recall_release_date.blank?
 
-      if @actual_parole_date.before?(post_recall_release_date)
-        @actual_parole_date
+      if actual_parole_date.before?(post_recall_release_date)
+        actual_parole_date
       else
         post_recall_release_date
       end
-    end
-
-    def nomis_post_recall_release_date
-      @nomis_post_recall_release_override_date.presence || @nomis_post_recall_release_date
     end
 
     def earliest_release_date
@@ -70,31 +57,70 @@ module HmppsApi
     end
 
     def initialize(payload)
-      @parole_eligibility_date = deserialise_date(payload, 'paroleEligibilityDate')
-      @release_date = deserialise_date(payload, 'releaseDate')
-      @sentence_start_date = deserialise_date(payload, 'sentenceStartDate')
-      @tariff_date = deserialise_date(payload, 'tariffDate')
-      @automatic_release_date = deserialise_date(payload, 'automaticReleaseDate')
-      @nomis_post_recall_release_date = deserialise_date(payload, 'postRecallReleaseDate')
-      @nomis_post_recall_release_override_date = deserialise_date(payload, 'postRecallReleaseOverrideDate')
+      @payload = payload
+    end
 
-      @conditional_release_date = deserialise_date(
-        payload, 'conditionalReleaseDate'
-      )
-      @automatic_release_override_date = deserialise_date(
-        payload, 'automaticReleaseOverrideDate'
-      )
-      @home_detention_curfew_eligibility_date = deserialise_date(
-        payload, 'homeDetentionCurfewEligibilityDate'
-      )
-      @home_detention_curfew_actual_date = deserialise_date(
-        payload, 'homeDetentionCurfewActualDate'
-      )
-      @conditional_release_override_date = deserialise_date(
-        payload, 'conditionalReleaseOverrideDate'
-      )
-      @actual_parole_date = deserialise_date(payload, 'actualParoleDate')
-      @licence_expiry_date = deserialise_date(payload, 'licenceExpiryDate')
+    def home_detention_curfew_eligibility_date
+      @home_detention_curfew_eligibility_date ||= deserialise_date(@payload, 'homeDetentionCurfewEligibilityDate')
+    end
+
+    def home_detention_curfew_actual_date
+      @home_detention_curfew_actual_date ||= deserialise_date(@payload, 'homeDetentionCurfewActualDate')
+    end
+
+    def parole_eligibility_date
+      @parole_eligibility_date ||= deserialise_date(@payload, 'paroleEligibilityDate')
+    end
+
+    def release_date
+      @release_date ||= deserialise_date(@payload, 'releaseDate')
+    end
+
+    def licence_expiry_date
+      @licence_expiry_date ||= deserialise_date(@payload, 'licenceExpiryDate')
+    end
+
+    def sentence_start_date
+      @sentence_start_date ||= deserialise_date(@payload, 'sentenceStartDate')
+    end
+
+    def tariff_date
+      @tariff_date ||= deserialise_date(@payload, 'tariffDate')
+    end
+
+    def actual_parole_date
+      @actual_parole_date ||= deserialise_date(@payload, 'actualParoleDate')
+    end
+
+    # This has a test, but shouldn't be public - no-one uses it apart from the test
+    def nomis_post_recall_release_date
+      parse_nomis_post_recall_release_override_date.presence || parse_nomis_post_recall_release_date
+    end
+
+    private
+
+    def nomis_automatic_release_date
+      @automatic_release_date ||= deserialise_date(@payload, 'automaticReleaseDate')
+    end
+
+    def automatic_release_override_date
+      @automatic_release_override_date ||= deserialise_date(@payload, 'automaticReleaseOverrideDate')
+    end
+
+    def nomis_conditional_release_date
+      @conditional_release_date ||= deserialise_date(@payload, 'conditionalReleaseDate')
+    end
+
+    def conditional_release_override_date
+      @conditional_release_override_date ||= deserialise_date(@payload, 'conditionalReleaseOverrideDate')
+    end
+
+    def parse_nomis_post_recall_release_override_date
+      @nomis_post_recall_release_override_date ||= deserialise_date(@payload, 'postRecallReleaseOverrideDate')
+    end
+
+    def parse_nomis_post_recall_release_date
+      @nomis_post_recall_release_date ||= deserialise_date(@payload, 'postRecallReleaseDate')
     end
   end
 end
