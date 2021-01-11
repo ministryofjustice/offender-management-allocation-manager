bb55d76ef2f73b95ce294f8bf3940f3384ad7a2b
Stephen Dicks <stephen@starswan.com>
2021-01-11 09:28:52 +0000

    try to fix sentry silence

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,7 @@
 sentry_dsn = Rails.configuration.sentry_dsn
 Sentry.init do |config|
+  config.send_default_pii = true
+  config.release = `git branch --show-current`
   config.breadcrumbs_logger = [:active_support_logger]
   if sentry_dsn
     config.dsn = sentry_dsn
@@ -12,11 +14,11 @@ Sentry.init do |config|
   end
 
   # Filter out sensitive fields from sentry logs
-  sanitize_fields = Rails.application.config.filter_parameters
-  config.before_send = lambda do |event|
-    sanitize_fields.each do |field|
-      event.remove(field)
-    end
-    event
-  end
+  #sanitize_fields = Rails.application.config.filter_parameters
+  #config.before_send = lambda do |event|
+  #  sanitize_fields.each do |field|
+  #    event.remove(field)
+  #  end
+  #  event
+  #end
 end
