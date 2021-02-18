3c9e12d285447752a5862df8e4c5d0a095ce5608
Ollie Treend <ollie@treend.com>
2021-02-18 14:49:10 +0000

    Ask the LDU for a Supporting COM in Open Prison
    This commit extends the existing functionality implemented in MO-70
    which emails the LDUs of offenders moving into an open prison.
    
    Currently, OMIC rules apply to Welsh offenders in HMP Prescoed only –
    this is the pilot prison. This commit extends the functionality to apply
    to any offender which falls under OMIC open prison rules.
    
    This has been implemented under the following assumptions:
    
    - COM is responsible. POM is supporting.
    - Send an email to the LDU asking for a Responsible COM to be allocated.
    - This currently applies to all open prisons (with the exception of the
      HMP Prescoed pilot) because OMIC rules haven't rolled out yet.
    
    - POM is responsible. COM is supporting.
    - Send an email to the LDU asking for a Supporting COM to be allocated,
      because OMIC rules apply and a COM is needed.
    - This currently applies to Welsh offenders in HMP Prescoed (open
      prisons pilot)
    - This will apply to all open prisons once the OMIC rules have launched
      because handover/responsibility calculations will change to apply the
      new rules.
    
    - POM is responsible. COM is not needed yet.
    - Do not email the LDU.
    - Currently this only applies to the HMP Prescoed pilot, but will apply
      to all open prisons once OMIC rules apply.

--- spec/jobs/open_prison_transfer_job_spec.rb
+++ spec/jobs/open_prison_transfer_job_spec.rb
@@ -4,12 +4,27 @@ RSpec.describe OpenPrisonTransferJob, type: :job do
   include ActiveJob::TestHelper
 
   let(:nomis_offender_id) { 'G3462VT' }
-  let(:nomis_offender) { build(:nomis_offender, offenderNo: nomis_offender_id, latestLocationId: open_prison_code) }
+  let(:determinate_nomis_offender) {
+    build(:nomis_offender, offenderNo: nomis_offender_id,
+          latestLocationId: open_prison_code,
+          sentence: attributes_for(:sentence_detail, :handover_in_8_days))
+  }
+  let(:indeterminate_nomis_offender) {
+    build(:nomis_offender, :indeterminate, offenderNo: nomis_offender_id,
+          latestLocationId: open_prison_code)
+  }
+  # Default to a Determinate offender – change by setting this to `indeterminate_nomis_offender`
+  let(:nomis_offender) { determinate_nomis_offender }
   let(:offender) { OffenderService.get_offender(nomis_offender_id) }
+
   let(:nomis_staff_id) { 485_637 }
   let(:other_staff_id) { 485_636 }
-  let(:open_prison_code) { 'HDI' }
+
   let(:closed_prison_code) { 'LEI' }
+  let(:closed_prison_name) { PrisonService.name_for(closed_prison_code) }
+  let(:open_prison_code) { 'HDI' }
+  let(:open_prison_name) { PrisonService.name_for(open_prison_code) }
+
   let(:poms) {
     [build(:pom,
            staffId: nomis_staff_id,
@@ -31,6 +46,13 @@ RSpec.describe OpenPrisonTransferJob, type: :job do
     }.stringify_keys
   }
   let(:movement_json) { HmppsApi::Movement.from_json(movement).to_json }
+
+  # Set the current date by changing the value of `today`
+  let(:today) { Time.zone.today }
+
+  before { Timecop.travel(today) }
+
+  after { Timecop.return }
 
   before do
     stub_auth_token
@@ -80,109 +102,209 @@ RSpec.describe OpenPrisonTransferJob, type: :job do
     end
   end
 
-  context 'when there is no previous allocation' do
-    let!(:case_info) {
-      create(:case_information, :nps, nomis_offender_id: nomis_offender_id,
-             local_delivery_unit: build(:local_delivery_unit)
-      )
-    }
+  describe "before OMIC launches in Open Prisons" do
+    # The day before Open Prison policy starts
+    let(:today) { HandoverDateService::OPEN_PRISON_POLICY_START_DATE - 1.day }
 
-    it 'sends an email' do
-      expect(PomMailer).to receive(:responsibility_override_open_prison)
-                             .with(hash_including(
-                                     prisoner_number: nomis_offender_id,
-                                     prisoner_name: offender.full_name,
-                                     responsible_pom_name: 'N/A',
-                                     responsible_pom_email: 'N/A',
-                                     prison_name: 'HMP/YOI Hatfield',
-                                     previous_prison_name: 'HMP Leeds'
-                                   )).and_call_original
-
-      described_class.perform_now(movement_json)
-    end
-  end
-
-  context "when a Welsh offender is transferred to Prescoed open prison" do
-    let(:open_prison_code) { PrisonService::PRESCOED_CODE }
-
-    let!(:case_info) {
-      create(:case_information, :welsh, :nps, nomis_offender_id: nomis_offender_id,
-             local_delivery_unit: build(:local_delivery_unit)
-      )
-    }
-
-    # Create an allocation where the offender is allocated, and then deallocate so we can
-    # test finding the last pom that was allocated to this offender ....
-    let!(:allocation) {
-      create(:allocation, nomis_offender_id: nomis_offender_id,
-             primary_pom_nomis_id: other_staff_id, primary_pom_name: 'Primary POMName',
-             prison: closed_prison_code
-      ).tap { |alloc|
-        alloc.update(primary_pom_nomis_id: nomis_staff_id)
-        alloc.offender_transferred
+    context 'when there is no previous allocation' do
+      let!(:case_info) {
+        create(:case_information, :nps, nomis_offender_id: nomis_offender_id,
+               local_delivery_unit: build(:local_delivery_unit)
+        )
       }
-    }
 
-    it 'includes previous POM details in the email' do
-      expect(PomMailer).to receive(:responsibility_override_open_prison)
-                             .with(hash_including(
-                                     prisoner_number: nomis_offender_id,
-                                     prisoner_name: offender.full_name,
-                                     responsible_pom_name: 'Primary POMName',
-                                     responsible_pom_email: 'pom@localhost.local',
-                                     prison_name: 'HMP/YOI Hatfield',
-                                     previous_prison_name: 'HMP Leeds'
-                                   )).and_call_original
+      it 'asks the LDU to allocate a Responsible COM' do
+        expect(PomMailer).to receive(:responsibility_override_open_prison)
+                               .with(hash_including(
+                                       prisoner_number: nomis_offender_id,
+                                       prisoner_name: offender.full_name,
+                                       responsible_pom_name: 'N/A',
+                                       responsible_pom_email: 'N/A',
+                                       prison_name: open_prison_name,
+                                       previous_prison_name: closed_prison_name
+                                     )).and_call_original
 
-      described_class.perform_now(movement_json)
-    end
-  end
-
-  context "when a Welsh offender is transferred to Prescoed open prison" do
-    let(:open_prison_code) { 'UPI' } # HMP Prescoed
-
-    let!(:case_info) {
-      create(:case_information, :welsh, :nps, nomis_offender_id: nomis_offender_id,
-             local_delivery_unit: build(:local_delivery_unit)
-      )
-    }
-
-    # Create an allocation where the offender is allocated, and then deallocate so we can
-    # test finding the last pom that was allocated to this offender ....
-    let!(:allocation) {
-      create(:allocation, nomis_offender_id: nomis_offender_id,
-             primary_pom_nomis_id: other_staff_id, primary_pom_name: 'Primary POMName',
-             prison: closed_prison_code
-      ).tap { |alloc|
-        alloc.update(primary_pom_nomis_id: nomis_staff_id)
-        alloc.offender_transferred
-      }
-    }
-
-    context 'with an indeterminate offender' do
-      let(:nomis_offender) { build(:nomis_offender, :indeterminate, offenderNo: nomis_offender_id, latestLocationId: open_prison_code) }
-
-      it 'sends an email to the LDU' do
-        expect(CommunityMailer).to receive(:omic_open_prison_community_allocation)
-                                     .with(hash_including(
-                                             nomis_offender_id: nomis_offender_id,
-                                             prisoner_name: offender.full_name,
-                                             crn: case_info.crn,
-                                             ldu_email: offender.ldu_email_address,
-                                             prison: 'HMP/YOI Prescoed',
-                                             pom_name: 'Primary POMName',
-                                             pom_email: 'pom@localhost.local'
-                                           )).and_call_original
-
-        expect { described_class.perform_now(movement_json) }.to change(EmailHistory, :count).by(1)
+        described_class.perform_now(movement_json)
       end
     end
 
-    context "with a determinate offender" do
-      it 'does not send an email to the LDU' do
-        expect_any_instance_of(PomMailer).not_to receive(:responsibility_override_open_prison)
-        expect_any_instance_of(CommunityMailer).not_to receive(:omic_open_prison_community_allocation)
-        expect { described_class.perform_now(movement_json) }.to change(EmailHistory, :count).by(0)
+    context 'when there is a previous allocation' do
+      let!(:case_info) {
+        create(:case_information, :nps, nomis_offender_id: nomis_offender_id,
+               local_delivery_unit: build(:local_delivery_unit)
+        )
+      }
+
+      # Create an allocation where the offender is allocated, and then deallocate so we can
+      # test finding the last pom that was allocated to this offender ....
+      let!(:allocation) {
+        create(:allocation, nomis_offender_id: nomis_offender_id,
+               primary_pom_nomis_id: other_staff_id, primary_pom_name: 'Primary POMName',
+               prison: closed_prison_code
+        ).tap { |alloc|
+          alloc.update(primary_pom_nomis_id: nomis_staff_id)
+          alloc.offender_transferred
+        }
+      }
+
+      it 'includes previous POM details in the email' do
+        expect(PomMailer).to receive(:responsibility_override_open_prison)
+                               .with(hash_including(
+                                       prisoner_number: nomis_offender_id,
+                                       prisoner_name: offender.full_name,
+                                       responsible_pom_name: 'Primary POMName',
+                                       responsible_pom_email: 'pom@localhost.local',
+                                       prison_name: open_prison_name,
+                                       previous_prison_name: closed_prison_name
+                                     )).and_call_original
+
+        described_class.perform_now(movement_json)
+      end
+    end
+
+    context "when a Welsh offender is transferred to Prescoed open prison" do
+      let(:open_prison_code) { PrisonService::PRESCOED_CODE }
+
+      let!(:case_info) {
+        create(:case_information, :welsh, :nps, nomis_offender_id: nomis_offender_id,
+               local_delivery_unit: build(:local_delivery_unit)
+        )
+      }
+
+      # Create an allocation where the offender is allocated, and then deallocate so we can
+      # test finding the last pom that was allocated to this offender ....
+      let!(:allocation) {
+        create(:allocation, nomis_offender_id: nomis_offender_id,
+               primary_pom_nomis_id: other_staff_id, primary_pom_name: 'Primary POMName',
+               prison: closed_prison_code
+        ).tap { |alloc|
+          alloc.update(primary_pom_nomis_id: nomis_staff_id)
+          alloc.offender_transferred
+        }
+      }
+
+      context 'with an indeterminate offender' do
+        let(:nomis_offender) { indeterminate_nomis_offender }
+
+        it 'asks the LDU to allocate a Supporting COM' do
+          expect(CommunityMailer).to receive(:omic_open_prison_community_allocation)
+                                       .with(hash_including(
+                                               nomis_offender_id: nomis_offender_id,
+                                               prisoner_name: offender.full_name,
+                                               crn: case_info.crn,
+                                               ldu_email: offender.ldu_email_address,
+                                               prison: open_prison_name,
+                                               pom_name: 'Primary POMName',
+                                               pom_email: 'pom@localhost.local'
+                                             )).and_call_original
+
+          expect { described_class.perform_now(movement_json) }.to change(EmailHistory, :count).by(1)
+        end
+      end
+
+      context "with a determinate offender" do
+        it 'does not send an email to the LDU' do
+          expect_any_instance_of(PomMailer).not_to receive(:responsibility_override_open_prison)
+          expect_any_instance_of(CommunityMailer).not_to receive(:omic_open_prison_community_allocation)
+          expect { described_class.perform_now(movement_json) }.to change(EmailHistory, :count).by(0)
+        end
+      end
+    end
+  end
+
+  describe "after OMIC has launched in Open Prisons" do
+    # The day Open Prison policy starts
+    let(:today) { HandoverDateService::OPEN_PRISON_POLICY_START_DATE }
+
+    context 'when the offender is NPS Determinate' do
+      let!(:case_info) {
+        create(:case_information, :nps, nomis_offender_id: nomis_offender_id,
+               local_delivery_unit: build(:local_delivery_unit)
+        )
+      }
+
+      it "does not send an email - a COM isn't needed yet" do
+        described_class.perform_now(movement_json)
+        email_job = enqueued_jobs.first
+        expect(email_job).to be_nil
+        expect(offender.com_responsibility).not_to be_involved
+      end
+    end
+
+    context 'when the offender is CRC Determinate' do
+      let!(:case_info) {
+        create(:case_information, :crc, nomis_offender_id: nomis_offender_id,
+               local_delivery_unit: build(:local_delivery_unit)
+        )
+      }
+
+      it "does not send an email - a COM isn't needed yet" do
+        described_class.perform_now(movement_json)
+        email_job = enqueued_jobs.first
+        expect(email_job).to be_nil
+        expect(offender.com_responsibility).not_to be_involved
+      end
+    end
+
+    context 'when the offender is NPS Indeterminate' do
+      let(:nomis_offender) { indeterminate_nomis_offender }
+
+      context 'when there is no previous allocation' do
+        let!(:case_info) {
+          create(:case_information, :nps, nomis_offender_id: nomis_offender_id,
+                 local_delivery_unit: build(:local_delivery_unit)
+          )
+        }
+
+        it 'asks the LDU to allocate a Supporting COM' do
+          expect(CommunityMailer).to receive(:omic_open_prison_community_allocation)
+                                       .with(hash_including(
+                                               nomis_offender_id: nomis_offender_id,
+                                               prisoner_name: offender.full_name,
+                                               crn: case_info.crn,
+                                               ldu_email: offender.ldu_email_address,
+                                               prison: open_prison_name,
+                                               pom_name: 'N/A',
+                                               pom_email: 'N/A'
+                                             )).and_call_original
+
+          expect { described_class.perform_now(movement_json) }.to change(EmailHistory, :count).by(1)
+        end
+      end
+
+      context 'when there is a previous allocation' do
+        let!(:case_info) {
+          create(:case_information, :nps, nomis_offender_id: nomis_offender_id,
+                 local_delivery_unit: build(:local_delivery_unit)
+          )
+        }
+
+        # Create an allocation where the offender is allocated, and then deallocate so we can
+        # test finding the last pom that was allocated to this offender ....
+        let!(:allocation) {
+          create(:allocation, nomis_offender_id: nomis_offender_id,
+                 primary_pom_nomis_id: other_staff_id, primary_pom_name: 'Primary POMName',
+                 prison: closed_prison_code
+          ).tap { |alloc|
+            alloc.update(primary_pom_nomis_id: nomis_staff_id)
+            alloc.offender_transferred
+          }
+        }
+
+        it 'includes previous POM details in the email' do
+          expect(CommunityMailer).to receive(:omic_open_prison_community_allocation)
+                                       .with(hash_including(
+                                               nomis_offender_id: nomis_offender_id,
+                                               prisoner_name: offender.full_name,
+                                               crn: case_info.crn,
+                                               ldu_email: offender.ldu_email_address,
+                                               prison: open_prison_name,
+                                               pom_name: 'Primary POMName',
+                                               pom_email: 'pom@localhost.local'
+                                             )).and_call_original
+
+          expect { described_class.perform_now(movement_json) }.to change(EmailHistory, :count).by(1)
+        end
       end
     end
   end
