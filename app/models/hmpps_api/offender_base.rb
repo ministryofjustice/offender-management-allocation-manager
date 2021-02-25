d52828e0fa43952c0ebdf1fc4a9a01af83f87a5f
Ollie Treend <ollie@treend.com>
2021-02-25 16:53:29 +0000

    Unify the "a COM is needed" notification messages
    Two error/notification messages were developed in separate tickets
    (MO-70 and MO-75) which would display when a COM is needed but not
    allocated:
    - MO-70 handled the scenario when the LDU has successfully been emailed.
    - MO-75 handled the scenario when we don't know the LDU's email
      address, and so are unable to contact them.
    
    The implementation resulted in duplicate (or _very similar_) code which
    really belonged together in a unified notification message.
    
    This commit is a refactor to put them both together, and slightly tweaks
    the behaviour after discussions with our designer. For a description of
    the new behaviour, see the spec for the `offender_needs_a_com` view
    partial.

--- app/models/hmpps_api/offender_base.rb
+++ app/models/hmpps_api/offender_base.rb
@@ -59,12 +59,6 @@ module HmppsApi
       has_case_information? && within_early_allocation_window? && @case_information.early_allocations.suitable_offenders_pre_referral_window.any?
     end
 
-    def welsh_offender_in_prescoed_needs_com?
-      prison_id == PrisonService::PRESCOED_CODE &&
-        allocated_com_name.blank? &&
-        EmailHistory.sent_within_current_sentence(self, EmailHistory::OPEN_PRISON_COMMUNITY_ALLOCATION).any?
-    end
-
     # Has a CaseInformation record been loaded for this offender?
     def has_case_information?
       @case_information.present?
@@ -222,8 +216,9 @@ module HmppsApi
       earliest_date.present? && earliest_date <= Time.zone.today + 18.months
     end
 
-    def needs_com_and_ldu_is_uncontactable?
-      needs_com? && sentenced? && allocated_com_name.blank? && ldu_email_address.blank?
+    def needs_a_com?
+      (com_responsibility.responsible? || com_responsibility.supporting?) &&
+        allocated_com_name.blank?
     end
 
     def inside_omic_policy?
@@ -233,10 +228,6 @@ module HmppsApi
     end
 
   private
-
-    def needs_com?
-      handover.community.responsible? || handover.community.supporting?
-    end
 
     def age
       return nil if date_of_birth.blank?
---
+++ spec/views/shared/notifications/offender_needs_a_com.html.erb_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe "shared/notifications/offender_needs_a_com", type: :view do
+  let(:page) { Nokogiri::HTML(rendered) }
+  let(:title) { page.css('#com-error-summary-title').text.strip }
+  let(:com_warning) { page.css('#com_warning') }
+  let(:ldu_warning) { page.css('#ldu_warning') }
+
+  let(:case_info) { build(:case_information, :nps, local_delivery_unit: ldu, manual_entry: ldu.nil?, team: nil) }
+  let(:ldu) { build(:local_delivery_unit) }
+  let(:email_history) { [] }
+
+  let(:offender) {
+    build(:offender, :determinate, sentence: build(:sentence_detail, :inside_handover_window)).tap { |o|
+      o.load_case_information(case_info)
+    }
+  }
+
+  before do
+    render partial: 'shared/notifications/offender_needs_a_com',
+           locals: { offender: offender, email_history: email_history }
+  end
+
+  context 'when an email has been sent to the LDU' do
+    let(:date_sent) { Time.zone.today }
+    let(:email_history) { [create(:email_history, :open_prison_community_allocation, created_at: date_sent)] }
+
+    it 'says "You may need to contact the community probation office"' do
+      expect(title).to eq(I18n.t('views.notifications.offender_needs_a_com.maybe_contact_ldu'))
+    end
+
+    it 'says a supporting COM is needed' do
+      text = I18n.t('views.notifications.offender_needs_a_com.com_needed')
+      href = '#com-name'
+      expect(com_warning).to have_link(text, href: href)
+    end
+
+    it 'says when the LDU was emailed' do
+      text = I18n.t('views.notifications.offender_needs_a_com.ldu_emailed', date: format_date(date_sent))
+      href = '#com-name'
+      expect(ldu_warning).to have_link(text, href: href)
+    end
+  end
+
+  context 'when an email has not been sent' do
+    it 'says "You must contact the community probation office"' do
+      expect(title).to eq(I18n.t('views.notifications.offender_needs_a_com.must_contact_ldu'))
+    end
+
+    it 'says a supporting COM is needed' do
+      text = I18n.t('views.notifications.offender_needs_a_com.com_needed')
+      href = '#com-name'
+      expect(com_warning).to have_link(text, href: href)
+    end
+
+    context 'when the LDU email address is unknown' do
+      let(:ldu) { nil }
+
+      it 'says the LDU cannot be contacted' do
+        text = I18n.t('views.notifications.offender_needs_a_com.ldu_uncontactable')
+        href = '#com-name'
+        expect(ldu_warning).to have_link(text, href: href)
+      end
+    end
+
+    context 'when we know the LDU email address' do
+      it 'does not warn that the LDU is un-contactable' do
+        expect(ldu_warning).to be_empty
+      end
+    end
+  end
+end
