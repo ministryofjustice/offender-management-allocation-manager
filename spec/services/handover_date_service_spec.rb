5af893e36cace6621c070e17bec20dbfa3c256bb
Stephen Dicks <stephen@starswan.com>
2021-03-17 08:25:35 +0000

    MO-429 Women's OMIC rules

--- spec/services/handover_date_service_spec.rb
+++ spec/services/handover_date_service_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 describe HandoverDateService do
   subject { described_class.handover(offender) }
 
+  let(:test_strategy) { Flipflop::FeatureSet.current.test! }
   let(:pom) { subject.custody }
   let(:com) { subject.community }
   let(:start_date) { subject.start_date }
@@ -14,8 +15,11 @@ describe HandoverDateService do
   let(:closed_prison) { 'LEI' } # HMP Leeds
   let(:prescoed_prison) { PrisonService::PRESCOED_CODE } # HMP Prescoed
   let(:open_prison) { 'HVI' } # HMP Haverigg
+  let(:womens_prison) { build(:womens_prison).code }
   let(:prescoed_policy_start_date) { Date.new(2020, 10, 19) } # 19th October 2020
   let(:open_policy_start_date) { Date.new(2021, 3, 31) } # 31st March 2021
+  let(:womens_policy_start_date) { Date.parse('30th April 2021') }
+  let(:category) { 'C' }
 
   # Set the current date by changing the value of `today`
   let(:today) { Time.zone.today }
@@ -23,10 +27,18 @@ describe HandoverDateService do
   let(:arrival_date) { today }
   let(:welsh?) { false }
   let(:prison) { closed_prison }
+  # Default sentence start if not provided
+  let(:sentence_start_date) { build(:sentence_detail).sentence_start_date }
 
-  before { Timecop.travel(today) }
+  before do
+    Timecop.travel(today)
+    test_strategy.switch!(:womens_estate, true)
+  end
 
-  after { Timecop.return }
+  after do
+    Timecop.return
+    test_strategy.switch!(:womens_estate, false)
+  end
 
   context 'when determinate' do
     let(:today) { Date.parse('01/01/2021') }
@@ -34,7 +46,7 @@ describe HandoverDateService do
     let(:offender) {
       build(:offender, :determinate,
             latestLocationId: prison,
-            sentence: build(:sentence_detail, conditionalReleaseDate: crd)
+            sentence: build(:sentence_detail, sentenceStartDate: sentence_start_date, conditionalReleaseDate: crd)
       ).tap { |o|
         o.load_case_information(case_info)
         o.prison_arrival_date = arrival_date
@@ -85,6 +97,40 @@ describe HandoverDateService do
 
         it 'COM is responsible' do
           expect(com).to be_responsible
+        end
+      end
+
+      context 'when in a Womens prison' do
+        let(:prison) { womens_prison }
+        let(:arrival_date) { sentence_start_date }
+
+        context 'when entering on or after the policy start date' do
+          let(:sentence_start_date) { womens_policy_start_date }
+
+          it 'is policy rules' do
+            expect(pom).to be_responsible
+          end
+        end
+
+        context 'when entering before the policy start date' do
+          let(:sentence_start_date) { womens_policy_start_date - 1.day }
+
+          context 'with release on or after 30/9/2022' do
+            let(:crd) { Date.parse('30/09/2022') }
+
+            it 'is policy rules' do
+              expect(pom).to be_responsible
+            end
+          end
+
+          context 'with release before 30/9/2022' do
+            let(:crd) { Date.parse('29/09/2022') }
+
+            it 'is pre-policy rules' do
+              expect(pom).to be_supporting
+              expect(com).to be_responsible
+            end
+          end
         end
       end
 
@@ -344,7 +390,8 @@ describe HandoverDateService do
       let(:offender) {
         build(:offender, :indeterminate,
               latestLocationId: prison,
-              sentence: build(:sentence_detail, tariffDate: tariff_date)
+              categoryCode: category,
+              sentence: build(:sentence_detail, tariffDate: tariff_date, sentence_start_date: sentence_start_date)
         ).tap { |o|
           o.load_case_information(case_info)
           o.prison_arrival_date = arrival_date
@@ -443,6 +490,28 @@ describe HandoverDateService do
 
             it 'COM is responsible' do
               expect(com).to be_responsible
+            end
+          end
+        end
+      end
+
+      context 'when in a Womens prison' do
+        let(:prison) { womens_prison }
+
+        context 'when entering on or after the policy start date' do
+          let(:sentence_start_date) { womens_policy_start_date }
+
+          it 'follows policy rules' do
+            expect(pom).to be_responsible
+            expect(com).not_to be_responsible
+          end
+
+          context 'when transferred to open conditions (Cat T)' do
+            let(:category) { 'T' }
+
+            it 'invokes open prison rules' do
+              expect(com).to be_responsible
+              expect(pom).to be_supporting
             end
           end
         end
