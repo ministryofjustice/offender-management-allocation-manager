358774e0e7fdadd6c0e0b8270db52a6865bf8ea6
Stephen Dicks <stephen@starswan.com>
2021-03-30 14:58:02 +0100

    MO-425 Complexity change history in allocations history view

---
+++ app/presenters/complexity_new_history.rb
@@ -0,0 +1,29 @@
+class ComplexityNewHistory
+  def initialize(history)
+    @history = history
+  end
+
+  def created_at
+    @history.fetch(:createdTimeStamp)
+  end
+
+  def level
+    @history.fetch(:level).titleize
+  end
+
+  def prison
+    nil
+  end
+
+  def to_partial_path
+    'case_history/new_complexity'
+  end
+
+  def created_by_name
+    username = @history[:sourceUser]
+    if username
+      user = HmppsApi::PrisonApi::UserApi.user_details(username)
+      "#{user.last_name}, #{user.first_name}"
+    end
+  end
+end
