ce8ffdabf873bf5f0134ba2341e338690b40053f
Ollie Treend <ollie@treend.com>
2021-04-07 16:05:59 +0100

    Transform complexity values on CSV import
    The complexity values that we're expecting to import via CSV need to be transformed to match what the Complexity of Need API expects.
    
    "High" => "high"
    "Medium" => "medium"
    "Low" => "low"
    "Unassigned" => ignore

--- lib/complexity_importer.rb
+++ lib/complexity_importer.rb
@@ -10,7 +10,9 @@ class ComplexityImporter
     def import(stream)
       CSV.new(stream, headers: true).each do |row|
         offender_no = row.fetch(NOMS_HEADER)
-        complexity = row.fetch(COMPLEXITY_HEADER)
+        complexity = row.fetch(COMPLEXITY_HEADER).downcase
+
+        next if complexity == 'unassessed' # skip these rows
 
         HmppsApi::ComplexityApi.save offender_no, level: complexity, reason: nil, username: nil
       end
