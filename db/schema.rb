6d4a41d7e01b1f8c89f19b74e1944d3c4bfd0dd0
Stephen Dicks <stephen@starswan.com>
2021-04-16 09:18:06 +0100

    move migration so that it is the most recent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_115125) do
+ActiveRecord::Schema.define(version: 2021_04_16_140412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
