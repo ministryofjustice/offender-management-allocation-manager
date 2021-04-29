3f87fe24baf7cc4b358678da8a988e18790648b7
Stephen Dicks <stephen@starswan.com>
2021-04-29 15:56:36 +0100

    MO-578 drop welsh offender column as its a duplicate of proabtion_service and is unused anyway

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_131040) do
+ActiveRecord::Schema.define(version: 2021_04_29_144703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_04_19_131040) do
     t.string "tier"
     t.string "case_allocation"
     t.string "nomis_offender_id"
-    t.text "welsh_offender"
     t.string "crn"
     t.integer "mappa_level"
     t.boolean "manual_entry", null: false
