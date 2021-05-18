39a70f53cb9ad69b8f2401ade7f878c30e35fa43
Stephen Dicks <stephen@starswan.com>
2021-05-18 15:17:32 +0100

    put in new survey links

--- spec/features/feedback_feature_spec.rb
+++ spec/features/feedback_feature_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 feature 'feedback' do
+  let(:feedback_link) { 'https://eu.surveymonkey.com/r/3BJ9M5K' }
+
   it 'provides a link to the feedback form', vcr: { cassette_name: 'prison_api/feedback_link' } do
     signin_spo_user
 
     visit '/'
 
-    feedback_link = 'https://www.research.net/r/MM8TNLW'
-
     footer = page.find(:css, '.govuk-footer')
     within footer do
       expect(page).to have_link('Feedback', href: feedback_link)
+    end
+
+    within '.govuk-phase-banner__text' do
+      expect(page).to have_link('feedback', href: feedback_link)
     end
   end
 end
