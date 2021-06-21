04e2ced9f6f87a9536629ecbf9b520a58ac09a91
Stephen Dicks <stephen@starswan.com>
2021-06-21 17:21:50 +0100

    Add retries on 500 errors and HTTP2 Framing Layer, and remove POST retries apart from Prison API and Prison Search

--- app/services/hmpps_api/prison_api/prison_api_client.rb
+++ app/services/hmpps_api/prison_api/prison_api_client.rb
@@ -8,13 +8,15 @@ module HmppsApi
       # Client for the Prison API
       def client
         host = Rails.configuration.prison_api_host
-        HmppsApi::Client.new(host + '/api')
+        # Prison API uses POSTs as fake GETs so its ok to retry them
+        HmppsApi::Client.new(host + '/api', extra_retry_methods: [:post])
       end
 
       # Client for the Prisoner Offender Search API
       def search_client
         host = Rails.configuration.prisoner_search_host
-        HmppsApi::Client.new(host + '/prisoner-search')
+        # Prison Search API uses POSTs as fake GETs so its ok to retry them
+        HmppsApi::Client.new(host + '/prisoner-search', extra_retry_methods: [:post])
       end
 
       def api_deserialiser
