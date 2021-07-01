f9014be0bd5ee8c298af328a96330e98b1bf37c8
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2021-07-01 09:20:44 +0100

    Add copy with counts and order of the first 4 cards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class DashboardController < PrisonsApplicationController
-  def index; end
+  def index
+    @unallocated_cases_count = @prison.unallocated.count
+    @missing_details_cases_count = @prison.missing_info.count
+  end
 end
