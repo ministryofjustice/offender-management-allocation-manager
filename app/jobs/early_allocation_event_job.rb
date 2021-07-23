eb7e3f3ef9d7e815ca64bcd5ea8df6c2ff24a6eb
Ollie Treend <ollie@treend.com>
2021-07-23 14:57:29 +0100

    MO-647 fix: don't trigger events on nil offenders
    The Offenders table can contain records for offenders who are no longer
    in prison.
    
    When the Prison API returns `nil` for queued offenders, do nothing.
    Otherwise we trigger errors for attempting to call undefined methods on
    a `nil` object.

--- app/jobs/early_allocation_event_job.rb
+++ app/jobs/early_allocation_event_job.rb
@@ -3,6 +3,6 @@ class EarlyAllocationEventJob < ApplicationJob
 
   def perform offender_id
     offender = OffenderService.get_offender(offender_id)
-    offender.trigger_early_allocation_event
+    offender.trigger_early_allocation_event unless offender.nil?
   end
 end
--- spec/jobs/early_allocation_event_job_spec.rb
+++ spec/jobs/early_allocation_event_job_spec.rb
@@ -4,12 +4,25 @@ RSpec.describe EarlyAllocationEventJob, type: :job do
   let(:prison) { create(:prison) }
   let(:offender) { build(:offender) }
 
-  before do
-    stub_offender(build(:nomis_offender, offenderNo: offender.nomis_offender_id, agencyId: prison.code))
-    expect(EarlyAllocationEventService).to receive(:send_early_allocation)
+  context 'when the offender exists in NOMIS' do
+    before do
+      stub_offender(build(:nomis_offender, offenderNo: offender.nomis_offender_id, agencyId: prison.code))
+      expect(EarlyAllocationEventService).to receive(:send_early_allocation)
+    end
+
+    it 'sends the event via the job' do
+      described_class.perform_now offender.nomis_offender_id
+    end
   end
 
-  it 'sends the event via the job' do
-    described_class.perform_now offender.nomis_offender_id
+  context 'when that offender ID no longer exists in NOMIS' do
+    before do
+      stub_non_existent_offender(offender.nomis_offender_id)
+      expect(EarlyAllocationEventService).not_to receive(:send_early_allocation)
+    end
+
+    it 'does not send the event' do
+      described_class.perform_now offender.nomis_offender_id
+    end
   end
 end
