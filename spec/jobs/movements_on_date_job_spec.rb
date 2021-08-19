8ccb20156c8696acb7149d2d2da978b36a66e984
Ollie Treend <ollie@treend.com>
2021-08-19 16:48:24 +0100

    MO-826: Write a test to demonstrate the bug
    I've made the spec for MovementOnDateJob more comprehensive to cover the
    bug which we're observing.
    
    The error actually occurs in the AllocationHistory model – but the only
    route to that functionality is through this job. So I'm treating this as
    a more 'high level' test of the movement processing functionality as a
    whole.

--- spec/jobs/movements_on_date_job_spec.rb
+++ spec/jobs/movements_on_date_job_spec.rb
@@ -1,38 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe MovementsOnDateJob, type: :job do
-  let!(:prison) { create(:prison, code: 'MDI') }
-  let(:nomis_offender_id) { 'G3462VT' }
-  let!(:alloc) { create(:allocation_history, nomis_offender_id: nomis_offender_id, secondary_pom_nomis_id: 123_435, prison: 'MDI') }
+  let(:nomis_offender_id) { nomis_offender.fetch(:offenderNo) }
+  let(:nomis_offender) { build(:nomis_offender, agencyId: to_prison_code) }
+  let(:case_info) { create(:case_information) }
+  let!(:offender_record) { create(:offender, nomis_offender_id: nomis_offender_id, case_information: case_info) }
+  let(:today) { Time.zone.today }
+  let(:yesterday) { today - 1.day }
+  let(:pom) { build(:pom) }
+  let!(:allocation) { create(:allocation_history, nomis_offender_id: nomis_offender_id, primary_pom_nomis_id: pom.staff_id, prison: from_prison_code) }
+  let(:mail_params) {
+    {
+      email: pom.email_address,
+      pom_name: pom.first_name.titleize,
+      offender_name: "#{nomis_offender.fetch(:lastName)}, #{nomis_offender.fetch(:firstName)}".titleize,
+      nomis_offender_id: nomis_offender_id,
+      prison_name: Prison.find(from_prison_code).name,
+      url: "http://localhost:3000/prisons/#{from_prison_code}/staff/#{pom.staff_id}/caseload"
+    }
+  }
 
   before do
-    stub_auth_token
+    stub_offender(nomis_offender)
+    stub_poms(from_prison_code, [pom])
+
+    # Stub offender movements from yesterday
+    stub_request(:get, "#{ApiHelper::T3}/movements?fromDateTime=#{yesterday - 1.year}T00:00&movementDate=#{yesterday}").
+      to_return(body: [movement].to_json)
+
+    # Expect POM to be emailed about the deallocation
+    expect(PomMailer).to receive(:offender_deallocated).with(mail_params).and_return(double(deliver_later: nil))
   end
 
-  it 'deallocates' do
-    allow(OffenderService).to receive(:get_offender).and_return(build(:hmpps_api_offender))
+  context 'when the offender has been released from prison' do
+    let(:from_prison_code) { create(:prison).code }
+    let(:to_prison_code) { 'OUT' } # "OUT" is the prison code NOMIS uses to mean "not in prison"
 
-    stub_request(:get, "#{ApiHelper::T3}/movements?fromDateTime=2018-06-30T00:00&movementDate=2019-06-30").
-      to_return(body: [attributes_for(:movement,
-                                      offenderNo: nomis_offender_id,
-         fromAgency: "MDI",
-         toAgency: "WEI",
-         movementType: "ADM",
-         directionCode: "IN")].to_json)
+    let(:movement) {
+      attributes_for(:movement,
+                     offenderNo: nomis_offender_id,
+                     movementType: 'REL',
+                     directionCode: 'OUT',
+                     fromAgency: from_prison_code,
+                     toAgency: to_prison_code)
+    }
 
-    pom = build(:pom, staffId: 485926)
-    stub_poms('MDI', [pom])
-    stub_pom pom
-    stub_offenders_for_prison('MDI', [])
+    it 'deallocates and emails the POM' do
+      expect(allocation).to be_active
+      described_class.perform_now(today.to_s)
+      allocation.reload
+      expect(allocation).not_to be_active
+    end
+  end
 
-    expect(alloc.primary_pom_nomis_id).not_to be_nil
-    expect(alloc.secondary_pom_nomis_id).not_to be_nil
+  context 'when the offender has transferred to a different prison' do
+    let(:from_prison_code) { create(:prison).code }
+    let(:to_prison_code) { create(:prison).code }
 
-    described_class.perform_now(Date.new(2019, 7, 1).to_s)
+    let(:movement) {
+      attributes_for(:movement,
+                     offenderNo: nomis_offender_id,
+                     movementType: 'ADM',
+                     directionCode: 'IN',
+                     fromAgency: from_prison_code,
+                     toAgency: to_prison_code)
+    }
 
-    alloc.reload
-
-    expect(alloc.primary_pom_nomis_id).to be_nil
-    expect(alloc.secondary_pom_nomis_id).to be_nil
+    it 'deallocates and emails the POM' do
+      expect(allocation).to be_active
+      described_class.perform_now(today.to_s)
+      allocation.reload
+      expect(allocation).not_to be_active
+    end
   end
 end
