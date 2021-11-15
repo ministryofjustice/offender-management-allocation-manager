77b7f784858e6c9bdecf095c545bb9749a0c9078
Matthew Green <matthew.green@digital.justice.gov.uk>
2021-11-15 14:58:46 +0000

    refs #MO-879 Include OASys LAYER_1 status if no LAYER_3 is present. Catch 409 exceptions when checking for OASys assessments and display user message to remove duplicate account on OASys for the offender.

--- spec/services/hmpps_api/assessment_api_spec.rb
+++ spec/services/hmpps_api/assessment_api_spec.rb
@@ -12,9 +12,9 @@ describe HmppsApi::AssessmentApi do
   describe '.get_latest_oasys_date' do
     let(:offender_no) { 'A9346AC' }
 
-    let(:stub_url) { "#{api_host}/offenders/nomisId/#{offender_no}/assessments/summary?assessmentType=LAYER_3&assessmentStatus=COMPLETE" }
+    let(:stub_url) { "#{api_host}/offenders/nomisId/#{offender_no}/assessments/summary?assessmentStatus=COMPLETE" }
 
-    context 'when there is one completed OASys assessment' do
+    context 'when there is one completed LAYER_3 OASys assessment' do
       # The last assessment date is the 'completed' field.
       # The test expects it to be a date object for consistency throughout the code base
       let(:completed_date) { '2012-12-10'.to_date }
@@ -30,22 +30,44 @@ describe HmppsApi::AssessmentApi do
         expect(a_request(:get, stub_url)).to have_been_made.once
       end
 
-      it 'returns the date the assessment was completed' do
-        expect(described_class.get_latest_oasys_date(offender_no)).to eq(completed_date)
+      it 'returns the date the assessment was completed for LAYER_3' do
+        expect(described_class.get_latest_oasys_date(offender_no)).to match(assessment_type: "LAYER_3", completed: completed_date)
+      end
+    end
+
+    context 'when there is one completed LAYER_1 OASys assessment' do
+      # The last assessment date is the 'completed' field.
+      # The test expects it to be a date object for consistency throughout the code base
+      let(:completed_date) { '2012-12-10'.to_date }
+      let(:latest_assessment) { build(:assessment_api_response, completed: completed_date, refAssessmentVersionCode: 'LAYER_1', assessmentType: 'LAYER_1') }
+
+      before do
+        stub_request(:get, stub_url).to_return(status: 200, body: [
+          latest_assessment].to_json)
+      end
+
+      it 'checks that the API has only been called once' do
+        described_class.get_latest_oasys_date(offender_no)
+        expect(a_request(:get, stub_url)).to have_been_made.once
+      end
+
+      it 'returns the date the assessment was completed for LAYER_1' do
+        expect(described_class.get_latest_oasys_date(offender_no)).to match(assessment_type: "LAYER_1", completed: completed_date)
       end
     end
 
     context 'when there are multiple completed OASys assessments' do
-      let(:old_assessments) { build_list(:assessment_api_response, 9) }
+      let(:old_layer_3_assessments) { build_list(:assessment_api_response, 5, refAssessmentVersionCode: 'LAYER_3', assessmentType: 'LAYER_3') }
+      let(:old_layer_1_assessments) { build_list(:assessment_api_response, 5, refAssessmentVersionCode: 'LAYER_1', assessmentType: 'LAYER_1') }
       let(:latest_assessment) { build(:assessment_api_response, completed: Time.zone.now) }
-      let(:all_assessments) { (old_assessments + [latest_assessment]).shuffle }
+      let(:all_assessments) { (old_layer_3_assessments + old_layer_1_assessments + [latest_assessment]).shuffle }
 
       before do
         stub_request(:get, stub_url).to_return(status: 200, body: all_assessments.to_json)
       end
 
       it 'returns the most recently completed assessment' do
-        expect(described_class.get_latest_oasys_date(offender_no)).to eq(Time.zone.today)
+        expect(described_class.get_latest_oasys_date(offender_no)).to match(assessment_type: "LAYER_3", completed: Time.zone.today)
       end
     end
 
@@ -62,8 +84,8 @@ describe HmppsApi::AssessmentApi do
     context 'when the offender is not found (status 404)' do
       before do
         stub_request(:get, stub_url).to_return(status: 404, body: {
-            "status": 404,
-            "developerMessage": "Offender not found for NOMIS, A1857ER"
+          "status": 404,
+          "developerMessage": "Offender not found for NOMIS, A1857ER"
         }.to_json)
       end
 
@@ -81,7 +103,7 @@ describe HmppsApi::AssessmentApi do
       end
 
       it 'returns 409 constant' do
-        expect(described_class.get_latest_oasys_date(offender_no)).to eq(Faraday::ConflictError)
+        expect(described_class.get_latest_oasys_date(offender_no)).to match(assessment_type: Faraday::ConflictError, completed: nil)
       end
     end
   end
