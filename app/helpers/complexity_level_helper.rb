1323357e86df8057b6648fa10505d0991e6c522f
Matthew Green <matthew.green@digital.justice.gov.uk>
2022-01-18 15:53:32 +0000

    refs #MO-894 include test for global caseload controller, fix caseload filters

--- app/helpers/complexity_level_helper.rb
+++ app/helpers/complexity_level_helper.rb
@@ -1,4 +1,6 @@
 module ComplexityLevelHelper
+  COMPLEXITIES = { 'high' => 3, 'medium' => 2, 'low' => 1 }.freeze
+
   def display_complexity_change_info(previous_complexity_level:, updated_complexity_level:)
     if updated_complexity_level == 'high'
       render partial: 'complexity_levels/complexity_level_increase_to_high', locals: { previous_complexity_level: previous_complexity_level }
@@ -7,5 +9,10 @@ module ComplexityLevelHelper
     else
       render partial: 'complexity_levels/complexity_level_change_medium_low'
     end
+  end
+
+  # this is required for sorting only
+  def complexity_level_number level
+    COMPLEXITIES.fetch(level)
   end
 end
