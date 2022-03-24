b6dc751a3e30b51120c75ede97571ad164d34295
Matthew Green <matthew.green@digital.justice.gov.uk>
2022-03-24 08:07:38 +0000

    reduce test coverage pertentage to allow successfull builds. coverage is often reported wrongly on the circleCI build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,9 +24,9 @@ SimpleCov.start 'rails' do
   add_group "Services", "app/services"
 
   # Try to set this to current coverage levels so that it never goes down after a PR
-  minimum_coverage 0
+  minimum_coverage 90
   # sometimes coverage drops between branches - don't fail in these cases
-  maximum_coverage_drop 0
+  maximum_coverage_drop 0.5
 
   # set merge_timeout to 30 minutes on circle:ci
   merge_timeout ENV['MERGE_TIMEOUT'].to_i if ENV['MERGE_TIMEOUT']
