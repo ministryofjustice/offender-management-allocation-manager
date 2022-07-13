69ee16abea28400361c5825541317aafd854c82f
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2022-07-14 00:32:11 +0100

    Rename a HandoverCaseListingService argument
    MO-1115

--- app/lib/handover_case_listing_service.rb
+++ app/lib/handover_case_listing_service.rb
@@ -3,14 +3,14 @@ class HandoverCaseListingService
     # TODO
   end
 
-  def upcoming_handover_allocated_offenders(pom)
+  def upcoming_handover_allocated_offenders(pom_staff_member)
     # collection = sort_collection(@pom.allocations.select(&:approaching_handover?), default_sort: :last_name)
     #
     # @offenders = Kaminari.paginate_array(collection).page(page)
     # @pending_handover_count = collection.count
     # @prison_total_handovers = @prison.offenders.count(&:approaching_handover?)
 
-    pom.allocations.select(&:in_upcoming_handover_window?)
+    pom_staff_member.allocations.select(&:in_upcoming_handover_window?)
   end
 
   def in_progress
