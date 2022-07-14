58ed7fc734a804fd47d1dd59ed5f6fb61c95ea25
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2022-07-14 11:54:00 +0100

    Upcoming handovers page prisoner detail link goes to prisoner profile
    MO-1115

--- spec/controllers/handovers_controller_spec.rb
+++ spec/controllers/handovers_controller_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe HandoversController, type: :controller do
     allow(controller).to receive(:service_notifications)
     allow(controller).to receive(:load_roles)
     allow(controller).to receive(:ensure_pom)
+    allow(controller).to receive(:active_prison_id).and_return(prison_code)
 
     controller.instance_variable_set(:@current_user, pom_staff_member)
 
@@ -41,6 +42,10 @@ RSpec.describe HandoversController, type: :controller do
 
     it 'renders successfully' do
       expect(response).to be_successful
+    end
+
+    it 'has prison id' do
+      expect(assigns(:prison_id)).to eq prison_code
     end
 
     it 'has counts' do
