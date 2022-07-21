177cff44a85eee2d0bd3f0b81878b6c54e868327
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2022-07-21 15:56:45 +0100

    Dynamic links in help pages now go to the right place
    However, they always use the default prison, and may not work if the
    user does not have relevant permissions
    
    MO-1030

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,3 +1,12 @@
 class HelpController < ApplicationController
   layout 'errors_and_contact'
+
+  def missing_cases
+    @prison_code = default_prison_code
+  end
+
+  def case_responsibility
+    @prison_code = default_prison_code
+    @staff_id = HmppsApi::PrisonApi::UserApi.user_details(current_user).staff_id
+  end
 end
