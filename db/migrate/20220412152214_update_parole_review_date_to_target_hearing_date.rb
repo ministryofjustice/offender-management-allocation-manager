9891f773fdc220d5d110faa6aec28a97cfe4e02c
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2022-08-02 15:49:18 +0100

    Removing problematic migration to now-unused table

--- db/migrate/20220412152214_update_parole_review_date_to_target_hearing_date.rb
+++
@@ -1,5 +0,0 @@
-class UpdateParoleReviewDateToTargetHearingDate < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :parole_records, :parole_review_date, :target_hearing_date
-  end
-end
