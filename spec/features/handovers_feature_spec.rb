aaa385e2bf3a62c687fee6c263a0fe71b2b697dd
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2022-08-09 18:53:22 +0100

    Update handover feature spec for in progress handovers
    MO-1116

--- spec/features/handovers_feature_spec.rb
+++ spec/features/handovers_feature_spec.rb
@@ -7,38 +7,67 @@ RSpec.feature 'Handovers feature:' do
   let(:default_params) do
     {
       prison_id: prison_code,
-      new_handover: 'b00a391b-c672-4c8c-bac8-801bfddaf44d'
+      new_handover: NEW_HANDOVER_TOKEN
     }
+  end
+  let(:offender_attrs) do
+    erd = Faker::Date.forward
+    {
+      full_name: 'Surname1, Firstname1',
+      last_name: 'Surname1',
+      offender_no: 'X1111XX',
+      tier: 'A',
+      earliest_release: { type: 'TED', date: erd },
+      earliest_release_date: erd,
+      case_allocation: 'HDCED'
+    }
+  end
+
+  before do
+    stub_auth_token
+    stub_user(staff_id: user.staff_id)
+
+    signin_pom_user([prison_code])
+    stub_poms(prison_code, [user])
   end
 
   describe 'upcoming handovers' do
     it 'works' do
-      allow_any_instance_of(StaffMember).to receive(:allocations).and_return(
+      allow_any_instance_of(StaffMember).to receive(:unreleased_allocations).and_return(
         [
-          instance_double(AllocatedOffender,
-                          full_name: 'Surname1, Firstname1',
-                          last_name: 'Surname1',
-                          offender_no: 'X1111XX',
-                          tier: 'A',
-                          earliest_release: { type: 'TED', date: Date.new(2022, 1, 30) },
-                          allocated_com_name: nil,
-                          case_allocation: 'HDCED')
+          instance_double(AllocatedOffender, offender_attrs.merge(allocated_com_name: nil))
         ]
       )
       date = FactoryBot.create :calculated_handover_date,
                                offender: FactoryBot.create(:offender, nomis_offender_id: 'X1111XX')
       allow(CalculatedHandoverDate).to receive(:by_upcoming_handover).and_return [date]
 
-      stub_auth_token
-      stub_user(staff_id: user.staff_id)
-
-      signin_pom_user([prison_code])
-      stub_poms(prison_code, [user])
-
       visit upcoming_prison_handovers_path(default_params)
 
       aggregate_failures do
         expect(page.status_code).to eq 200
+        expect(page).to have_text 'Upcoming handovers'
+        expect(page).to have_text 'Surname1, Firstname1 X1111XX'
+      end
+    end
+  end
+
+  describe 'in progress handovers' do
+    it 'works' do
+      allow_any_instance_of(StaffMember).to receive(:unreleased_allocations).and_return(
+        [
+          instance_double(AllocatedOffender, offender_attrs.merge(allocated_com_name: 'Mr COM'))
+        ]
+      )
+      date = FactoryBot.create :calculated_handover_date, :between_com_allocated_and_responsible_dates,
+                               offender: FactoryBot.create(:offender, nomis_offender_id: 'X1111XX')
+      allow(CalculatedHandoverDate).to receive(:by_handover_in_progress).and_return [date]
+
+      visit in_progress_prison_handovers_path(default_params)
+
+      aggregate_failures do
+        expect(page.status_code).to eq 200
+        expect(page).to have_text 'Handovers in progress'
         expect(page).to have_text 'Surname1, Firstname1 X1111XX'
       end
     end
