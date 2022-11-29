53641a107f3569fb57637471c3ead8cc595172d7
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2022-11-29 12:28:30 +0000

    Tidy up and shrink scope of handovers feature spec
    Only tests integration up to domain object and no further
    
    MO-1105

--- spec/features/handovers_feature_spec.rb
+++ spec/features/handovers_feature_spec.rb
@@ -16,29 +16,34 @@ RSpec.feature 'Handovers feature:' do
       earliest_release_date: erd,
       case_allocation: 'HDCED',
       handover_progress_task_completion_data: {},
+      allocated_com_email: nil,
+      allocated_com_name: nil,
     }
+  end
+  let(:offender) { instance_double AllocatedOffender, offender_attrs }
+  let(:calc_handover_date) do
+    instance_double CalculatedHandoverDate, :calc_handover_date, handover_date: Faker::Date.forward
+  end
+  let(:handover_cases_list) do
+    instance_double(HandoverCasesList, :handover_cases_list, upcoming: [],
+                                                             in_progress: [],
+                                                             com_allocation_overdue: [])
   end
 
   before do
-    stub_auth_token
-    stub_user(staff_id: user.staff_id)
     activate_new_handovers_ui
 
+    stub_auth_token
+    stub_user(staff_id: user.staff_id)
     signin_pom_user([prison_code])
     stub_poms(prison_code, [user])
+
+    allow(HandoverCasesList).to receive(:new).and_return(handover_cases_list)
   end
 
   describe 'upcoming handovers' do
     it 'works' do
-      allow_any_instance_of(StaffMember).to receive(:unreleased_allocations).and_return(
-        [
-          instance_double(AllocatedOffender, offender_attrs.merge(allocated_com_name: nil))
-        ]
-      )
-      date = FactoryBot.create :calculated_handover_date, :before_handover,
-                               offender: FactoryBot.create(:offender, nomis_offender_id: 'X1111XX')
-      allow(CalculatedHandoverDate).to receive(:by_upcoming_handover).and_return [date]
-
+      allow(handover_cases_list).to receive(:upcoming).and_return([[calc_handover_date, offender]])
       visit upcoming_prison_handovers_path(default_params)
 
       aggregate_failures do
@@ -51,16 +56,9 @@ RSpec.feature 'Handovers feature:' do
 
   describe 'in progress handovers' do
     it 'works' do
-      allow_any_instance_of(StaffMember).to receive(:unreleased_allocations).and_return(
-        [
-          instance_double(AllocatedOffender, offender_attrs.merge(allocated_com_name: 'Mr COM',
-                                                                  allocated_com_email: 'mr-com@example.org'))
-        ]
-      )
-      date = FactoryBot.create :calculated_handover_date, :after_handover,
-                               offender: FactoryBot.create(:offender, nomis_offender_id: 'X1111XX')
-      allow(CalculatedHandoverDate).to receive(:by_handover_in_progress).and_return [date]
-
+      allow(offender).to receive(:allocated_com_name).and_return('Mr COM')
+      allow(offender).to receive(:allocated_com_email).and_return('mr-com@example.org')
+      allow(handover_cases_list).to receive(:in_progress).and_return([[calc_handover_date, offender]])
       visit in_progress_prison_handovers_path(default_params)
 
       aggregate_failures do
