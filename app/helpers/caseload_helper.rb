a5f1ff216aff2ad8daee464ee928909b41b462e0
Andy White <andy@andywhite.org>
2022-11-30 10:45:29 +0000

    New allocation flow demonstrable

--- app/helpers/caseload_helper.rb
+++ app/helpers/caseload_helper.rb
@@ -5,9 +5,9 @@ module CaseloadHelper
     location = offender.location.presence || (offender.restricted_patient? ? 'Unknown' : 'N/A')
 
     if offender.restricted_patient?
-      return location_only ? location : "This person is being held under the Mental Health Act at #{location}"
+      return location_only ? location : "This person is being held<br />under the Mental Health Act<br />at #{location}".html_safe
     end
 
-    offender.latest_temp_movement_date.blank? ? location : "Temporary absence (out #{format_date(offender.latest_temp_movement_date)})"
+    offender.latest_temp_movement_date.blank? ? location : "Temporary absence<br />(out #{format_date(offender.latest_temp_movement_date)})".html_safe
   end
 end
