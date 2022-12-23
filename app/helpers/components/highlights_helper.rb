167ac4518f357cf3f8b391188546b48f45849e6b
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2022-12-23 17:42:39 +0000

    Test and refactor highlight_tag and highlight_conditionally helpers
    Their APIs did not change so no need to refactor code using them
    
    MO-1175

--- app/helpers/components/highlights_helper.rb
+++ app/helpers/components/highlights_helper.rb
@@ -1,29 +1,26 @@
 module Components
   module HighlightsHelper
     def highlight_tag(type, level, text_content = nil, more_classes: [], &block_content)
-      raise 'Invalid type' unless %w[primary secondary].include?(type)
-      raise 'Invalid level' unless %w[notice alert].include?(level)
+      raise ArgumentError, 'Invalid type' unless %w[primary secondary].include?(type)
+      raise ArgumentError, 'Invalid level' unless %w[notice alert].include?(level)
 
-      content = if text_content.present?
-                  text_content
-                elsif block_content.present?
-                  capture(&block_content)
-                end
-
-      tag.div(content, class: ["highlight-#{type}", "highlight-#{level}"] + more_classes)
+      classes_list = ["highlight-#{type}", "highlight-#{level}"] + more_classes
+      if text_content.present?
+        tag.div(text_content, class: classes_list)
+      elsif block_content.present?
+        tag.div(class: classes_list, &block_content)
+      end
     end
 
     def highlight_conditionally(level, secondary_message, condition, &primary_content)
-      markup = []
-
       if condition.call
-        markup.push highlight_tag('primary', level, &primary_content)
-        markup.push highlight_tag('secondary', level, secondary_message)
+        capture do
+          concat highlight_tag('primary', level, &primary_content)
+          concat highlight_tag('secondary', level, secondary_message)
+        end
       else
-        markup.push capture(&primary_content)
+        capture(&primary_content)
       end
-
-      markup.join.html_safe
     end
   end
 end
