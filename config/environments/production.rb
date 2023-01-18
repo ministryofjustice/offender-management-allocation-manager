4313ce231da8d429266c946e0254b3f971f04900
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-01-18 12:57:07 +0000

    Allow 'consider_all_requests_local' to be configured by env vars
    MO-1183

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = ENV['CONSIDER_ALL_REQUESTS_LOCAL'].present?
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
