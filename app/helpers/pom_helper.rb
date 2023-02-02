e001554ea46b4124d7a41f4382e6a887e2aff0db
Andy White <andy@andywhite.org>
2023-02-02 11:30:59 +0000

    Add more display values to Choose POM page
    Start adding column sorting

--- app/helpers/pom_helper.rb
+++ app/helpers/pom_helper.rb
@@ -35,6 +35,10 @@ module PomHelper
     "#{pom.position_description.split(' ').first} POM"
   end
 
+  def full_grade(pom)
+    "#{pom.position_description.split(' ').first} Officer POM"
+  end
+
   def status(pom)
     # we are now displaying 'Available', instead of 'Active' which is stored in the database
     pom.status == 'active' ? 'available' : pom.status
