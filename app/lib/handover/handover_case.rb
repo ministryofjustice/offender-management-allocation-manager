47c43263244dc0e55401759eca1f8ffa836f2027
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-02-08 19:55:30 +0000

    CategorisedHandoverCases now build HandoverCase objects
    They used to produce a tuple of [something, something] which is a bit
    icky and doesn't work well with the built-in controller sorting
    mechanics.
    
    We were always looking to move towards domain-specific composite
    objects anyway (instead of the effing Offender wrapper abominations)
    
    MO-1191

---
+++ app/lib/handover/handover_case.rb
@@ -0,0 +1,23 @@
+class Handover::HandoverCase
+  # we pass calculated handover date explicitly instead of finding it ourselves because: (1) its not our job to find it,
+  # and (2) the CategorisedHandoverCases factories will have found them already when it initialises this class
+  def initialize(calculated_handover_date, allocated_offender)
+    raise ArgumentError unless calculated_handover_date.is_a?(CalculatedHandoverDate)
+    raise ArgumentError unless allocated_offender.is_a?(AllocatedOffender)
+
+    @offender = allocated_offender
+    @calculated_handover_date = calculated_handover_date
+  end
+
+  attr_reader :offender, :calculated_handover_date
+
+  def ==(other)
+    [@offender, @calculated_handover_date] == [other.offender, other.calculated_handover_date]
+  end
+
+  delegate :last_name, to: :offender, prefix: true
+  delegate :staff_member, :allocated_com_name, :earliest_release_date, :case_allocation, :handover_progress_complete?,
+           to: :offender
+  delegate :last_name, to: :staff_member, prefix: true
+  delegate :handover_date, to: :calculated_handover_date
+end
