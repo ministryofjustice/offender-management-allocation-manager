7ee5b65c206747e84dc119e62d2fe16fc6bcabc7
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-02-14 11:51:21 +0000

    Add batch job and rake task to deliver upcoming handover emails
    MO-1114

---
+++ app/lib/handover/handover_email_batch_run.rb
@@ -0,0 +1,30 @@
+class Handover::HandoverEmailBatchRun
+  def self.send_all_upcoming_handover_window(for_date: Time.zone.now.to_date)
+    Rails.logger.info("event=start_handover_email_batch_run,email=upcoming_handover_window,for_date=#{for_date.iso8601}")
+    AllocatedOffender.all.each do |offender|
+      send_one_upcoming_handover_window(offender, for_date: for_date)
+    end
+  end
+
+  def self.send_one_upcoming_handover_window(offender, deliver_now: false, for_date: Time.zone.now.to_date)
+    chd = CalculatedHandoverDate.find_by(nomis_offender_id: offender.offender_no)
+    return unless chd.handover_date == for_date + DEFAULT_UPCOMING_HANDOVER_WINDOW_DURATION
+
+    Handover::HandoverEmail.deliver_if_deliverable(
+      :upcoming_handover_window,
+      offender.offender_no,
+      email: offender.staff_member.email_address,
+      full_name_ordered: offender.full_name_ordered,
+      first_name: offender.first_name,
+      handover_date: format_date(chd.handover_date),
+      service_provider: offender.case_allocation,
+      release_date: format_date(offender.earliest_release_date),
+      deliver_now: deliver_now,
+    )
+    Rails.logger.info("event=handover_email_delivered,nomis_offender_id=#{offender.offender_no},email=upcoming_handover_window,for_date=#{for_date.iso8601}")
+  end
+
+  def self.format_date(date)
+    date.strftime('%-d %b %Y')
+  end
+end
