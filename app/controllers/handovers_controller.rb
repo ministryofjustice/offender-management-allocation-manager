9d73e32552ea397c8731833dda9aab101ea2329a
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-02-15 11:18:23 +0000

    Handovers: Bring back modified feature flag
    It doesn't show a popup anymore, it alters the site to have new
    handovers navigation and views.

--- app/controllers/handovers_controller.rb
+++ app/controllers/handovers_controller.rb
@@ -6,19 +6,22 @@ class HandoversController < PrisonsApplicationController
   layout 'handovers'
 
   before_action :check_prerequisites_and_prepare_variables, except: :index
-  # before_action :ensure_spo_user, only: :index
+  before_action :ensure_spo_user, only: :index
 
   def index
-    redirect_to upcoming_prison_handovers_path
-    # @pending_handover_count = @current_user.allocations.count(&:approaching_handover?)
-    # offender_list = @prison.offenders.select(&:approaching_handover?)
-    # allocations = @prison.allocations.where(nomis_offender_id: offender_list.map(&:offender_no))
-    # offenders_with_allocs = offender_list.map do |o|
-    #   OffenderWithAllocationPresenter.new(o, allocations.detect { |a| a.nomis_offender_id == o.offender_no })
-    # end
-    # offenders = sort_collection offenders_with_allocs, default_sort: :last_name
-    # @offenders = Kaminari.paginate_array(offenders).page(page)
-    # render :legacy_index, layout: 'application'
+    if session[:new_handovers_ui] == true
+      redirect_to upcoming_prison_handovers_path
+    else
+      @pending_handover_count = @current_user.allocations.count(&:approaching_handover?)
+      offender_list = @prison.offenders.select(&:approaching_handover?)
+      allocations = @prison.allocations.where(nomis_offender_id: offender_list.map(&:offender_no))
+      offenders_with_allocs = offender_list.map do |o|
+        OffenderWithAllocationPresenter.new(o, allocations.detect { |a| a.nomis_offender_id == o.offender_no })
+      end
+      offenders = sort_collection offenders_with_allocs, default_sort: :last_name
+      @offenders = Kaminari.paginate_array(offenders).page(page)
+      render :legacy_index, layout: 'application'
+    end
   end
 
   def upcoming
@@ -44,11 +47,6 @@ private
   end
 
   def check_prerequisites_and_prepare_variables
-    # unless session[:new_handovers_ui] == true
-    #   redirect_to '/401'
-    #   return
-    # end
-
     if params[:sort].blank?
       redirect_to permitted_params.merge(sort: 'handover_date asc')
       return
