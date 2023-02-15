ecd8e8cb0dd0d5791959386deeb59a6e5f027d7f
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-02-15 22:12:39 +0000

    Gracefully handle and log errors batch sending handover reminder emails
    MO-1114

--- app/lib/handover/handover_email_batch_run.rb
+++ app/lib/handover/handover_email_batch_run.rb
@@ -1,7 +1,7 @@
 class Handover::HandoverEmailBatchRun
   def self.send_all_upcoming_handover_window(for_date: Time.zone.now.to_date)
     Rails.logger.info("event=start_handover_email_batch_run,email=upcoming_handover_window,for_date=#{for_date.iso8601}")
-    AllocatedOffender.all.each do |offender|
+    for_all_allocated_offenders('upcoming_handover_window') do |offender|
       send_one_upcoming_handover_window(offender, for_date: for_date)
     end
   end
@@ -27,7 +27,7 @@ class Handover::HandoverEmailBatchRun
 
   def self.send_all_handover_date(for_date: Time.zone.now.to_date)
     Rails.logger.info("event=start_handover_email_batch_run,email=handover_date,for_date=#{for_date.iso8601}")
-    AllocatedOffender.all.each do |offender|
+    for_all_allocated_offenders('handover_date') do |offender|
       send_one_handover_date(offender, for_date: for_date)
     end
   end
@@ -54,7 +54,8 @@ class Handover::HandoverEmailBatchRun
 
   def self.send_all_com_allocation_overdue(for_date: Time.zone.now.to_date)
     Rails.logger.info("event=start_handover_email_batch_run,email=com_allocation_overdue,for_date=#{for_date.iso8601}")
-    AllocatedOffender.all.each do |offender|
+
+    for_all_allocated_offenders('com_allocation_overdue') do |offender|
       send_one_com_allocation_overdue(offender, for_date: for_date)
     end
   end
@@ -78,7 +79,19 @@ class Handover::HandoverEmailBatchRun
     Rails.logger.info("event=handover_email_delivered,nomis_offender_id=#{offender.offender_no},email=com_allocation_overdue,for_date=#{for_date.iso8601}")
   end
 
-  def self.format_date(date)
-    date.strftime('%-d %B %Y')
+  class << self
+    def format_date(date)
+      date.strftime('%-d %B %Y')
+    end
+
+    def for_all_allocated_offenders(email_type)
+      AllocatedOffender.all.each do |offender|
+        yield(offender)
+      rescue StandardError => e
+        raise unless Rails.env.production?
+
+        Rails.logger.error("event=handover_email_batch_run_error,email=#{email_type},nomis_offender_id=#{offender.offender_no}|#{e.inspect},#{e.backtrace.join(',')}")
+      end
+    end
   end
 end
