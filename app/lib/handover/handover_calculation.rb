2fc515734568b7d9f2dff33aa0f1c2b6007ad67b
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-03-14 11:33:17 +0000

    Add handover date calculations for Apr 2023 handover policy change
    MO-1235

---
+++ app/lib/handover/handover_calculation.rb
@@ -0,0 +1,15 @@
+class Handover::HandoverCalculation
+  class << self
+    def calculate_handover_date(sentence_start_date:,
+                                earliest_release_date:,
+                                is_determinate:)
+      raise NotImplementedError unless is_determinate
+
+      if sentence_start_date + 10.months >= earliest_release_date
+        nil
+      else
+        earliest_release_date - 8.months - 14.days
+      end
+    end
+  end
+end
---
+++ spec/lib/handover/handover_calculation_spec.rb
@@ -0,0 +1,53 @@
+RSpec.describe Handover::HandoverCalculation do
+  let(:sentence_start_date) { Date.new(2024, 1, 1) }
+
+  describe '::calculate_handover_date' do
+    describe 'for determinate case' do
+      describe 'when earliest release date is 1 day less than 10 months after sentence start' do
+        example 'there is no handover' do
+          result = described_class.calculate_handover_date(sentence_start_date: sentence_start_date,
+                                                           earliest_release_date: Date.new(2024, 10, 31),
+                                                           is_determinate: true)
+          expect(result).to eq nil
+        end
+      end
+
+      describe 'when earliest release date is exactly 10 months after sentence start' do
+        example 'there is no handover' do
+          result = described_class.calculate_handover_date(sentence_start_date: sentence_start_date,
+                                                           earliest_release_date: Date.new(2024, 11, 1),
+                                                           is_determinate: true)
+          expect(result).to eq nil
+        end
+      end
+
+      describe 'when earliest release date is 10 months and 1 day after sentence start' do
+        example 'handover date is 8 months 14 days before earliest release date' do
+          result = described_class.calculate_handover_date(sentence_start_date: sentence_start_date,
+                                                           earliest_release_date: Date.new(2024, 11, 2),
+                                                           is_determinate: true)
+          expect(result).to eq Date.new(2024, 2, 17)
+        end
+      end
+
+      describe 'when earliest release date is 10 months and 2 day after sentence start' do
+        example 'handover date is 8 months 14 days before earliest release date' do
+          result = described_class.calculate_handover_date(sentence_start_date: sentence_start_date,
+                                                           earliest_release_date: Date.new(2024, 11, 3),
+                                                           is_determinate: true)
+          expect(result).to eq Date.new(2024, 2, 18)
+        end
+      end
+    end
+
+    describe 'for indeterminate case' do
+      it 'raises an error' do
+        expect {
+          described_class.calculate_handover_date(sentence_start_date: sentence_start_date,
+                                                  earliest_release_date: sentence_start_date + 1.year,
+                                                  is_determinate: false)
+        }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+end
