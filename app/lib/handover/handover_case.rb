2f82f32e809e4c1ef36cb74b6aed65d8dce15269
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-03-15 17:45:26 +0000

    Move earliest_release_for_handover to MpcOffender
    Delegate to it from HandoverCase and AllocatedOffender
    
    MO-1235

--- app/lib/handover/handover_case.rb
+++ app/lib/handover/handover_case.rb
@@ -16,7 +16,8 @@ class Handover::HandoverCase
   end
 
   delegate :last_name, to: :offender, prefix: true
-  delegate :staff_member, :allocated_com_name, :tier, :handover_progress_complete?, to: :offender
+  delegate :staff_member, :allocated_com_name, :tier, :handover_progress_complete?,
+           :earliest_release_for_handover, to: :offender
   delegate :last_name, to: :staff_member, prefix: true
   delegate :handover_date, to: :calculated_handover_date
 
@@ -24,31 +25,6 @@ class Handover::HandoverCase
     raise ArgumentError, 'Handover date not set' unless handover_date
 
     (relative_to_date - handover_date).to_i
-  end
-
-  # We can not calculate the handover date for NPS Indeterminate
-  # with parole cases where the TED is in the past as we need
-  # the parole board decision which currently is not available to us.
-  def earliest_release_for_handover
-    if offender.indeterminate_sentence?
-      if offender.tariff_date&.future?
-        NamedDate[offender.tariff_date, 'TED']
-      else
-        [
-          NamedDate[offender.parole_review_date, 'PRD'],
-          NamedDate[offender.parole_eligibility_date, 'PED'],
-        ].compact.reject { |nd| nd.date.past? }.min
-      end
-    elsif offender.case_information&.nps_case?
-      possible_dates = [NamedDate[offender.conditional_release_date, 'CRD'], NamedDate[offender.automatic_release_date, 'ARD']]
-      NamedDate[offender.parole_eligibility_date, 'PED'] || possible_dates.compact.min
-    else
-      # CRC can look at HDC date, NPS is not supposed to
-      NamedDate[offender.home_detention_curfew_actual_date, 'HDCEA'] ||
-        [NamedDate[offender.automatic_release_date, 'ARD'],
-         NamedDate[offender.conditional_release_date, 'CRD'],
-         NamedDate[offender.home_detention_curfew_eligibility_date, 'HDCED']].compact.min
-    end
   end
 
   def earliest_release_date
