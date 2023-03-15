da3edb4c7ef257c57e200749861d32e935aa299f
Andy White <andy@andywhite.org>
2023-03-15 15:11:14 +0000

    Upgrade sentry-raven to sentry-ruby

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,19 @@
 sentry_dsn = Rails.configuration.sentry_dsn
+
 if sentry_dsn
-  Raven.configure do |config|
+  param_filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+
+  Sentry.init do |config|
     config.dsn = sentry_dsn
     config.excluded_exceptions << 'JWT::ExpiredSignature'
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 
     # If we're in Heroku, set the environment name to be the current app name
     # This allows us to tell which PR/Review App an error came from
-    config.current_environment = ENV['HEROKU_APP_NAME'] if ENV['HEROKU_APP_NAME'].present?
+    config.environment = ENV['HEROKU_APP_NAME'] if ENV['HEROKU_APP_NAME'].present?
+
+    config.before_send = lambda do |event, _hint|
+      SentryCircuitBreakerService.check_within_quota ? param_filter.filter(event.to_hash) : nil
+    end
   end
 else
   Rails.logger.warn '[WARN] Sentry is not configured (SENTRY_DSN)'
