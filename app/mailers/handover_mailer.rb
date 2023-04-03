b25f4b7cc0569efb2108892c246ca79cbc62a9dd
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-04-03 17:13:17 +0100

    Handover reminder emails updated to reflect April 2023 changes
    MO-1238

--- app/mailers/handover_mailer.rb
+++ app/mailers/handover_mailer.rb
@@ -6,13 +6,13 @@ class HandoverMailer < GovukNotifyRails::Mailer
                                handover_date:,
                                service_provider:,
                                release_date:)
-    set_template('38f1d962-7b36-44d2-a78d-c6fb8aaaffcb')
+    set_template('7114ad9e-e71a-4424-a884-bcc72bd1a569')
     set_personalisation(nomis_offender_id: nomis_offender_id,
                         full_name_ordered: full_name_ordered,
                         first_name: first_name,
                         handover_date: handover_date,
-                        service_provider: service_provider,
-                        is_nps: service_provider == 'NPS' ? 'yes' : 'no',
+                        is_standard: service_provider == 'NPS' ? 'no' : 'yes',
+                        is_enhanced: service_provider == 'NPS' ? 'yes' : 'no',
                         release_date: release_date)
     mail(to: email)
   end
@@ -25,15 +25,14 @@ class HandoverMailer < GovukNotifyRails::Mailer
                     com_name:,
                     com_email:,
                     service_provider:)
-    set_template('7eaafff6-dae8-4eb2-bca5-bb530e0f1078')
+    set_template('95ddd96c-23f9-4066-b033-d4a1d83b702e')
     set_personalisation(nomis_offender_id: nomis_offender_id,
                         full_name_ordered: full_name_ordered,
                         first_name: first_name,
-                        service_provider: service_provider,
                         com_name: com_name,
                         com_email: com_email,
-                        is_nps: service_provider == 'NPS' ? 'yes' : 'no',
-                        is_crc: service_provider == 'CRC' ? 'yes' : 'no',
+                        is_standard: service_provider == 'NPS' ? 'no' : 'yes',
+                        is_enhanced: service_provider == 'NPS' ? 'yes' : 'no',
                         release_date: release_date)
     mail(to: email)
   end
@@ -46,7 +45,7 @@ class HandoverMailer < GovukNotifyRails::Mailer
                              ldu_name:,
                              ldu_email:,
                              service_provider:)
-    set_template('be1ea651-be9d-4fb0-84cd-60af0556c8f9')
+    set_template('21d34a34-f2ec-42c2-82b3-720899b58a3b')
 
     ldu_information = ''
     ldu_information += "LDU: #{ldu_name}\n" if ldu_name.present?
@@ -54,9 +53,10 @@ class HandoverMailer < GovukNotifyRails::Mailer
 
     set_personalisation(nomis_offender_id: nomis_offender_id,
                         full_name_ordered: full_name_ordered,
-                        service_provider: service_provider,
                         handover_date: handover_date,
                         release_date: release_date,
+                        is_standard: service_provider == 'NPS' ? 'no' : 'yes',
+                        is_enhanced: service_provider == 'NPS' ? 'yes' : 'no',
                         ldu_information: ldu_information,
                         has_ldu_email: ldu_email.present?,
                         missing_ldu_email: ldu_email.blank?)
