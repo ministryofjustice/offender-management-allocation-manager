ff8500a2576071c84f0c96f786ca9778a7819159
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-04-17 16:50:53 +0100

    Add a view test for the logic changes
    MO-1243

--- spec/views/allocations/show.html.erb_spec.rb
+++ spec/views/allocations/show.html.erb_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "allocations/show", type: :view do
   let(:offender) { build(:mpc_offender, prison: prison, offender: case_info.offender, prison_record: api_offender) }
 
   before do
-    assign(:prison, create(:prison))
+    assign(:prison, prison)
     assign(:pom, build(:pom))
     assign(:prisoner, offender)
     assign(:allocation, create(:allocation_history, prison: build(:prison).code))
@@ -59,6 +59,40 @@ RSpec.describe "allocations/show", type: :view do
       it 'shows "Unknown"' do
         expect(key).to eq('Category')
         expect(value).to eq('Unknown')
+      end
+    end
+  end
+
+  describe 'with mocking' do
+    let(:nomis_offender_id) { FactoryBot.generate :nomis_offender_id }
+
+    before do
+      allow(offender).to receive_messages(
+        pom_responsible?: true,
+        com_responsible?: false,
+        pom_supporting?: true,
+        responsibility_override?: false,
+      )
+      stub_template 'shared/_badges.html.erb' => ''
+      allow(view).to receive(:vlo_tag).and_return('')
+      allow(view).to receive(:prisoner_location).and_return('')
+      assign(:prisoner, offender)
+    end
+
+    describe 'responsibility' do
+      it 'allows POM responsible cases to have responsibility overridden' do
+        render
+        expect(page).to have_css ".responsibility_change a[href='#{new_prison_responsibility_path(prison.code, nomis_offender_id: offender.offender_no)}']"
+      end
+
+      it 'allows COM responsible overrides to be deleted' do
+        allow(offender).to receive_messages(
+          pom_responsible?: false,
+          com_responsible?: true,
+          responsibility_override?: true,
+        )
+        render
+        expect(page).to have_css ".responsibility_change a[href='#{confirm_removal_prison_responsibility_path(prison.code, nomis_offender_id: offender.offender_no)}']"
       end
     end
   end
