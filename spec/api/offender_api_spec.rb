704d1051ab5ab7755777bd27cc7ee6b0ad10907e
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-05-10 10:47:26 +0100

    Make API specs simpler by mocking out token validation/authorisation
    MO-1288

--- spec/api/offender_api_spec.rb
+++ spec/api/offender_api_spec.rb
@@ -3,20 +3,7 @@
 require 'swagger_helper'
 
 describe 'Offender Early Allocation API', vcr: { cassette_name: 'prison_api/offender_api' } do
-  let!(:private_key) { OpenSSL::PKey::RSA.generate 2048 }
-  let!(:public_key) { Base64.strict_encode64(private_key.public_key.to_s) }
-  let!(:payload) do
-    {
-      user_name: 'test-user',
-      scope: ['read'],
-      exp: 4.hours.from_now.to_i
-    }
-  end
-  let!(:token) { JWT.encode payload, private_key, 'RS256' }
-
-  before do
-    allow(Rails.configuration).to receive(:nomis_oauth_public_key).and_return(public_key)
-  end
+  let(:Authorization) { "Bearer TEST_TOKEN" }
 
   path '/api/offenders/{offender_no}' do
     get 'Retrieves the early allocation status for an prisoner' do
@@ -24,54 +11,59 @@ describe 'Offender Early Allocation API', vcr: { cassette_name: 'prison_api/offe
       produces 'application/json'
       parameter name: :offender_no, in: :path, type: :string
 
-      response '200', 'Offender has an early allocation' do
-        security [Bearer: []]
-        schema type: :object,
-               properties: {
-                 offender_no: { type: :string },
-                 early_allocation_eligibility_status: {
-                   type: :boolean,
-                   description: "true if prisoner is subject to early allocation, and so has an early handover date"
+      describe 'when not authorised' do
+        response '401', 'Request is not authorised' do
+          security [Bearer: []]
+          schema type: :object,
+                 properties: {
+                   status: { type: :string },
+                   message: { type: :string }
                  }
-               },
-               required: %w[offender_no early_allocation_eligibility_status]
 
-        let(:offender_no) { 'G7266VD' }
-        let!(:allocation) do
-          create(:case_information, offender: build(:offender, nomis_offender_id: offender_no, early_allocations: build_list(:early_allocation, 1)))
-        end
-        let(:Authorization) { "Bearer #{token}" }
-
-        run_test! do |_|
-          expect(JSON.parse(response.body).fetch('offender_no')).to eq(offender_no)
-          expect(JSON.parse(response.body).fetch('early_allocation_eligibility_status')).to eq(true)
+          let(:offender_no) { 'A1111AA' }
+          run_test!
         end
       end
 
-      response '401', 'Request is not authorised' do
-        security [Bearer: []]
-        schema type: :object,
-               properties: {
-                 status: { type: :string },
-                 message: { type: :string }
-               }
+      describe 'when authorised' do
+        before do
+          allow_any_instance_of(Api::OffendersController).to receive(:verify_token)
+        end
 
-        let(:offender_no) { 'A1111AA' }
-        let(:Authorization) { "Bearer missing" }
-        run_test!
-      end
+        response '200', 'Offender has an early allocation' do
+          security [Bearer: []]
+          schema type: :object,
+                 properties: {
+                   offender_no: { type: :string },
+                   early_allocation_eligibility_status: {
+                     type: :boolean,
+                     description: "true if prisoner is subject to early allocation, and so has an early handover date"
+                   }
+                 },
+                 required: %w[offender_no early_allocation_eligibility_status]
 
-      response '404', 'offender not found' do
-        security [Bearer: []]
-        schema type: :object,
-               properties: {
-                 status: { type: :string },
-                 message: { type: :string }
-               }
+          let(:offender_no) { 'G7266VD' }
+          let!(:allocation) do
+            create(:case_information, offender: build(:offender, nomis_offender_id: offender_no, early_allocations: build_list(:early_allocation, 1)))
+          end
 
-        let(:offender_no) { 'A1111AA' }
-        let(:Authorization) { "Bearer #{token}" }
-        run_test!
+          run_test! do |_|
+            expect(JSON.parse(response.body).fetch('offender_no')).to eq(offender_no)
+            expect(JSON.parse(response.body).fetch('early_allocation_eligibility_status')).to eq(true)
+          end
+        end
+
+        response '404', 'offender not found' do
+          security [Bearer: []]
+          schema type: :object,
+                 properties: {
+                   status: { type: :string },
+                   message: { type: :string }
+                 }
+
+          let(:offender_no) { 'A1111AA' }
+          run_test!
+        end
       end
     end
   end
