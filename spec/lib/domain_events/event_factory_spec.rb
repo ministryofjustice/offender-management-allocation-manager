509810c332c857c03cda9a613d0bb64de446a840
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-05-15 16:04:12 +0100

    DomainEvents::EventFactory added
    MO-1226

---
+++ spec/lib/domain_events/event_factory_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe DomainEvents::EventFactory do
+  let(:event) { instance_double DomainEvents::Event, :event }
+
+  before do
+    allow(DomainEvents::Event).to receive(:new).and_return(event)
+  end
+
+  it 'builds handover events' do
+    response = described_class.build_handover_event(noms_number: 'X1111XX', host: 'https://example.com')
+
+    expect(response).to eq event
+    expect(DomainEvents::Event).to have_received(:new).with(
+      event_type: 'domain-events.handover.changed',
+      version: 1,
+      description: 'Handover date and/or responsibility was updated',
+      detail_url: 'https://example.com/handovers/X1111XX',
+      noms_number: 'X1111XX',
+    )
+  end
+end
