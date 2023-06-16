480a847501e3ec480b3e9d97f050293ef57d26d7
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-06-16 10:06:12 +0100

    Get more visibility into the MailPublishAuditEventObserver
    Why is it not working on test2 when I allocate an offender?
    
    MO-1300

--- app/lib/mail_publish_audit_event_observer.rb
+++ app/lib/mail_publish_audit_event_observer.rb
@@ -1,8 +1,12 @@
 class MailPublishAuditEventObserver
   def self.delivered_email(message)
-    return unless message.respond_to?(:govuk_notify_personalisation)
+    unless message.respond_to?(:govuk_notify_personalisation)
+      Rails.logger.error "event=email_not_auditable_error|#{message.inspect}"
+      return
+    end
 
     params = message.govuk_notify_personalisation
+    Rails.logger.error "event=audit_event_created,nomis_offender_id=#{params[:nomis_offender_id]}|#{message.inspect}"
     AuditEvent.create!(
       nomis_offender_id: params[:nomis_offender_id],
       tags: message.govuk_notify_reference || [],
