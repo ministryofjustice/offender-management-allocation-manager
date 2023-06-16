27ff786474dc44102cf3eb67aea3cd6eba3708fa
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-06-16 15:13:14 +0100

    Audit events page is now sortable and searchable by noms number
    MO-1300

--- app/controllers/manage/audit_events_controller.rb
+++ app/controllers/manage/audit_events_controller.rb
@@ -1,5 +1,12 @@
 class Manage::AuditEventsController < PrisonsApplicationController
   def index
-    @audit_events = AuditEvent.order(published_at: :desc).all
+    @nomis_offender_id = params[:nomis_offender_id]
+    query = AuditEvent.order(published_at: :desc)
+
+    if @nomis_offender_id.present?
+      query = query.where(nomis_offender_id: @nomis_offender_id)
+    end
+
+    @audit_events = query.page params[:page]
   end
 end
