bda92a03ff07a2910b2c964f7e517ab6ed60b617
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-06-16 12:59:55 +0100

    Using reference for an array of tags broke - use a string with dots
    MO-1300

--- app/lib/mail_publish_audit_event_observer.rb
+++ app/lib/mail_publish_audit_event_observer.rb
@@ -1,15 +1,16 @@
 class MailPublishAuditEventObserver
   def self.delivered_email(message)
     unless message.respond_to?(:govuk_notify_personalisation)
-      Rails.logger.error "event=email_not_auditable_error|#{message.inspect}"
+      Rails.logger.error "event=email_not_auditable_error,reason=no_govuk_notify_support|#{message.inspect}"
       return
     end
 
     params = message.govuk_notify_personalisation
+    tags = message.govuk_notify_reference.present? ? message.govuk_notify_reference.split('.') : []
     Rails.logger.error "event=audit_event_created,nomis_offender_id=#{params[:nomis_offender_id]}|#{message.inspect}"
     AuditEvent.create!(
       nomis_offender_id: params[:nomis_offender_id],
-      tags: message.govuk_notify_reference || [],
+      tags: tags,
       published_at: Time.zone.now.utc,
       system_event: true,
       data: {
--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -3,13 +3,13 @@ RSpec.describe ApplicationMailer do
     it 'uses default tags based on mailer name' do
       TestOnlyMailer.set_mailer_tag nil
       mail = TestOnlyMailer.with(template: 'x', personalisation: {}).test_mail
-      expect(mail.govuk_notify_reference).to eq ['email', 'test_only', 'test_mail']
+      expect(mail.govuk_notify_reference).to eq 'email.test_only.test_mail'
     end
 
     it 'allows mailer name tag to be configured' do
       TestOnlyMailer.set_mailer_tag 'only_for_testing'
       mail = TestOnlyMailer.with(template: 'x', personalisation: {}).test_mail
-      expect(mail.govuk_notify_reference).to eq ['email', 'only_for_testing', 'test_mail']
+      expect(mail.govuk_notify_reference).to eq 'email.only_for_testing.test_mail'
     end
   end
 end
