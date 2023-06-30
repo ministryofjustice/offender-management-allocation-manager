f7c433ca379dc1e848292e52cb8482784b246539
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-06-30 18:54:27 +0100

    AuditEvent::publish is now the API for creating them
    Does some extra actions behind the scenese as well as creating the
    record
    
    MO-1266

--- app/models/audit_event.rb
+++ app/models/audit_event.rb
@@ -1,5 +1,21 @@
 class AuditEvent < ApplicationRecord
   class << self
+    def publish(**attrs)
+      attrs = attrs.stringify_keys
+      attrs['published_at'] ||= Time.zone.now.utc
+      record = create!(**attrs)
+
+      system_log = [
+        'event=audit_event_published',
+        "nomis_offender_id=#{record.nomis_offender_id}",
+        "audit_event_id=#{record.id}",
+        *record.tags.map { |t| "tag=#{t}" },
+      ].join(',')
+      Rails.logger.info system_log
+
+      record
+    end
+
     def tags(*tags)
       where('lower(ARRAY[?]::text)::text[] <@ tags', tags)
     end
--- spec/models/audit_event_spec.rb
+++ spec/models/audit_event_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe AuditEvent do
+  let(:attributes) { FactoryBot.attributes_for(:audit_event, :system, tags: %w[tag1 tag2]).except(:published_at) }
+
   it 'can have username and user_human_name if system_event is false', :aggregate_failures do
     event = FactoryBot.create :audit_event, :user
     expect(event.system_event?).to eq false
@@ -11,6 +13,29 @@ RSpec.describe AuditEvent do
       .to raise_error(/PG::CheckViolation: ERROR:.+audit_events.+system_event_cannot_have_user_details/)
     expect { FactoryBot.create :audit_event, :system, user_human_name: 'test' }
       .to raise_error(/PG::CheckViolation: ERROR:.+audit_events.+system_event_cannot_have_user_details/)
+  end
+
+  describe 'when published' do
+    it 'creates a record' do
+      expect { described_class.publish(**attributes) }
+        .to change(described_class, :count).by(1)
+    end
+
+    it 'logs the audit event to the system/Rails logger', :aggregate_failures do
+      line = ''
+      allow(Rails.logger).to receive(:info) { |data| line.replace(data) }
+      record = described_class.publish(**attributes)
+
+      expect(line).to match('event=audit_event_published')
+      expect(line).to match(record.nomis_offender_id)
+      expect(line).to match(record.id)
+      expect(line).to match(/tag1.+tag2/)
+    end
+
+    it 'saves a published_at date' do
+      record = described_class.publish(**attributes)
+      expect(record.published_at).to be_within(1.second).of(Time.zone.now.utc)
+    end
   end
 
   describe '::tags' do
