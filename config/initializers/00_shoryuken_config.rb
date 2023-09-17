bd0d688c08d04b12898d9f26fcf32b6f84b93f3f
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-09-17 17:04:47 +0100

    Allow Shoryuken to use localstack
    MO-1357

--- config/initializers/00_shoryuken_config.rb
+++ config/initializers/00_shoryuken_config.rb
@@ -1,10 +1,14 @@
-Shoryuken.configure_server do |_config|
-  # Replace Rails logger so messages are logged wherever Shoryuken is logging
-  # Note: this entire block is only run by the processor, so we don't overwrite
+Shoryuken.configure_server do |config|
+  # NOTE: this entire block is only run by the processor, so we don't overwrite
   #       the logger when the app is running as usual.
+
   Rails.logger = Shoryuken::Logging.logger
 
-  # config.server_middleware do |chain|
-  #   chain.add Shoryuken::MyMiddleware
-  # end
+  localstack_url = ENV['LOCALSTACK_URL']
+  if localstack_url.present?
+    config.sqs_client = Aws::SQS::Client.new(
+      endpoint: localstack_url,
+      verify_checksums: false,
+    )
+  end
 end
