59fb9da3c78352121f8c4d4be426a06925c410a5
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-09-18 16:25:09 +0100

    Significantly shorten the prefix of Shoryuken log entries
    MO-1357

--- config/initializers/00_shoryuken_config.rb
+++ config/initializers/00_shoryuken_config.rb
@@ -2,6 +2,9 @@ Shoryuken.configure_server do |config|
   # NOTE: this entire block is only run by the processor, so we don't overwrite
   #       the logger when the app is running as usual.
 
+  Shoryuken::Logging.logger.formatter = lambda do |severity, datetime, _, msg|
+    "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}] #{severity.ljust(5)}: #{msg}\n"
+  end
   Rails.logger = Shoryuken::Logging.logger
 
   localstack_url = ENV['LOCALSTACK_URL']
