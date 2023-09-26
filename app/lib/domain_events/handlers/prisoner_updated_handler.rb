f04aa35888da503446b0e754a7214ef2f283f157
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-09-26 14:49:47 +0100

    Log reason for skip prisoner search events for cases...
    ...without probation info
    
    MO-1388

--- app/lib/domain_events/handlers/prisoner_updated_handler.rb
+++ app/lib/domain_events/handlers/prisoner_updated_handler.rb
@@ -4,7 +4,11 @@ class DomainEvents::Handlers::PrisonerUpdatedHandler
 
     nomis_offender_id = event.additional_information.fetch('nomsNumber')
 
-    return if CaseInformation.find_by_nomis_offender_id(nomis_offender_id).nil?
+    if CaseInformation.find_by_nomis_offender_id(nomis_offender_id).nil?
+      Shoryuken::Logging.logger.info("event=domain_event_handle_skip,domain_event_type=#{event.event_type}," \
+                                     "nomis_offender_id=#{nomis_offender_id},reason=missing_probation_case_information")
+      return
+    end
 
     Shoryuken::Logging.logger.info "event=domain_event_handle_start,domain_event_type=#{event.event_type},nomis_offender_id=#{nomis_offender_id}"
     categories = event.additional_information.fetch('categoriesChanged')
