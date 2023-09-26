f218f69ca7a944ceb6a15e400ee96421e764351c
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-09-26 11:57:14 +0100

    Only calculate handover data changes if probation data exists
    If not, we wait to sync with Delius to calculate handover data changes.
    
    This will stop many errors in prisoner case information event handling
    
    MO-1388

--- app/lib/domain_events/handlers/prisoner_updated_handler.rb
+++ app/lib/domain_events/handlers/prisoner_updated_handler.rb
@@ -3,6 +3,9 @@ class DomainEvents::Handlers::PrisonerUpdatedHandler
     return unless ENABLE_EVENT_BASED_HANDOVER_CALCULATION
 
     nomis_offender_id = event.additional_information.fetch('nomsNumber')
+
+    return if CaseInformation.find_by_nomis_offender_id(nomis_offender_id).nil?
+
     Shoryuken::Logging.logger.info "event=domain_event_handle_start,domain_event_type=#{event.event_type},nomis_offender_id=#{nomis_offender_id}"
     categories = event.additional_information.fetch('categoriesChanged')
     RecalculateHandoverDateJob.perform_now(nomis_offender_id) if categories.include?('SENTENCE')
