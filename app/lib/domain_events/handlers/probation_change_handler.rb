618644f756271c499ff84432d0a68c8766c5b35f
Andy White <andy@andywhite.org>
2023-09-27 11:35:56 +0100

    Add log message for irrelevant registration type

--- app/lib/domain_events/handlers/probation_change_handler.rb
+++ app/lib/domain_events/handlers/probation_change_handler.rb
@@ -10,7 +10,7 @@ module DomainEvents
 
         case event.event_type
         when /probation-case\.registration\..+/
-          handle_registration_added(event, logger)
+          handle_registration_change(event, logger)
         when 'OFFENDER_MANAGER_CHANGED'
           call_delius_data_job(event, logger)
         end
@@ -18,9 +18,15 @@ module DomainEvents
 
     private
 
-      def handle_registration_added(event, logger)
+      def handle_registration_change(event, logger)
         registration_types = %w[MAPP DASO INVI]
-        call_delius_data_job(event, logger) if registration_types.include?(event.additional_information['registerTypeCode'])
+
+        if registration_types.include?(event.additional_information['registerTypeCode'])
+          call_delius_data_job(event, logger)
+        else
+          logger.info "event=domain_event_handle_noop,domain_event_type=#{event.event_type},crn=#{event.crn_number}" \
+                      '|Registration type is not of interest. No action required'
+        end
       end
 
       def call_delius_data_job(event, logger)
--- spec/lib/domain_events/handlers/probation_change_handler_spec.rb
+++ spec/lib/domain_events/handlers/probation_change_handler_spec.rb
@@ -43,9 +43,14 @@ RSpec.describe DomainEvents::Handlers::ProbationChangeHandler do
       context 'with cases whose registration type does not includes MAPP, DASO, INVI' do
         let(:register_type) { 'BOBBINS' }
 
+        before { handler.handle(event) }
+
         it 'does not update case information' do
-          handler.handle(event)
           expect(ProcessDeliusDataJob).not_to have_received(:perform_now)
+        end
+
+        it 'emits a noop log info message' do
+          expect(Shoryuken::Logging.logger).to have_received(:info).with(/domain_event_handle_noop/).once
         end
       end
     end
