95635cd80dbf24a7b64a2bc733b52b4a82c0b42a
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-10-09 13:10:44 +0100

    Add param-based feature flag to force DPS header/footer
    This shows the DPS header/footer even if the env var feature flag is
    disabled.
    
    MO-1392

--- app/helpers/dps_header_footer_helper.rb
+++ app/helpers/dps_header_footer_helper.rb
@@ -1,13 +1,17 @@
 module DpsHeaderFooterHelper
   def dps_footer_html
-    ENABLE_DPS_HEADER_FOOTER ? dps_footer_all.fetch('html').html_safe : ''
+    dps_header_footer_enabled? ? dps_footer_all.fetch('html').html_safe : ''
   end
 
   def dps_footer_css
-    ENABLE_DPS_HEADER_FOOTER ? dps_footer_all.fetch('css').map { |link| stylesheet_link_tag link }.join("\n").html_safe : ''
+    dps_header_footer_enabled? ? dps_footer_all.fetch('css').map { |link| stylesheet_link_tag link }.join("\n").html_safe : ''
   end
 
   def dps_footer_all
     @dps_footer_all ||= HmppsApi::DpsFrontendComponentsApi.footer
+  end
+
+  def dps_header_footer_enabled?
+    ENABLE_DPS_HEADER_FOOTER || params[:dps_header_footer].present?
   end
 end
