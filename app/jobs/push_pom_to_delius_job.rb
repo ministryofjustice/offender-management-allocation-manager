9485a70d1821a7fbf0fe3f90a62e5a0c7b1febda
Andy White <andy@andywhite.org>
2023-10-18 17:01:35 +0100

    Add audit event to push to Delius job

--- app/jobs/push_pom_to_delius_job.rb
+++ app/jobs/push_pom_to_delius_job.rb
@@ -9,6 +9,7 @@ class PushPomToDeliusJob < ApplicationJob
   def perform(allocation)
     if allocation.primary_pom_nomis_id.nil?
       HmppsApi::CommunityApi.unset_pom(allocation.nomis_offender_id)
+      publish_audit_event(allocation.nomis_offender_id, 'removed')
 
     else
 
@@ -16,12 +17,26 @@ class PushPomToDeliusJob < ApplicationJob
       # splits up to allow them to be pushed separately.
       staff = HmppsApi::PrisonApi::PrisonOffenderManagerApi.staff_detail(allocation.primary_pom_nomis_id)
 
-      HmppsApi::CommunityApi.set_pom(
+      params = {
         offender_no: allocation.nomis_offender_id,
         prison: allocation.prison,
         forename: staff.first_name,
         surname: staff.last_name
-      )
+      }
+
+      HmppsApi::CommunityApi.set_pom(**params)
+      publish_audit_event(allocation.nomis_offender_id, 'changed', params.except(:offender_no))
     end
+  end
+
+private
+
+  def publish_audit_event(offender_no, tag, data = {})
+    AuditEvent.publish(
+      nomis_offender_id: offender_no,
+      tags: %w[job push_pom_to_delius_job allocation] + [tag],
+      system_event: true,
+      data: data.stringify_keys
+    )
   end
 end
