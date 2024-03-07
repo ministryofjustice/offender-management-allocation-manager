911d1ae99060f536cdf96b19ba097804504097f2
Ryan <ryankendallt@gmail.com>
2024-03-07 14:31:28 +0000

    Openapi config surfaced as JSON
    Also refactored /swagger-ui.html redirection into controller

--- spec/requests/swagger_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'GET /swagger-ui.html', type: :request do
-  it 'redirects to the Swagger UI' do
-    get '/swagger-ui.html'
-
-    expect(response).to redirect_to("/api-docs/index.html")
-  end
-end
