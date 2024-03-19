682d8ec3d64502885a798ef32f8f89e26a1be065
Ryan <ryankendallt@gmail.com>
2024-03-19 15:11:34 +0000

    extracted sending of handover follow up email to aid testing

--- app/jobs/handover_follow_up_job.rb
+++ app/jobs/handover_follow_up_job.rb
@@ -1,6 +1,5 @@
 class HandoverFollowUpJob < ApplicationJob
   queue_as :default
-  include PomHelper
 
   def perform(ldu)
     today = Time.zone.today
@@ -13,33 +12,9 @@ class HandoverFollowUpJob < ApplicationJob
                   end
 
     offenders.each do |offender|
-      allocation = AllocationHistory.find_by(nomis_offender_id: offender.offender_no)
-
-      if allocation.present? && allocation.active?
-        prison = Prison.find(offender.prison_id)
-        pom = prison.get_single_pom(allocation.primary_pom_nomis_id)
-        pom_name = full_name(pom)
-        pom_email = pom.email_address || 'unknown'
-      else
-        # There is no POM allocated
-        pom_name = 'This offender does not have an allocated POM'
-        pom_email = 'n/a'
-      end
-
-      offender_type = offender.indeterminate_sentence? ? 'Indeterminate' : 'Determinate'
-
-      CommunityMailer.with(
-        nomis_offender_id: offender.offender_no,
-        offender_name: offender.full_name,
-        offender_crn: offender.crn,
-        sentence_type: offender_type,
-        ldu_email: offender.ldu_email_address,
-        prison: Prison.find(offender.prison_id).name,
-        start_date: offender.handover_start_date,
-        responsibility_handover_date: offender.handover_date,
-        pom_name: pom_name,
-        pom_email: pom_email
-      ).urgent_pipeline_to_community.deliver_now
+      CommunityMailer
+        .with(FollowUpEmailDetails.for(offender:))
+        .urgent_pipeline_to_community
     end
   end
 
---
+++ spec/jobs/handover_follow_up_job/follow_up_email_details_spec.rb
@@ -0,0 +1,93 @@
+require "rails_helper"
+
+describe HandoverFollowUpJob::FollowUpEmailDetails, vcr: { cassette_name: "prison_api/handover_follow_up_email_details" } do
+  around do |example|
+    cassettes = [
+      { name: 'prison_api/pom_api_list_spec', options: { record: :all } },
+      { name: 'prison_api/elite2_staff_api_get_email', options: { record: :all } }
+    ]
+    VCR.use_cassettes(cassettes) do
+      example.run
+    end
+  end
+
+  let(:prison) { Prison.find_by(code: "LEI") }
+  let(:handover_start_date) { 1.week.from_now }
+  let(:handover_date) { 2.weeks.from_now }
+  let(:indeterminate_sentence) { false }
+  let(:offender) do
+    double("offender",
+           offender_no: "OFF1",
+           prison_id: prison.id,
+           indeterminate_sentence?: indeterminate_sentence,
+           full_name: "Nevis, Granville",
+           crn: "CRN123",
+           ldu_email_address: "ldu@email.com",
+           handover_start_date:,
+           handover_date:
+          ).as_null_object
+  end
+
+  describe "the details used in sending the CommunityMailer email" do
+    it "includes basic details regardless of allocation or sentence type" do
+      details = described_class.for(offender:)
+
+      expect(details).to match(
+        ldu_email: "ldu@email.com",
+        nomis_offender_id: "OFF1",
+        offender_crn: "CRN123",
+        offender_name: "Nevis, Granville",
+        pom_email: anything,
+        pom_name: anything,
+        prison: "Leeds (HMP)",
+        responsibility_handover_date: handover_date,
+        start_date: handover_start_date,
+        sentence_type: anything
+      )
+    end
+
+    context "when the offender has a POM allocated" do
+      before { FactoryBot.create(:allocation_history, nomis_offender_id: offender.offender_no, prison: prison.code, primary_pom_nomis_id: "485982") }
+
+      it "includes the POM details in the email" do
+        details = described_class.for(offender:)
+
+        expect(details).to include(
+          pom_email: "jon.hindle@digital.justice.gov.uk", # comes from the elite2_staff_api_get_email VCR
+          pom_name: "Hindle, Jon", # comes from the pom_api_list_spec VCR
+        )
+      end
+    end
+
+    context "when the offender has no POM allocated" do
+      it "does not include the POM details in the email" do
+        details = described_class.for(offender:)
+
+        expect(details).to include(
+          pom_email: "n/a",
+          pom_name: "This offender does not have an allocated POM",
+        )
+      end
+    end
+
+    context "when the offender sentence is determinate" do
+      let(:indeterminate_sentence) { false }
+
+      it "has 'Determinate' as the sentence type in the email" do
+        details = described_class.for(offender:)
+
+        expect(details).to include(sentence_type: "Determinate")
+      end
+    end
+
+    context "when the offender sentence is indeterminate" do
+      let(:indeterminate_sentence) { true }
+
+      it "has 'Ineterminate' as the sentence type in the email" do
+        details = described_class.for(offender:)
+
+        expect(details).to include(sentence_type: "Indeterminate")
+      end
+    end
+  end
+end
