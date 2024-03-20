872bddb0193e9b9c68643d49074c6d84eb8394a8
Ryan <ryankendallt@gmail.com>
2024-03-20 14:55:24 +0000

    Test coverage for HandoverFollowUpJob

--- app/jobs/handover_follow_up_job.rb
+++ app/jobs/handover_follow_up_job.rb
@@ -5,11 +5,12 @@ class HandoverFollowUpJob < ApplicationJob
     today = Time.zone.today
     active_prison_codes = Prison.active.map(&:code)
 
-    offenders = get_ldu_offenders(ldu)
-                  .select do |o|
-                    o.sentenced? && o.handover_start_date.present? &&
-                      active_prison_codes.include?(o.prison_id) && o.handover_start_date == today - 1.week
-                  end
+    offenders = get_ldu_offenders(ldu).select do |offender|
+      offender.sentenced? \
+        && offender.handover_start_date.present? \
+        && active_prison_codes.include?(offender.prison_id) \
+        && offender.handover_start_date == today - 1.week
+    end
 
     offenders.each do |offender|
       CommunityMailer
@@ -21,8 +22,10 @@ class HandoverFollowUpJob < ApplicationJob
 private
 
   def get_ldu_offenders(ldu)
-    ldu.case_information.where(com_name: nil).map(&:nomis_offender_id).map { |offender_id|
-      OffenderService.get_offender(offender_id)
-    }.compact
+    ldu.case_information
+      .where(com_name: nil)
+      .pluck(:nomis_offender_id)
+      .map { |offender_id| OffenderService.get_offender(offender_id) }
+      .compact
   end
 end
