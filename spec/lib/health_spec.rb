50ea8cb35d3c39ed0d1987dd0cf50f0e94814ad5
Ryan <ryankendallt@gmail.com>
2024-03-27 13:47:59 +0000

    Ability to add and execute on health checks

---
+++ spec/lib/health_spec.rb
@@ -0,0 +1,159 @@
+require "rails_helper"
+
+describe Health do
+  subject(:health) { described_class }
+
+  before { described_class.reset_checks! }
+
+  describe "status" do
+    context "when all status checks return { 'status': 'UP' }" do
+      it "is UP" do
+        health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
+        health.add_check(name: 'serviceB', get_response: -> { { "status" => "UP" } })
+
+        expect(described_class.status).to eq({
+          status: "UP",
+          components: {
+            "serviceA" => { status: "UP" },
+            "serviceB" => { status: "UP" },
+          }
+        })
+      end
+    end
+
+    context "when any of the status checks return { 'status': 'DOWN' }" do
+      it "is DOWN" do
+        health.add_check(name: 'serviceA', get_response: -> { { "status" => "DOWN" } })
+        health.add_check(name: 'serviceB', get_response: -> { { "status" => "UP" } })
+
+        expect(described_class.status).to eq({
+          status: "DOWN",
+          components: {
+            "serviceA" => { status: "DOWN" },
+            "serviceB" => { status: "UP" },
+          }
+        })
+      end
+    end
+
+    describe "checks can be configured to match a specific key and value" do
+      context "when the response matches that value" do
+        it "is UP" do
+          health.add_check(
+            name: 'serviceA',
+            get_response: -> { { "custom" => "value" } },
+            check_response: { key: "custom", value: "value" }
+          )
+
+          expect(described_class.status).to eq({
+            status: "UP",
+            components: {
+              "serviceA" => { status: "UP" },
+            }
+          })
+        end
+      end
+
+      context "when the response does not match that value" do
+        it "is DOWN" do
+          health.add_check(
+            name: 'serviceA',
+            get_response: -> { { "custom" => "not_value" } },
+            check_response: { key: "custom", value: "value" }
+          )
+
+          expect(described_class.status).to eq({
+            status: "DOWN",
+            components: {
+              "serviceA" => { status: "DOWN" },
+            }
+          })
+        end
+      end
+    end
+
+    describe "checks can be configured to match a specific value" do
+      context "when the response matches that value" do
+        it "is UP" do
+          health.add_check(
+            name: 'serviceA',
+            get_response: -> { "pong" },
+            check_response: { value: "pong" }
+          )
+
+          expect(described_class.status).to eq({
+            status: "UP",
+            components: {
+              "serviceA" => { status: "UP" },
+            }
+          })
+        end
+      end
+
+      context "when the response does not match that value" do
+        it "is UP" do
+          health.add_check(
+            name: 'serviceA',
+            get_response: -> { "not_pong" },
+            check_response: { value: "pong" }
+          )
+
+          expect(described_class.status).to eq({
+            status: "DOWN",
+            components: {
+              "serviceA" => { status: "DOWN" },
+            }
+          })
+        end
+      end
+    end
+
+    describe "when any of the status checks fail" do
+      it "is DOWN" do
+        health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
+        health.add_check(name: 'serviceB', get_response: -> { raise "FAILED!" })
+
+        expect(described_class.status).to eq({
+          status: "DOWN",
+          components: {
+            "serviceA" => { status: "UP" },
+            "serviceB" => { status: "DOWN" },
+          }
+        })
+      end
+
+      context "when the check passes after retrying once" do
+        it "is UP" do
+          momentarily_down = double('momentarily_down')
+          allow(momentarily_down).to receive(:call).and_return({ "status" => "DOWN" }, { "status" => "UP" })
+
+          health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
+          health.add_check(name: 'serviceB', get_response: momentarily_down)
+
+          expect(described_class.status).to eq({
+            status: "UP",
+            components: {
+              "serviceA" => { status: "UP" },
+              "serviceB" => { status: "UP" },
+            }
+          })
+        end
+      end
+
+      context "when the check times out" do
+        it "is DOWN" do
+          allow(Timeout).to receive(:timeout).and_raise(Timeout::Error)
+
+          health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
+
+          expect(described_class.status).to eq({
+            status: "DOWN",
+            components: {
+              "serviceA" => { status: "DOWN" },
+            }
+          })
+        end
+      end
+    end
+  end
+end
