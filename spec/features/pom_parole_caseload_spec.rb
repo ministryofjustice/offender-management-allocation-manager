394b9b12353f23e889f224c7d1dff239cf331890
Ryan <ryankendallt@gmail.com>
2024-04-04 12:21:02 +0100

    quick and dirty commit

---
+++ spec/features/pom_parole_caseload_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "Viewing parole caseload" do
+  describe "As a HOMD" do
+    before do
+      stub_user staff_id: 123_456
+    end
+
+    it "lists each case with an active parole review" do
+
+    end
+  end
+
+  describe "As a POM" do
+    
+  end
+end
