bbfe86c91e71e1c0985674a860f4fb33a3f87585
Ryan <ryankendallt@gmail.com>
2024-05-15 14:29:44 +0100

    Fix issue in parole case selection logic

--- app/models/mpc_offender.rb
+++ app/models/mpc_offender.rb
@@ -177,29 +177,13 @@ class MpcOffender
     most_recent_completed_parole_review&.hearing_outcome_received_on
   end
 
-  # If the parole application is set for a hearing within 10 months, or the outcome for a hearing was received in the last 14 days,
-  # return true. If the hearing has an outcome but we do not have the date that it was received, assume that it is no longer
-  # approaching parole (return false).
   def approaching_parole?
-    earliest_date = next_parole_date
-    return false if earliest_date.blank?
-    return false unless earliest_date <= Time.zone.today + 10.months
-    return false if !most_recent_parole_review&.no_hearing_outcome? && hearing_outcome_received_on.blank?
-
-    if earliest_date.past? &&
-      hearing_outcome_received_on.present? &&
-      hearing_outcome_received_on <= Time.zone.today - 14.days
-
-      return false
-    end
-
-    true
+    next_parole_date&.between?(Time.zone.now, 10.months.from_now.end_of_day) ||
+    hearing_outcome_received_on&.between?(14.days.ago.beginning_of_day, Time.zone.now)
   end
 
   def next_parole_date
-    return target_hearing_date if target_hearing_date.present?
-
-    [tariff_date, parole_eligibility_date].compact.min
+    target_hearing_date || [tariff_date, parole_eligibility_date].compact.min
   end
 
   # Separate from next_parole_date as parole case index view sorts by next_parole_date, so it seemed sensible to avoid changing default rails behaviour
