9faea54ea2fba6ca2a23d95d6e613e07e0044b08
Ryan <ryankendallt@gmail.com>
2024-07-18 17:12:08 +0100

    more refactoring

--- app/lib/email_history_mail_observer.rb
+++ app/lib/email_history_mail_observer.rb
@@ -1,46 +1,23 @@
 class EmailHistoryMailObserver
   def self.delivered_email(message)
-    case message.govuk_notify_reference
-    when 'email.pom.responsibility_override'
-      EmailHistory.create!(
-        event: EmailHistory::RESPONSIBILITY_OVERRIDE,
-        nomis_offender_id: message.govuk_notify_personalisation[:prisoner_number],
-        prison: Prison.find_by(name: message.govuk_notify_personalisation[:prison_name]).code,
-        email: message.to.first,
-        name: message.to.first,
-      )
-    when 'email.community.open_prison_supporting_com_needed'
-      EmailHistory.create!(
-        event: EmailHistory::OPEN_PRISON_SUPPORTING_COM_NEEDED,
-        nomis_offender_id: message.govuk_notify_personalisation[:prisoner_number],
-        prison: Prison.find_by(name: message.govuk_notify_personalisation[:prison_name]).code,
-        email: message.to.first,
-        name: message.to.first,
-      )
-    when 'email.community.urgent_pipeline_to_community'
-      EmailHistory.create!(
-        event: EmailHistory::URGENT_PIPELINE_TO_COMMUNITY,
-        nomis_offender_id: message.govuk_notify_personalisation[:noms_no],
-        prison: Prison.find_by(name: message.govuk_notify_personalisation[:prison_name]).code,
-        email: message.to.first,
-        name: message.to.first,
-      )
-    when 'email.community.assign_com_less_than_10_months'
-      EmailHistory.create!(
-        event: EmailHistory::ASSIGN_COM_LESS_THAN_10_MONTHS,
-        nomis_offender_id: message.govuk_notify_personalisation[:prisoner_number],
-        prison: Prison.find_by(name: message.govuk_notify_personalisation[:prison_name]).code,
-        email: message.to.first,
-        name: message.to.first,
-      )
-    when 'email.early_allocation.community_early_allocation'
-      EmailHistory.create!(
-        event: EmailHistory::COMMUNITY_EARLY_ALLOCATION,
-        nomis_offender_id: message.govuk_notify_personalisation[:prisoner_number],
-        prison: Prison.find_by(name: message.govuk_notify_personalisation[:prison_name]).code,
-        email: message.to.first,
-        name: message.to.first,
-      )
+    reference_to_event_map = {
+      'email.pom.responsibility_override' => EmailHistory::RESPONSIBILITY_OVERRIDE,
+      'email.community.open_prison_supporting_com_needed' => EmailHistory::OPEN_PRISON_SUPPORTING_COM_NEEDED,
+      'email.community.urgent_pipeline_to_community' => EmailHistory::URGENT_PIPELINE_TO_COMMUNITY,
+      'email.community.assign_com_less_than_10_months' => EmailHistory::ASSIGN_COM_LESS_THAN_10_MONTHS,
+      'email.early_allocation.community_early_allocation' => EmailHistory::COMMUNITY_EARLY_ALLOCATION
+    }
+
+    if (event = reference_to_event_map[message.govuk_notify_reference]).present?
+      prison = Prison.find_by(name: message.govuk_notify_personalisation[:prison_name])&.code
+
+      nomis_offender_id = [:prisoner_number, :noms_no, :nomis_offender_id]
+        .map { |key| message.govuk_notify_personalisation[key] }
+        .find(&:itself)
+
+      email = message.to.first
+
+      EmailHistory.create!(event:, nomis_offender_id:, prison:, email:, name: email)
     end
   end
 end
