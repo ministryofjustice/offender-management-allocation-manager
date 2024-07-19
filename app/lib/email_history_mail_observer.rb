443f2443a444c070bda269ba4240c469f59d84e7
Ryan <ryankendallt@gmail.com>
2024-07-19 09:25:35 +0100

    more refactoring

--- app/lib/email_history_mail_observer.rb
+++ app/lib/email_history_mail_observer.rb
@@ -1,27 +1,26 @@
 class EmailHistoryMailObserver
   def self.delivered_email(message)
-    reference_to_event_map = {
-      'email.pom.responsibility_override' => EmailHistory::RESPONSIBILITY_OVERRIDE,
-      'email.community.open_prison_supporting_com_needed' => EmailHistory::OPEN_PRISON_SUPPORTING_COM_NEEDED,
-      'email.community.urgent_pipeline_to_community' => EmailHistory::URGENT_PIPELINE_TO_COMMUNITY,
-      'email.community.assign_com_less_than_10_months' => EmailHistory::ASSIGN_COM_LESS_THAN_10_MONTHS,
-      'email.early_allocation.community_early_allocation' => EmailHistory::COMMUNITY_EARLY_ALLOCATION
-    }
-
-    if (event = reference_to_event_map[message.govuk_notify_reference]).present?
-      EmailHistory.create!(event:, **EmailHistoryDetailsFromMessage.new(message).as_attributes)
-    end
+    email_history_from_message = EmailHistoryFromMessage.new(message)
+    email_history_from_message.log_history_if_valid
   end
 
-  class EmailHistoryDetailsFromMessage < SimpleDelegator
+  class EmailHistoryFromMessage < SimpleDelegator
+    def log_history_if_valid
+      email_hsitory = EmailHistory.new(event:, prison:, nomis_offender_id:, email:, name:)
+      email_hsitory.save if email_hsitory.valid?
+    end
+
+  private
+
     def nomis_offender_id
       [:prisoner_number, :noms_no, :nomis_offender_id]
         .map { |key| govuk_notify_personalisation[key] }
         .find(&:itself)
     end
+
+    def event = String(govuk_notify_reference).split('.').last
     def prison = Prison.find_by(name: govuk_notify_personalisation[:prison_name])&.code
     def email = to.first
     def name = to.first
-    def as_attributes = { prison:, nomis_offender_id:, email:, name: }
   end
 end
