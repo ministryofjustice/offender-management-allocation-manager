54559a75f03df11ad6e76fdb887bb42e292db3dc
Ryan <ryankendallt@gmail.com>
2024-08-14 14:21:12 +0100

    Clean up sentences logic and fix bug with durations

--- spec/domains/sentences/sentence_term_spec.rb
+++ spec/domains/sentences/sentence_term_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe HmppsApi::OffenderSentenceTerm do
+describe Sentences::SentenceTerm do
   it { is_expected.not_to be_indeterminate }
 
   context 'when it is a life sentence' do
@@ -10,7 +10,7 @@ describe HmppsApi::OffenderSentenceTerm do
   end
 
   context 'when the sentence type is indeterminate' do
-    HmppsApi::OffenderSentenceTerm::SentenceType::INDETERMINATE_SENTENCE_TYPES.each do |indeterminate_type|
+    Sentences::SentenceTerm::SentenceType::INDETERMINATE_SENTENCE_TYPES.each do |indeterminate_type|
       subject { described_class.new('sentenceType' => indeterminate_type) }
 
       it { is_expected.to be_indeterminate }
---
+++ spec/domains/sentences_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+describe Sentences do
+  describe '.for' do
+    before { stub_request(:get, "https://prison-api-dev.prison.service.justice.gov.uk/api/offender-sentences/booking/12345678/sentenceTerms").to_return(status: 200, body: results.to_json, headers: {}) }
+
+    let(:results) do
+      [
+        {
+          'bookingId' => 12_345_678,
+          'caseId' => 98_765_432,
+          'sentenceSequence' => 1,
+          'lineSeq' => 1,
+          'termSequence' => 1,
+          'lifeSentence' => true,
+          'sentenceType' => "LR_ALP",
+          'sentenceTypeDescription' => "Recall from Automatic Life",
+          'sentenceTermCode' => "IMP",
+          'sentenceStartDate' => "1993-10-21",
+          'startDate' => "1993-10-21",
+          'years' => nil,
+          'months' => nil,
+          'days' => nil
+        },
+        {
+          'bookingId' => 12_345_678,
+          'caseId' => 98_765_433,
+          'sentenceSequence' => 2,
+          'lineSeq' => 2,
+          'termSequence' => 1,
+          'lifeSentence' => false,
+          'sentenceType' => "LR_ALP",
+          'sentenceTypeDescription' => "Recall from Automatic Life",
+          'sentenceTermCode' => "IMP",
+          'sentenceStartDate' => "1993-10-21",
+          'startDate' => "1993-10-21",
+          'years' => nil,
+          'months' => nil,
+          'days' => nil
+        },
+      ]
+    end
+
+    it 'returns each result as an instance of Sentences::SentenceSequence' do
+      sentences = described_class.for(booking_id: 12_345_678)
+      expect(sentences.count).to eq(2)
+      expect(sentences.first.class).to eq(Sentences::SentenceSequence)
+    end
+  end
+end
