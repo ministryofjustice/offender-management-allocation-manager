abdcfe8bbd406acb46e10981d1258849b072a31b
Ryan Kendall <ryankendallt@gmail.com>
2024-08-15 15:26:19 +0100

    Only show parole eligible badge when they are eligible (#2477)

--- spec/views/shared/badges.html.erb_spec.rb
+++ spec/views/shared/badges.html.erb_spec.rb
@@ -41,86 +41,41 @@ RSpec.describe "shared/badges", type: :view do
 
   context "when recall" do
     let(:api_offender) do
-      build(:hmpps_api_offender, sentence: attributes_for(:sentence_detail, :indeterminate_recall))
+      build(:hmpps_api_offender, sentence: attributes_for(:sentence_detail, :recall))
     end
 
     it "displays a recall case type badge and a indeterminate badge" do
-      expect(case_type_badge.first.attributes['class'].value).to include 'govuk-tag--purple'
-      expect(case_type_badge.text).to include 'Indeterminate'
       assert_you_have_a_recall_badge
     end
   end
 
-  context "when parole eligibility(PED)" do
+  context "when eligibile for parole" do
+    let(:api_offender) do
+      build(:hmpps_api_offender, sentence: attributes_for(:sentence_detail, :approaching_parole))
+    end
+
+    it "displays an parole eligibility badge" do
+      assert_you_have_a_parole_eligibility_badge
+    end
+  end
+
+  context "when indeterminate, a recall case" do
     let(:api_offender) do
       build(:hmpps_api_offender,
-            sentence: attributes_for(:sentence_detail,
-                                     :indeterminate, paroleEligibilityDate: Time.zone.today.to_s))
-    end
-
-    it "displays an parole eligibility case type badge" do
-      assert_you_have_a_parole_eligibility_badge
-      assert_you_have_an_indeterminate_badge
-    end
-  end
-
-  context "when parole eligibility(TED)" do
-    let(:api_offender) do
-      build(:hmpps_api_offender, sentence: attributes_for(:sentence_detail, :determinate, tariffDate: Time.zone.today.to_s))
-    end
-
-    it "displays an parole eligibility case type badge and determinate badge" do
-      assert_you_have_a_parole_eligibility_badge
-      assert_you_have_a_determinate_badge
-    end
-  end
-
-  context "when there is a parole review date" do
-    let(:offender_no) { 'G7514GW' }
-    let(:api_offender) do
-      build(:hmpps_api_offender, sentence: attributes_for(:sentence_detail, :indeterminate), prisonerNumber: offender_no)
-    end
-
-    it "displays an parole eligibility case type badge" do
-      assert_you_have_an_indeterminate_badge
-      assert_you_have_a_parole_eligibility_badge
-    end
-  end
-
-  context 'when parole eligibility (both TED and THD) and target hearing date are nil' do
-    let(:offender_no) { 'G7514GW' }
-    let(:case_info) { build(:case_information, offender: build(:offender, nomis_offender_id: offender_no)) }
-    let(:api_offender) do
-      build(:hmpps_api_offender, prisonerNumber: offender_no,
-                                 sentence: attributes_for(:sentence_detail, :indeterminate, tariffDate: nil))
-    end
-
-    it 'does not display the badge' do
-      expect(offender.tariff_date).to eq nil
-      expect(offender.parole_eligibility_date).to eq nil
-      expect(parole_badge.text).not_to include 'Parole eligible'
-    end
-  end
-
-  context "when indeterminate, a recall case and parole eligibility(PED)" do
-    let(:api_offender) do
-      build(:hmpps_api_offender,
-            sentence: attributes_for(:sentence_detail, :indeterminate_recall, paroleEligibilityDate: Time.zone.today.to_s))
+            sentence: attributes_for(:sentence_detail, :indeterminate_recall))
     end
 
     it "displays an indeterminate, parole eligibility and recall case type badges" do
       assert_you_have_an_indeterminate_badge
-      assert_you_have_a_parole_eligibility_badge
       assert_you_have_a_recall_badge
     end
   end
 
-  context "when determinate, a recall case and parole eligibility(PED)" do
-    let(:api_offender) { build(:hmpps_api_offender, sentence: attributes_for(:sentence_detail, :determinate_recall, paroleEligibilityDate: Time.zone.today)) }
+  context "when determinate, a recall case" do
+    let(:api_offender) { build(:hmpps_api_offender, sentence: attributes_for(:sentence_detail, :determinate_recall)) }
 
     it "displays an determinate, parole eligibility and recall case type badges" do
       assert_you_have_a_determinate_badge
-      assert_you_have_a_parole_eligibility_badge
       assert_you_have_a_recall_badge
     end
   end
