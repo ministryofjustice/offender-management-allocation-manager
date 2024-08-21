96cc103dd410f02bcb00033176ab54e679f6bfe6
Ryan <ryankendallt@gmail.com>
2024-08-21 15:48:25 +0100

    Added console debugging helpers

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -1,12 +1,9 @@
+require 'console_display_helpers'
+
 Rails.application.configure do
   console do
-    def with_audit_notes(user_first_name:, user_last_name:, note:)
-      whodunnit = "#{user_first_name} #{user_last_name}"
-      controller_info = { user_first_name:, user_last_name:, system_admin_note: note }
-
-      PaperTrail.request(whodunnit:, controller_info:) do
-        yield if block_given?
-      end
-    end
+    Rails::ConsoleMethods.include(ConsoleDisplayHelpers)
+    Rails::ConsoleMethods.include(ConsoleAuditHelpers)
+    Rails::ConsoleMethods.include(ConsoleDebugOffenderHelpers)
   end
 end
