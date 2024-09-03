dc899f072e6d2f9bc9b05c7125be135ce8f23c79
Jesus Laiz <zheileman@users.noreply.github.com>
2024-09-03 12:18:30 +0100

    Handle formats other than html in catch-all route

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,22 @@
 class ErrorsController < ApplicationController
   def not_found
-    render(status: :not_found)
+    respond_with_status(:not_found)
   end
 
   def unauthorized
-    render(status: :unauthorized)
+    respond_with_status(:unauthorized)
   end
 
   def internal_server_error
-    render(status: :internal_server_error)
+    respond_with_status(:internal_server_error)
+  end
+
+private
+
+  def respond_with_status(status)
+    respond_to do |format|
+      format.html { render status: }
+      format.all  { head status }
+    end
   end
 end
