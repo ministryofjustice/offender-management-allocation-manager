faab59ce059a3fdb7bdc48db0da16c4890a24357
Jesus Laiz <zheileman@users.noreply.github.com>
2024-09-11 11:01:35 +0100

    MO-1564 Remove VCR from complexities API specs (#2489)
    Ticket: https://dsdmoj.atlassian.net/browse/MO-1564
    
    We are commencing a cleanup of VCR from some of our specs to reduce its use where it really doesn't add any value.
    
    This PR cleanups VCR from the  complexities api spec. That's 5 cassettes gone.

--- spec/services/hmpps_api/complexity_api_spec.rb
+++ spec/services/hmpps_api/complexity_api_spec.rb
@@ -3,40 +3,115 @@
 require "rails_helper"
 
 describe HmppsApi::ComplexityApi do
-  describe '#get_complexity', vcr: { cassette_name: 'complexity/get_complexity' } do
-    scenario 'item present' do
-      expect(described_class.get_complexity 'G0276VC').to eq('high')
+  let(:api_host) { Rails.configuration.complexity_api_host }
+
+  describe '#get_complexity' do
+    let(:endpoint) { "#{api_host}/v1/complexity-of-need/offender-no/#{offender_no}" }
+
+    before do
+      stub_request(:get, endpoint).to_return(status:, body:)
     end
 
-    scenario 'item absent' do
-      expect(described_class.get_complexity 'T0276VC').to eq(nil)
+    context 'when item is present' do
+      let(:offender_no) { 'G0276VC' }
+      let(:status) { 200 }
+      let(:body) do
+        {
+          offenderNo: "G0276VC",
+          level: "high",
+          createdTimeStamp: "2021-03-11T11:05:10.493Z",
+          sourceSystem: "testing",
+          active: true
+        }.to_json
+      end
+
+      it { expect(described_class.get_complexity 'G0276VC').to eq('high') }
+    end
+
+    context 'when item is absent' do
+      let(:offender_no) { 'T0276VC' }
+      let(:status) { 404 }
+      let(:body) { nil }
+
+      it { expect(described_class.get_complexity 'T0276VC').to eq(nil) }
     end
   end
 
-  describe '#save', vcr: { cassette_name: 'complexity/save_complexity' } do
-    let(:saved_offender_no) { 'S0005FT' }
+  describe '#save' do
+    let(:endpoint) { "#{api_host}/v1/complexity-of-need/offender-no/#{offender_no}" }
+    let(:offender_no) { 'S0005FT' }
+    let(:body) do
+      {
+        offenderNo: "S0005FT",
+        level: "high",
+        createdTimeStamp: "2021-03-11T11:05:10.493Z",
+        sourceSystem: "testing",
+        active: true
+      }.to_json
+    end
+
+    before do
+      stub_request(:post, endpoint).with(
+        body: { level: 'high', sourceUser: 'SDICKS_GEN', notes: 'Happy Feet' }
+      ).to_return(body:)
+    end
 
     scenario 'happy path' do
-      described_class.save saved_offender_no, level: 'high', username: 'SDICKS_GEN', reason: 'Happy Feet'
-      expect(described_class.get_complexity saved_offender_no).to eq('high')
+      expect(
+        described_class.save(offender_no, level: 'high', username: 'SDICKS_GEN', reason: 'Happy Feet')
+      ).to eq(JSON.parse(body))
     end
   end
 
-  describe '#get_complexities', vcr: { cassette_name: 'complexity/get_complexities' } do
-    scenario 'happy path' do
-      expect(described_class.get_complexities ['G0276VC', 'T0000FT'])
-        .to eq('G0276VC' => 'high', 'T0000FT' => 'high')
+  describe '#get_complexities' do
+    let(:endpoint) { "#{api_host}/v1/complexity-of-need/multiple/offender-no" }
+    let(:body) do
+      [
+        { "offenderNo": "G0276VC", "level": "high", "createdTimeStamp": "2021-03-11T11:05:10.493Z", "sourceSystem": "testing", "active": true },
+        { "offenderNo": "T0000FT", "level": "high", "createdTimeStamp": "2021-03-17T20:54:55.902Z", "sourceSystem": "hardcoded-oauth-client-id", "active": true }
+      ].to_json
     end
 
-    scenario 'with one item missing' do
-      expect(described_class.get_complexities ['G0276VC', 'T0000FT', "X00887XX"])
-        .to eq('G0276VC' => 'high', 'T0000FT' => 'high')
+    before do
+      stub_request(:post, endpoint).with(body: offender_nos).to_return(body:)
+    end
+
+    context 'when all offenders are found' do
+      let(:offender_nos) { %w[G0276VC T0000FT] }
+
+      it {
+        expect(described_class.get_complexities(offender_nos))
+          .to eq('G0276VC' => 'high', 'T0000FT' => 'high')
+      }
+    end
+
+    context 'when some offenders are not found' do
+      let(:offender_nos) { %w[G0276VC T0000FT X00887XX] }
+
+      it {
+        expect(described_class.get_complexities(offender_nos))
+          .to eq('G0276VC' => 'high', 'T0000FT' => 'high')
+      }
     end
   end
 
-  describe '#get_history', vcr: { cassette_name: 'complexity/get_history' } do
+  describe '#get_history' do
+    let(:endpoint) { "#{api_host}/v1/complexity-of-need/offender-no/#{offender_no}/history" }
+    let(:offender_no) { 'S0004FT' }
+    let(:body) do
+      [
+        { "offenderNo": "S0004FT", "level": "high", "createdTimeStamp": "2021-03-18T14:35:20.046Z", "sourceUser": "SDICKS_GEN", "sourceSystem": "hardcoded-oauth-client-id", "notes": "Happy Feet", "active": true },
+        { "offenderNo": "S0004FT", "level": "high", "createdTimeStamp": "2021-03-18T14:34:58.551Z", "sourceUser": "SDICKS_GEN", "sourceSystem": "hardcoded-oauth-client-id", "notes": "Happy Feet", "active": true },
+        { "offenderNo": "S0004FT", "level": "low", "createdTimeStamp": "2021-03-18T14:33:28.364Z", "sourceUser": "SDICKS_GEN", "sourceSystem": "hardcoded-oauth-client-id", "notes": "Happy Feet", "active": true }
+      ].to_json
+    end
+
+    before do
+      stub_request(:get, endpoint).to_return(body:)
+    end
+
     scenario 'happy path' do
-      expect(described_class.get_history('S0004FT')).to eq(
+      expect(described_class.get_history(offender_no)).to eq(
         [
           { createdTimeStamp: Time.parse('2021-03-18T14:33:28.364Z'), level: 'low', sourceUser: 'SDICKS_GEN', notes: 'Happy Feet' },
           { createdTimeStamp: Time.parse('2021-03-18T14:34:58.551Z'), level: 'high', sourceUser: 'SDICKS_GEN', notes: 'Happy Feet' },
@@ -45,12 +120,21 @@ describe HmppsApi::ComplexityApi do
     end
   end
 
-  describe '#inactivate', vcr: { cassette_name: 'complexity/put_inactivate' } do
+  describe '#inactivate' do
+    let(:endpoint) { "#{api_host}/v1/complexity-of-need/offender-no/#{offender_no}/inactivate" }
     let(:offender_no) { 'S0005FT' }
+    let(:body) do
+      { "offenderNo": "S0005FT", "level": "high", "createdTimeStamp": "2022-08-09T10:29:51.814Z", "sourceUser": "SDICKS_GEN", "sourceSystem": "manage-pom-cases-api-3", "notes": "Happy Feet", "active": false }.to_json
+    end
+
+    before do
+      stub_request(:put, endpoint).to_return(body:)
+    end
 
     scenario 'item present' do
-      described_class.inactivate offender_no
-      expect(described_class.get_complexity offender_no).to eq(nil)
+      expect(
+        described_class.inactivate(offender_no)
+      ).to eq(JSON.parse(body))
     end
   end
 end
