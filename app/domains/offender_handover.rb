51d6a09dc6ee59a5308e393c9f1ab2d732e78668
Ryan Kendall <ryankendallt@gmail.com>
2024-10-16 10:25:18 +0100

    condensed down rules

--- app/domains/offender_handover.rb
+++ app/domains/offender_handover.rb
@@ -4,42 +4,26 @@ class OffenderHandover < SimpleDelegator
     reason: :com_responsibility)
 
   def as_calculated_handover_date
-    indeterminate_sentences || edge_cases || general_rules
+    if indeterminate_sentence? && sentences.sentenced_to_additional_future_isp?
+      pom_only(:additional_isp)
+    elsif indeterminate_sentence? && thd_12_or_more_months_from_now? && recalled? && mappa_level.in?([nil, 0, 1])
+      pom_with_com(:recall_thd_over_12_months)
+    elsif indeterminate_sentence? && thd_12_or_more_months_from_now? && parole_outcome_not_release? && mappa_level.in?([2, 3])
+      com(:parole_mappa_2_3)
+    elsif indeterminate_sentence? && thd_12_or_more_months_from_now? && parole_outcome_not_release?
+      pom_with_com(:thd_over_12_months)
+    elsif recalled?
+      com(:recall_case)
+    elsif immigration_case?
+      com(:immigration_case)
+    elsif !earliest_release_for_handover
+      pom_only(:release_date_unknown)
+    else
+      general_rules
+    end
   end
 
 private
-
-  def indeterminate_sentences
-    return unless indeterminate_sentence?
-
-    if sentences.sentenced_to_additional_future_isp?
-      return CalculatedHandoverDate.new(responsibility: pom_only, reason: :additional_isp)
-    end
-
-    # Anything after 12 months has no handover dates
-    return unless thd_12_or_more_months_from_now?
-
-    if recalled? && mappa_level.in?([nil, 0, 1])
-      CalculatedHandoverDate.new(responsibility: pom_with_com, reason: :recall_thd_over_12_months)
-
-    elsif parole_outcome_not_release?
-      if mappa_level.in?([2, 3])
-        CalculatedHandoverDate.new(responsibility: com, reason: :parole_mappa_2_3)
-      else
-        CalculatedHandoverDate.new(responsibility: pom_with_com, reason: :thd_over_12_months)
-      end
-    end
-  end
-
-  def edge_cases
-    if recalled?
-      CalculatedHandoverDate.new(responsibility: com, reason: :recall_case)
-    elsif immigration_case?
-      CalculatedHandoverDate.new(responsibility: com, reason: :immigration_case)
-    elsif !earliest_release_for_handover
-      CalculatedHandoverDate.new(responsibility: pom_only, reason: :release_date_unknown)
-    end
-  end
 
   def general_rules
     handover_date, reason = Handover::HandoverCalculation.calculate_handover_date(
@@ -68,7 +52,7 @@ private
     CalculatedHandoverDate.new(responsibility:, handover_date:, start_date:, reason:)
   end
 
-  def pom_only = CalculatedHandoverDate::CUSTODY_ONLY
-  def pom_with_com = CalculatedHandoverDate::CUSTODY_WITH_COM
-  def com = CalculatedHandoverDate::COMMUNITY_RESPONSIBLE
+  def pom_only(reason = nil) = CalculatedHandoverDate.new(responsibility: CalculatedHandoverDate::CUSTODY_ONLY, reason:)
+  def pom_with_com(reason = nil) = CalculatedHandoverDate.new(responsibility: CalculatedHandoverDate::CUSTODY_WITH_COM, reason:)
+  def com(reason = nil) = CalculatedHandoverDate.new(responsibility: CalculatedHandoverDate::COMMUNITY_RESPONSIBLE, reason:)
 end
