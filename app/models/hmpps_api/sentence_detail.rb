52240f125a8b863dbb299f92489da46ac4886b3c
Ryan Kendall <ryankendallt@gmail.com>
2024-10-31 15:12:33 +0000

    refactor earlisest_release date

--- app/models/hmpps_api/sentence_detail.rb
+++ app/models/hmpps_api/sentence_detail.rb
@@ -45,26 +45,7 @@ module HmppsApi
     end
 
     def earliest_release_date
-      dates = [
-        automatic_release_date,
-        conditional_release_date,
-        home_detention_curfew_actual_date,
-        home_detention_curfew_eligibility_date,
-        parole_eligibility_date,
-        tariff_date
-      ].compact
-
-      if dates.empty?
-        dates = [
-          sentence_expiry_date,
-          licence_expiry_date,
-          post_recall_release_date,
-          actual_parole_date].compact
-      end
-
-      past_dates, future_dates = dates.partition { |date| date < Time.zone.today }
-
-      future_dates.any? ? future_dates.min.to_date : past_dates.max.try(:to_date)
+      earliest_release.try(:[], :date)
     end
 
     def earliest_release
@@ -75,7 +56,7 @@ module HmppsApi
         { type: 'HDCEA', date: home_detention_curfew_actual_date },
         { type: 'PED', date: parole_eligibility_date },
         { type: 'TED', date: tariff_date }
-      ].filter { |v| !v[:date].nil? }
+      ].reject { |v| v[:date].nil? }
 
       if release_dates.empty?
         release_dates = [
@@ -83,13 +64,16 @@ module HmppsApi
           { type: 'LED', date: licence_expiry_date },
           { type: 'PRRD', date: post_recall_release_date },
           { type: 'APD', date: actual_parole_date }
-        ].filter { |v| !v[:date].nil? }
+        ].reject { |v| v[:date].nil? }
       end
 
-      past_dates, future_dates = release_dates.partition { |d| d.fetch(:date) < Time.zone.today }
-      return future_dates.min_by(1) { |r| r.fetch(:date).to_date }.first if future_dates.any?
+      past_dates, future_dates = release_dates.partition { |date| date[:date].to_date.past? }
 
-      past_dates.max_by(1) { |r| r.fetch(:date).to_date }.first
+      if future_dates.any?
+        future_dates.min_by { |date| date[:date].to_date }
+      else
+        past_dates.max_by { |date| date[:date].to_date }
+      end
     end
 
     def initialize(payload)
