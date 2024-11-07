51b808c29de0f73bb779ba268c8b660385a9368a
Ryan Kendall <ryankendallt@gmail.com>
2024-11-07 16:37:22 +0000

    fix cops

--- app/models/prison.rb
+++ app/models/prison.rb
@@ -4,7 +4,7 @@ class Prison < ApplicationRecord
   validates :prison_type, presence: true
   validates :code, :name, presence: true, uniqueness: true
 
-  has_many :allocations, -> { active }, class_name: "AllocationHistory", foreign_key: :prison
+  has_many :allocations, -> { active }, class_name: 'AllocationHistory', foreign_key: :prison
   has_many :pom_details, dependent: :destroy, foreign_key: :prison_code, inverse_of: :prison
 
   enum prison_type: { womens: 'womens', mens_open: 'mens_open', mens_closed: 'mens_closed' }
