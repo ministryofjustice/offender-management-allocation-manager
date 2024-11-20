c4685a84e3f8d5634e12dfc4daad00e4af7bcb4e
Ryan Kendall <ryankendallt@gmail.com>
2024-11-20 09:27:43 +0000

    Tidy of handover summary test

--- spec/lib/handover/summary_spec.rb
+++ spec/lib/handover/summary_spec.rb
@@ -1,113 +1,78 @@
-RSpec.describe Handover::Summary do
+describe Handover::Summary do
   subject(:handover_cases) { described_class.new(offenders.values) }
 
-  let(:offender_numbers) { ('A'..'Z').to_a }
   let(:offenders) do
-    value = {}
-    offender_numbers.each do |no|
-      value[no] = sneaky_instance_double(AllocatedOffender,
-                                         "offender#{no}",
-                                         offender_no: no,
-                                         allocated_com_name: nil,
-                                         handover_progress_complete?: true)
+    ('A'..'Z').index_with do |id|
+      double(
+        :"offender_#{id}",
+        offender_no: id,
+        nomis_offender_id: id,
+        allocated_com_name: nil,
+        handover_progress_complete?: true
+      )
     end
-    value
   end
-  let(:upcoming_calculated_handover_dates) do
-    [
-      sneaky_instance_double(CalculatedHandoverDate, :upcoming1, nomis_offender_id: 'A'),
-      sneaky_instance_double(CalculatedHandoverDate, :upcoming2, nomis_offender_id: 'C'),
-    ]
+
+  def calculated_handover_dates_for(*nomis_offender_ids)
+    nomis_offender_ids.map do |nomis_offender_id|
+      double(:"chd_for_#{nomis_offender_id}", nomis_offender_id: nomis_offender_id)
+    end
   end
-  let(:in_progress_calculated_handover_dates) do
-    [
-      sneaky_instance_double(CalculatedHandoverDate, :in_progress1, nomis_offender_id: 'D'),
-      sneaky_instance_double(CalculatedHandoverDate, :in_progress2, nomis_offender_id: 'F'),
-    ]
-  end
-  let(:overdue_tasks_calculated_handover_dates) do
-    [
-      sneaky_instance_double(CalculatedHandoverDate, :overdue_tasks1, nomis_offender_id: 'G'),
-      sneaky_instance_double(CalculatedHandoverDate, :overdue_tasks2, nomis_offender_id: 'H'),
-    ]
-  end
-  let(:com_allocation_overdue_calculated_handover_dates) do
-    [
-      sneaky_instance_double(CalculatedHandoverDate, :com_allocation_overdue1, nomis_offender_id: 'J'),
-      sneaky_instance_double(CalculatedHandoverDate, :com_allocation_overdue2, nomis_offender_id: 'K'),
-    ]
+
+  def ids_of(offenders)
+    offenders.map(&:nomis_offender_id)
   end
 
   before do
     allow(CalculatedHandoverDate).to receive(:by_upcoming_handover)
-                                       .with(offender_ids: offender_numbers)
-                                       .and_return(Array(upcoming_calculated_handover_dates))
-    allow(CalculatedHandoverDate).to(
-      receive(:by_handover_in_progress)
-        .with(offender_ids: offender_numbers)
-        .and_return(Array(in_progress_calculated_handover_dates + overdue_tasks_calculated_handover_dates))
-    )
-    allow(CalculatedHandoverDate).to receive(:by_com_allocation_overdue)
-                                       .with(offender_ids: offender_numbers)
-                                       .and_return(Array(com_allocation_overdue_calculated_handover_dates))
+      .with(offender_ids: offenders.keys)
+      .and_return(calculated_handover_dates_for 'A', 'C')
 
-    allow(offenders['G']).to receive(:handover_progress_complete?).and_return(false)
-    allow(offenders['H']).to receive(:handover_progress_complete?).and_return(false)
+    allow(CalculatedHandoverDate).to receive(:by_handover_in_progress)
+      .with(offender_ids: offenders.keys)
+      .and_return(calculated_handover_dates_for 'D', 'F', 'G', 'H')
+
+    allow(CalculatedHandoverDate).to receive(:by_com_allocation_overdue)
+      .with(offender_ids: offenders.keys)
+      .and_return(calculated_handover_dates_for 'J', 'K')
   end
 
   describe '#upcoming' do
-    it 'gets a list of handover cases whose handovers are upcoming' do
-      expect(handover_cases.upcoming).to eq([
-        offenders['A'],
-        offenders['C']
-      ])
+    it 'returns cases whose handovers are upcoming' do
+      expect(ids_of handover_cases.upcoming).to eq(['A', 'C'])
     end
 
     it 'does not include upcoming handover cases with a COM allocated' do
       allow(offenders['C']).to receive(:allocated_com_name).and_return('TEST COM NAME')
 
-      expect(handover_cases.upcoming).not_to include(offenders['C'])
+      expect(ids_of handover_cases.upcoming).not_to include('C')
     end
   end
 
   describe '#in_progress' do
-    it 'gets a list of handover cases whose handovers are in progress' do
-      expect(handover_cases.in_progress).to eq([
-        offenders['D'],
-        offenders['F'],
-        offenders['G'],
-        offenders['H'],
-      ])
+    it 'returns cases whose handovers are in progress' do
+      expect(ids_of handover_cases.in_progress).to eq(['D', 'F', 'G', 'H'])
     end
 
-    it 'includes upcoming handover cases that have a COM allocated' do
+    it 'returns upcoming cases with a COM allocated' do
       allow(offenders['C']).to receive(:allocated_com_name).and_return('TEST COM NAME')
 
-      expect(handover_cases.in_progress).to include(offenders['C'])
+      expect(ids_of handover_cases.in_progress).to include('C')
     end
   end
 
   describe '#overdue_tasks' do
-    it 'gets a list of handover cases past handover and with tasks overdue' do
-      expect(handover_cases.overdue_tasks).to eq([
-        offenders['G'],
-        offenders['H'],
-      ])
-    end
+    it 'returns in progress cases with incomplete handover tasks' do
+      allow(offenders['G']).to receive(:handover_progress_complete?).and_return(false)
+      allow(offenders['H']).to receive(:handover_progress_complete?).and_return(false)
 
-    it 'includes upcoming handover cases that have a COM allocated' do
-      allow(offenders['C']).to receive(:allocated_com_name).and_return('TEST COM NAME')
-
-      expect(handover_cases.in_progress).to include(offenders['C'])
+      expect(ids_of handover_cases.overdue_tasks).to eq(['G', 'H'])
     end
   end
 
   describe '#com_allocation_overdue' do
-    it 'gets a list of handover cases that are COM allocation overdue' do
-      expect(handover_cases.com_allocation_overdue).to eq([
-        offenders['J'],
-        offenders['K']
-      ])
+    it 'returns cases that are overdue COM allocation' do
+      expect(ids_of handover_cases.com_allocation_overdue).to eq(['J', 'K'])
     end
   end
 end
