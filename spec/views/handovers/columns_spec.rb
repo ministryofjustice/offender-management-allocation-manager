396b403322e5064a5e70d577cf08d19c91058611
Ryan Kendall <ryankendallt@gmail.com>
2024-11-20 13:46:41 +0000

    tests for handover tables on pom screen

--- spec/views/handovers/columns_spec.rb
+++ spec/views/handovers/columns_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
 shared_examples 'handover cases table' do |data|
-  let(:page) { Capybara.string(rendered) }
+  let(:page) do
+    Capybara.string(rendered).then do |page|
+      data[:id] ? page.find_by_id(data[:id]) : page
+    end
+  end
 
   before do
     assign(:filtered_handover_cases, [])
@@ -14,7 +18,7 @@ shared_examples 'handover cases table' do |data|
   end
 
   specify 'the header is correct' do
-    expect(page).to have_css('.govuk-heading-l', text: data[:heading])
+    expect(page).to have_css('.govuk-heading-l,.govuk-heading-m', text: data[:heading])
   end
 end
 
@@ -44,4 +48,39 @@ describe 'handovers/overdue_tasks.html.erb' do
                   heading: 'Overdue tasks',
                   table_class: 'overdue-tasks-handovers',
                   has_columns: ["Prisoner details", "POM", "COM details", "COM responsible", "Earliest release date", "Tier", "Handover progress"]
+end
+
+describe 'poms/_handover_tab.html.erb' do
+  before do
+    assign(:pom_view, true)
+    assign(:upcoming_handovers, [])
+    assign(:in_progress_handovers, [])
+    assign(:overdue_tasks, [])
+    assign(:overdue_com_allocations, [])
+    assign(:summary, double.as_null_object)
+  end
+
+  it_behaves_like 'handover cases table',
+                  heading: 'Upcoming handovers',
+                  id: 'upcoming-handovers',
+                  table_class: 'upcoming-handovers',
+                  has_columns: ['Prisoner details', 'COM responsible', 'Earliest release date', 'Tier', 'Handover progress']
+
+  it_behaves_like 'handover cases table',
+                  heading: 'Handovers in progress',
+                  id: 'in-progress-handovers',
+                  table_class: 'in-progress-handovers',
+                  has_columns: ["Prisoner details", "COM details", "COM responsible", "Earliest release date", "Tier", "Handover progress"]
+
+  it_behaves_like 'handover cases table',
+                  heading: 'Overdue tasks',
+                  id: 'overdue-tasks',
+                  table_class: 'overdue-tasks',
+                  has_columns: ["Prisoner details", "COM details", "COM responsible", "Earliest release date", "Tier", "Handover progress"]
+
+  it_behaves_like 'handover cases table',
+                  heading: 'COM allocation overdue',
+                  id: 'overdue-com-allocations',
+                  table_class: 'overdue-com-allocation',
+                  has_columns: ["Prisoner details", "COM responsible", "Earliest release date", "Tier", "Days overdue", "LDU details"]
 end
