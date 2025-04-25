f29c32f328f14f796014c64742d7e07cc3513ff8
Jesus Laiz <zheileman@users.noreply.github.com>
2025-04-25 10:39:18 +0100

    Stop reporting to sentry appinsights exceptions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,8 +8,11 @@ if sentry_dsn
     config.release = ENV['BUILD_NUMBER']
     config.excluded_exceptions << 'JWT::ExpiredSignature'
 
-    config.before_send = lambda do |event, _hint|
-      SentryCircuitBreakerService.check_within_quota ? param_filter.filter(event.to_hash) : nil
+    config.before_send = lambda do |event, hint|
+      return nil if hint[:exception].full_message.match?(/ApplicationInsights::TelemetryClient/)
+      return nil unless SentryCircuitBreakerService.check_within_quota
+
+      param_filter.filter(event.to_hash)
     end
   end
 else
