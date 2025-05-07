bb621c5adba1ec370c2991b3ad34bd03bc97c9ef
Ryan Kendall <ryankendallt@gmail.com>
2025-05-07 14:02:38 +0100

    POM deallocation tool can deallocate case by case

--- spec/controllers/manage/deallocate_poms_controller_spec.rb
+++ spec/controllers/manage/deallocate_poms_controller_spec.rb
@@ -1,40 +1,50 @@
 require 'rails_helper'
 
 describe Manage::DeallocatePomsController do
+  before do
+    create(:allocation_history, id: 1, prison: 'LEI', nomis_offender_id: 'GA1234', primary_pom_nomis_id: 456_456, secondary_pom_nomis_id: 999_999)
+    create(:allocation_history, id: 2, prison: 'LEI', nomis_offender_id: 'GA2345', primary_pom_nomis_id: 456_456)
+    create(:allocation_history, id: 3, prison: 'LEI', nomis_offender_id: 'GA3456', primary_pom_nomis_id: 456_456)
+    create(:allocation_history, id: 4, prison: 'WHI', nomis_offender_id: 'GA4567', primary_pom_nomis_id: 456_456)
+    create(:allocation_history, id: 5, prison: 'LEI', nomis_offender_id: 'GA5678', primary_pom_nomis_id: 999_999)
+
+    stub_sso_data('LEI', roles: [SsoIdentity::SPO_ROLE, SsoIdentity::ADMIN_ROLE])
+  end
+
+  describe 'GET search' do
+    specify 'can display a specific POMs allocated cases' do
+      get :search, params: { staff_id: 999_999 }
+      expect(assigns(:allocations).count).to eq(2)
+      expect(assigns(:allocations).map(&:id)).to eq([1, 5])
+    end
+
+    specify 'can display a specific allocated case' do
+      get :search, params: { case_id: 'GA2345' }
+      expect(assigns(:allocations).count).to eq(1)
+      expect(assigns(:allocations).map(&:id)).to eq([2])
+    end
+  end
+
+  describe 'PATCH confirm' do
+    specify 'displays the selected allocations back to the user' do
+      patch :confirm, params: { allocation_ids: ['1', '2', '4', '5'] }
+      expect(assigns(:allocations).count).to eq(4)
+      expect(assigns(:allocations).map(&:id)).to eq([1, 2, 4, 5])
+    end
+  end
+
   describe 'PATCH update' do
-    before do
-      create(:allocation_history, prison: 'LEI', nomis_offender_id: 'GA1234', primary_pom_nomis_id: 456_456, secondary_pom_nomis_id: 999_999)
-      create(:allocation_history, prison: 'LEI', nomis_offender_id: 'GA2345', primary_pom_nomis_id: 456_456)
-      create(:allocation_history, prison: 'LEI', nomis_offender_id: 'GA3456', primary_pom_nomis_id: 456_456)
-      create(:allocation_history, prison: 'WHI', nomis_offender_id: 'GA4567', primary_pom_nomis_id: 456_456)
-      create(:allocation_history, prison: 'LEI', nomis_offender_id: 'GA5678', primary_pom_nomis_id: 999_999)
-
-      stub_sso_data('LEI', roles: [SsoIdentity::SPO_ROLE, SsoIdentity::ADMIN_ROLE])
-    end
-
-    specify 'deallocates all cases for specified POM' do
-      patch :update, params: { staff_id: 456_456 }
-      expect(AllocationHistory.for_pom(456_456).count).to eq(0)
-    end
-
-    specify 'deallocates from both primary and secondary roles as allocated POM' do
-      patch :update, params: { staff_id: 999_999 }
-      expect(AllocationHistory.for_pom(999_999).count).to eq(0)
-      expect(AllocationHistory.where(secondary_pom_nomis_id: 999_999).count).to eq(0)
-      expect(AllocationHistory.where(primary_pom_nomis_id: 999_999).count).to eq(0)
-    end
-
-    specify 'deallocates all cases for specified POM in a given prison only when prison specified' do
-      patch :update, params: { staff_id: 456_456, prison: 'WHI' }
-      pom_allocations = AllocationHistory.for_pom(456_456)
-      expect(pom_allocations.count).to eq(3)
-      expect(pom_allocations.at_prison('LEI').count).to eq(3)
-      expect(pom_allocations.at_prison('WHI').count).to eq(0)
-    end
-
-    specify 'allocations for other POMs remain untouched' do
-      patch :update, params: { staff_id: 456_456 }
-      expect(AllocationHistory.for_pom(999_999).count).to eq(1)
+    specify 'deallocates primary and secondary POM from the given allocations' do
+      patch :update, params: { allocation_ids: ['1', '2'] }
+      alloc_1 = AllocationHistory.find(1)
+      expect(alloc_1.primary_pom_nomis_id).to be_nil
+      expect(alloc_1.secondary_pom_nomis_id).to be_nil
+      alloc_2 = AllocationHistory.find(2)
+      expect(alloc_2.primary_pom_nomis_id).to be_nil
+      expect(alloc_2.secondary_pom_nomis_id).to be_nil
+      expect(AllocationHistory.find(3).primary_pom_nomis_id).to eq(456_456)
+      expect(AllocationHistory.find(4).primary_pom_nomis_id).to eq(456_456)
+      expect(AllocationHistory.find(5).primary_pom_nomis_id).to eq(999_999)
     end
   end
 end
