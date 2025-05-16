953d79fa762721f64d63ac8fd9af5704e00e38a2
Ryan Kendall <ryankendallt@gmail.com>
2025-05-16 11:30:21 +0100

    console debugger deprecated in favour of debugging page

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -2,8 +2,6 @@ require 'console_display_helpers'
 
 Rails.application.configure do
   console do
-    Rails::ConsoleMethods.include(ConsoleDisplayHelpers)
     Rails::ConsoleMethods.include(ConsoleAuditHelpers)
-    Rails::ConsoleMethods.include(ConsoleDebugOffenderHelpers)
   end
 end
