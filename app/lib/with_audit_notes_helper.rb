219bd8bd77311d157263ae656ae9d7eee0dd3d24
Ryan Kendall <ryankendallt@gmail.com>
2025-05-16 11:57:07 +0100

    fix cops

--- app/lib/with_audit_notes_helper.rb
+++ app/lib/with_audit_notes_helper.rb
@@ -1,8 +1,8 @@
-require "irb/helper_method"
+require 'irb/helper_method'
 
 class WithAuditNotesHelper < IRB::HelperMethod::Base
   description 'Ensure that paper_trail fields are correctly filled when performing manual changes on the console'
-  
+
   def execute(user_first_name:, user_last_name:, note:)
     whodunnit = "#{user_first_name} #{user_last_name}"
     controller_info = { user_first_name:, user_last_name:, system_admin_note: note }
