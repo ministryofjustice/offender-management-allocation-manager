76c91c352dacfca942d6cf4ccf51e33625339e97
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2025-12-18 15:27:42 +0000

    Reworked high risk cases report to improve performance (#2712)

--- app/models/mpc_offender.rb
+++ app/models/mpc_offender.rb
@@ -314,8 +314,6 @@ class MpcOffender
     }
   end
 
-  def high_rosh_level? = rosh_summary && rosh_summary[:overall] == 'HIGH'
-
   def active_alert_labels
     HmppsApi::PrisonAlertsApi.alerts_for(offender_no)
       .select { |alert| alert['isActive'] }
