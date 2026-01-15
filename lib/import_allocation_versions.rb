112336ec62985f9341d7f916583541931a2080af
Jesus Laiz <zheileman@users.noreply.github.com>
2026-01-15 12:15:00 +0000

    MO-1857 Further optimisations and prepare prod job

--- lib/import_allocation_versions.rb
+++ lib/import_allocation_versions.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 
 class ImportAllocationVersions
-  FROM_DATE = Time.zone.local(2019, 1, 1)
-  BATCH_SIZE = 5_000
+  attr_reader :offset_id, :batch_size
 
-  attr_reader :offset_id
-
-  def initialize(offset_id: nil)
+  def initialize(offset_id: nil, batch_size: nil)
     @offset_id = (offset_id || ENV.fetch('OFFSET_ID', 1)).to_i
+    @batch_size = (batch_size || ENV.fetch('BATCH_SIZE', 2_000)).to_i
   end
 
   def call
-    log("Starting with offset ID #{offset_id}...")
-
     papertrail_versions = PaperTrail::Version.where(item_type: 'AllocationHistory', event: 'update')
     create_counter = 0
     batch_counter = 0
 
+    log("Current PaperTrail versions: #{papertrail_versions.count}. Last ID: #{papertrail_versions.last.id}")
+    log("Starting with offset ID #{offset_id} and batch size #{batch_size}...")
+
     papertrail_versions.select(:id, :item_id, :whodunnit, :created_at, :object)
-                       .in_batches(
-                         of: BATCH_SIZE, start: [FROM_DATE, offset_id],
-                         cursor: [:created_at, :id], order: [:asc, :asc]
-                       ) do |relation|
+                       .in_batches(of: batch_size, start: offset_id, order: :asc) do |relation|
       rows = relation.map do |version|
         AllocationHistoryVersion.attrs_from_papertrail(version)
       end
 
-      ids = AllocationHistoryVersion.insert_all(rows)
-      create_counter += ids.count
+      AllocationHistoryVersion.insert_all(rows)
+      create_counter += relation.size
       batch_counter += 1
 
       log("Processed batch #{batch_counter} (#{create_counter} total records)") if batch_counter % 10 == 0
@@ -39,11 +35,8 @@ class ImportAllocationVersions
 
 private
 
+  # Using puts, as logger does not seem to output anything until the job exits
   def log(msg)
-    logger.info("[#{self.class}] #{msg}")
-  end
-
-  def logger
-    @logger ||= Logger.new($stdout)
+    puts "#{Time.current.strftime('%Y-%m-%d %H:%M:%S.%3N')} [#{self.class}] #{msg}"
   end
 end
