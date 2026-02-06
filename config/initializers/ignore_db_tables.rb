e5f3abe1483596987cfcd297681fc7cdf652ef97
Jesus Laiz <zheileman@users.noreply.github.com>
2026-02-06 12:16:26 +0000

    MO-1889 Tidy up schema spy
    Work related to data domains.
    Clean up no longer used flyway table and hint schema spy relationships in recently created tables.

--- config/initializers/ignore_db_tables.rb
+++
@@ -1,7 +0,0 @@
-# Needed because we are pointing new kotlin service to
-# the same database as the rails app, and we don't want
-# the flyway stuff showing up in the structure.sql file
-#
-ActiveRecord::SchemaDumper.ignore_tables += [
-  'flyway_schema_history',
-]
