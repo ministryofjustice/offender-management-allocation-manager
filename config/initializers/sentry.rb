f0640d0ab337115ac0a0daf1165492f8c28cce59
Jesus Laiz <zheileman@users.noreply.github.com>
2026-02-16 11:01:00 +0000

    Adapt sentry config to new 6.x version
    Main breaking change was the `before_send` hook no longer support returning a hash, it has to return the event itself, once filtered.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,12 +8,29 @@ if sentry_dsn
     config.dsn = sentry_dsn
     config.release = ENV['BUILD_NUMBER']
     config.excluded_exceptions << 'JWT::ExpiredSignature'
+    config.enable_metrics = false
 
     config.before_send = lambda do |event, hint|
-      return nil if hint[:exception].full_message.match?(/ApplicationInsights::TelemetryClient/)
+      return nil if hint[:exception]&.full_message&.match?(/ApplicationInsights::TelemetryClient/)
       return nil unless SentryCircuitBreakerService.check_within_quota
 
-      param_filter.filter(event.to_hash)
+      # Sanitize extra data
+      if event.extra
+        event.extra = param_filter.filter(event.extra)
+      end
+
+      # Sanitize user data
+      if event.user
+        event.user = param_filter.filter(event.user)
+      end
+
+      # Sanitize context data
+      if event.contexts
+        event.contexts = param_filter.filter(event.contexts)
+      end
+
+      # Return the sanitized event object
+      event
     end
   end
 else
