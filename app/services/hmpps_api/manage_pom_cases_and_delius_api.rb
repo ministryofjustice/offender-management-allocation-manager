1411b42790c50d2ce970e71dd23a67fa47c7edb7
Jesus Laiz <zheileman@users.noreply.github.com>
2026-02-17 09:24:19 +0000

    Do not cache these requests
    We always need most up to date details in these calls.

--- app/services/hmpps_api/manage_pom_cases_and_delius_api.rb
+++ app/services/hmpps_api/manage_pom_cases_and_delius_api.rb
@@ -8,13 +8,13 @@ module HmppsApi
     # https://manage-pom-cases-and-delius-preprod.hmpps.service.justice.gov.uk/swagger-ui/index.html#/probation-record-resource/handle
     def self.get_probation_record(offender_no_or_crn)
       route = "/case-records/#{offender_no_or_crn}"
-      client.get(route)
+      client.get(route, cache: false)
     end
 
     # https://manage-pom-cases-and-delius-preprod.hmpps.service.justice.gov.uk/swagger-ui/index.html#/mappa-resource/getMappaDetails
     def self.get_mappa_details(crn)
       route = "/case-records/#{crn}/risks/mappa"
-      client.get(route)
+      client.get(route, cache: false)
     end
   end
 end
