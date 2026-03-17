fcffe991441c45844f95e68e2a3c0a24921c4145
Jesus Laiz <zheileman@users.noreply.github.com>
2026-03-17 10:46:41 +0000

    wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,5 +70,3 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
-
-Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
