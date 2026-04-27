196067472c9358aef9ad5e6bbf0dea6bb0126cca
Jesus L <zheileman@users.noreply.github.com>
2026-04-27 12:23:08 +0100

    Improve missing complexity level action buttons (#2806)
    Some improvements to the buttons:
    
    - when case info is incomplete: single primary button "Save and continue".
    
    - when case info is complete: two buttons. Primary "Save" (returns to missing information list) and secondary "Save and allocate" (goes to review/allocation page).

--- spec/controllers/female_missing_infos_controller_spec.rb
+++ spec/controllers/female_missing_infos_controller_spec.rb
@@ -41,6 +41,7 @@ RSpec.describe FemaleMissingInfosController, type: :controller do
           prison_id: prison.code,
           prisoner_id: offender_no,
           complexity_form: { complexity_level: 'low' },
+          save_and_continue: 'Save and continue',
           sort: 'last_name',
           page: '2'
         }
@@ -65,21 +66,46 @@ RSpec.describe FemaleMissingInfosController, type: :controller do
         create(:case_information, offender: build(:offender, nomis_offender_id: offender_no))
       end
 
-      it 'redirects to review case details after saving complexity' do
-        put :update, params: {
-          prison_id: prison.code,
-          prisoner_id: offender_no,
-          complexity_form: { complexity_level: 'medium' }
-        }
+      context 'when save and allocate is clicked' do
+        it 'redirects to review case details after saving complexity' do
+          put :update, params: {
+            prison_id: prison.code,
+            prisoner_id: offender_no,
+            complexity_form: { complexity_level: 'medium' },
+            save_and_allocate: 'Save and allocate'
+          }
 
-        aggregate_failures do
-          expect(HmppsApi::ComplexityApi).to have_received(:save).with(
-            offender_no,
-            level: 'medium',
-            username: 'user',
-            reason: nil
-          )
-          expect(response).to redirect_to(prison_prisoner_review_case_details_path(prison_id: prison.code, prisoner_id: offender_no))
+          aggregate_failures do
+            expect(HmppsApi::ComplexityApi).to have_received(:save).with(
+              offender_no,
+              level: 'medium',
+              username: 'user',
+              reason: nil
+            )
+            expect(response).to redirect_to(prison_prisoner_review_case_details_path(prison_id: prison.code, prisoner_id: offender_no))
+          end
+        end
+      end
+
+      context 'when save is clicked' do
+        it 'saves complexity and redirects to missing information list' do
+          put :update, params: {
+            prison_id: prison.code,
+            prisoner_id: offender_no,
+            complexity_form: { complexity_level: 'medium' },
+            sort: 'last_name',
+            page: '2'
+          }
+
+          aggregate_failures do
+            expect(HmppsApi::ComplexityApi).to have_received(:save).with(
+              offender_no,
+              level: 'medium',
+              username: 'user',
+              reason: nil
+            )
+            expect(response).to redirect_to(missing_information_prison_prisoners_path(prison.code, sort: 'last_name', page: '2'))
+          end
         end
       end
     end
@@ -94,33 +120,13 @@ RSpec.describe FemaleMissingInfosController, type: :controller do
                enhanced_resourcing: false)
       end
 
-      it 'redirects to case information after saving complexity' do
-        put :update, params: {
-          prison_id: prison.code,
-          prisoner_id: offender_no,
-          complexity_form: { complexity_level: 'medium' }
-        }
-
-        aggregate_failures do
-          expect(HmppsApi::ComplexityApi).to have_received(:save).with(
-            offender_no,
-            level: 'medium',
-            username: 'user',
-            reason: nil
-          )
-          expect(session["female_missing_info_complexity_saved_#{offender_no}"]).to eq(true)
-          expect(response).to redirect_to(new_prison_prisoner_case_information_path(prison.code, prisoner_id: offender_no, sort: nil, page: nil))
-        end
-      end
-
-      context 'when the rosh feature flag is disabled' do
-        let(:rosh_level_feature_enabled) { false }
-
-        it 'redirects to review case details after saving complexity' do
+      context 'when save and continue is clicked' do
+        it 'redirects to case information after saving complexity' do
           put :update, params: {
             prison_id: prison.code,
             prisoner_id: offender_no,
-            complexity_form: { complexity_level: 'medium' }
+            complexity_form: { complexity_level: 'medium' },
+            save_and_continue: 'Save and continue'
           }
 
           aggregate_failures do
@@ -130,7 +136,53 @@ RSpec.describe FemaleMissingInfosController, type: :controller do
               username: 'user',
               reason: nil
             )
-            expect(response).to redirect_to(prison_prisoner_review_case_details_path(prison_id: prison.code, prisoner_id: offender_no))
+            expect(session["female_missing_info_complexity_saved_#{offender_no}"]).to eq(true)
+            expect(response).to redirect_to(new_prison_prisoner_case_information_path(prison.code, prisoner_id: offender_no, sort: nil, page: nil))
+          end
+        end
+      end
+
+      context 'when the rosh feature flag is disabled' do
+        let(:rosh_level_feature_enabled) { false }
+
+        context 'when save and allocate is clicked' do
+          it 'redirects to review case details after saving complexity' do
+            put :update, params: {
+              prison_id: prison.code,
+              prisoner_id: offender_no,
+              complexity_form: { complexity_level: 'medium' },
+              save_and_allocate: 'Save and allocate'
+            }
+
+            aggregate_failures do
+              expect(HmppsApi::ComplexityApi).to have_received(:save).with(
+                offender_no,
+                level: 'medium',
+                username: 'user',
+                reason: nil
+              )
+              expect(response).to redirect_to(prison_prisoner_review_case_details_path(prison_id: prison.code, prisoner_id: offender_no))
+            end
+          end
+        end
+
+        context 'when save is clicked' do
+          it 'saves complexity and redirects to missing information list' do
+            put :update, params: {
+              prison_id: prison.code,
+              prisoner_id: offender_no,
+              complexity_form: { complexity_level: 'medium' }
+            }
+
+            aggregate_failures do
+              expect(HmppsApi::ComplexityApi).to have_received(:save).with(
+                offender_no,
+                level: 'medium',
+                username: 'user',
+                reason: nil
+              )
+              expect(response).to redirect_to(missing_information_prison_prisoners_path(prison.code, sort: nil, page: nil))
+            end
           end
         end
       end
