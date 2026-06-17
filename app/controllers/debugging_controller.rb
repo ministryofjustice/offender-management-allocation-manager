12e1a1c327ff71672fa5040d64f1ffe553f871cf
Jesus Laiz <zheileman@users.noreply.github.com>
2026-06-17 15:15:56 +0100

    MO-1984 Fix outside OMiC debugging page
    Ticket: https://dsdmoj.atlassian.net/browse/MO-1984
    
    The debugging page would not load due to an exception raised for offenders previously "known" to the service but then deemed outside OMiC policy.
    
    Also, added early allocations debugging data and some other minor improvements.

--- app/controllers/debugging_controller.rb
+++ app/controllers/debugging_controller.rb
@@ -2,22 +2,24 @@
 
 class DebuggingController < PrisonsApplicationController
   before_action :ensure_admin_user
+  before_action :set_nomis_id
+  before_action :ensure_known_offender, if: -> { @nomis_id.present? }
 
   def debugging
-    return unless id
+    return unless @nomis_id
+    return unless @known_offender
 
-    @nomis_id = id
     @offender = OffenderService.get_offender(@nomis_id, ignore_legal_status: true)
 
     if @offender
-      @live_handover = OffenderHandover.new(@offender).as_calculated_handover_date
       @sentences = Sentences.for(booking_id: @offender.booking_id)
     end
 
     @case_information = CaseInformation.find_by(nomis_offender_id: @nomis_id)
     @persisted_handover = CalculatedHandoverDate.find_by(nomis_offender_id: @nomis_id)
     @overidden_responsibility = Responsibility.find_by(nomis_offender_id: @nomis_id)
-    @parole_reviews = ParoleReview.where(nomis_offender_id: @nomis_id).order('updated_at DESC')
+    @parole_reviews = ParoleReview.where(nomis_offender_id: @nomis_id).order(updated_at: :desc)
+    @early_allocations = EarlyAllocation.where(nomis_offender_id: @nomis_id).order(created_at: :desc)
     @oasys_assessment = HmppsApi::AssessRisksAndNeedsApi.get_latest_oasys_date(@nomis_id)
     @allocation = AllocationHistory.find_by(nomis_offender_id: @nomis_id)
     @movements = HmppsApi::PrisonApi::MovementApi.movements_for(
@@ -30,13 +32,19 @@ class DebuggingController < PrisonsApplicationController
   end
 
   def timeline
-    return unless id
+    return unless @nomis_id
+    return unless @known_offender
 
-    @nomis_id = id
     @log = AuditEvent.where(nomis_offender_id: @nomis_id).order(created_at: :desc)
   end
 
 private
 
-  def id = params[:offender_no]&.strip
+  def set_nomis_id
+    @nomis_id = params[:offender_no]&.strip
+  end
+
+  def ensure_known_offender
+    @known_offender = Offender.exists?(nomis_offender_id: @nomis_id)
+  end
 end
