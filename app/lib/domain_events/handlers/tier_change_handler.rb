7db9ec4f0c29f51ddf17263fd8aecc9f53b27c4f
Jesus Laiz <zheileman@users.noreply.github.com>
2026-06-29 12:21:53 +0100

    MO-1989 Handle `tier.calculation.changed` event
    Ticket: https://dsdmoj.atlassian.net/browse/MO-1989
    
    We are switching to this event that is more appropriate for us as we only care about changes in tier, whereas previous event is too noisy as it is triggered even if the end result is the same (no tier level change).
    
    Improved some logging.

--- app/lib/domain_events/handlers/tier_change_handler.rb
+++ app/lib/domain_events/handlers/tier_change_handler.rb
@@ -19,6 +19,9 @@ module DomainEvents
         return if new_tier == old_tier
 
         case_info.tier = new_tier
+        case_info.manual_entry = false
+
+        attrs_before = case_info.changed_attributes
 
         if case_info.save
           AuditEvent.publish(
@@ -26,8 +29,8 @@ module DomainEvents
             tags: %w[handler case_information tier changed],
             system_event: true,
             data: {
-              'before' => old_tier,
-              'after' => new_tier
+              'before' => attrs_before,
+              'after' => case_info.slice(attrs_before.keys)
             }
           )
 
@@ -35,7 +38,7 @@ module DomainEvents
                         "version=#{event.version},crn=#{event.crn_number},old_tier=#{old_tier},new_tier=#{new_tier}"
         else
           logger.error "event=domain_event_handle_failure,domain_event_type=#{event.event_type}," \
-            "version=#{event.version},crn=#{event.crn_number}|#{case_info.errors.full_messages.join(',')}"
+            "version=#{event.version},crn=#{event.crn_number},old_tier=#{old_tier},new_tier=#{new_tier}|#{case_info.errors.full_messages.join(',')}"
         end
       end
     end
