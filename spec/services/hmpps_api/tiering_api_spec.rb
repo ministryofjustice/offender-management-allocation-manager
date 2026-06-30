c1b474ce5a2e06f7bc6be1d6bac1adf683535ef1
Jesus L <zheileman@users.noreply.github.com>
2026-06-30 12:29:54 +0100

    Disable cache in tier calculation API request (#2854)

--- spec/services/hmpps_api/tiering_api_spec.rb
+++ spec/services/hmpps_api/tiering_api_spec.rb
@@ -4,73 +4,70 @@ require "rails_helper"
 
 describe HmppsApi::TieringApi do
   describe '.get_calculation' do
-    let(:api_host) { Rails.configuration.tiering_api_host }
     let(:crn) { 'X408769' }
     let(:calculation_id) { 'a5e7d3c1-9b4f-4e2a-8c6d-1f3b5a7e9d02' }
-    let(:response_body) do
-      { 'tierScore' => 'E', 'calculationDate' => '2024-03-15' }.to_json
+    let(:version) { 3 }
+    let(:route) { "/v#{version}/crn/#{crn}/tier/#{calculation_id}" }
+    let(:client) { instance_double(HmppsApi::Client) }
+
+    before do
+      allow(HmppsApi::Client).to receive(:new).and_return(client)
     end
 
     context 'with version 3' do
-      let(:endpoint) { "#{api_host}/v3/crn/#{crn}/tier/#{calculation_id}" }
-
       before do
-        stub_request(:get, endpoint).to_return(status: 200, body: response_body)
+        allow(client).to receive(:get).with(route, cache: false)
+          .and_return({ 'tierScore' => 'E', 'calculationDate' => '2024-03-15' })
       end
 
-      it 'calls the v3 endpoint' do
-        result = described_class.get_calculation(crn, calculation_id, version: 3)
+      it 'returns the parsed tier and date' do
+        result = described_class.get_calculation(crn, calculation_id, version:)
         expect(result).to eq(tier: 'E', calculation_date: Date.parse('2024-03-15'))
-        expect(WebMock).to have_requested(:get, endpoint)
+        expect(client).to have_received(:get).with(route, cache: false)
       end
     end
 
     context 'with version 2' do
-      let(:endpoint) { "#{api_host}/v2/crn/#{crn}/tier/#{calculation_id}" }
-      let(:response_body) do
-        { 'tierScore' => 'D1', 'calculationDate' => '2024-03-15' }.to_json
-      end
+      let(:version) { 2 }
 
       before do
-        stub_request(:get, endpoint).to_return(status: 200, body: response_body)
+        allow(client).to receive(:get).with(route, cache: false)
+          .and_return({ 'tierScore' => 'D1', 'calculationDate' => '2024-03-15' })
       end
 
-      it 'calls the v2 endpoint' do
-        result = described_class.get_calculation(crn, calculation_id, version: 2)
+      it 'returns the parsed tier and date' do
+        result = described_class.get_calculation(crn, calculation_id, version:)
         expect(result).to eq(tier: 'D1', calculation_date: Date.parse('2024-03-15'))
-        expect(WebMock).to have_requested(:get, endpoint)
+        expect(client).to have_received(:get).with(route, cache: false)
       end
     end
 
     context 'when the endpoint returns 404' do
-      let(:endpoint) { "#{api_host}/v3/crn/#{crn}/tier/#{calculation_id}" }
-
       before do
-        stub_request(:get, endpoint).to_return(status: 404, body: '')
+        allow(client).to receive(:get).with(route, cache: false)
+          .and_raise(Faraday::ResourceNotFound.new(nil))
       end
 
       it 'returns nil' do
-        result = described_class.get_calculation(crn, calculation_id, version: 3)
-        expect(result).to be_nil
+        expect(described_class.get_calculation(crn, calculation_id, version:)).to be_nil
       end
     end
 
     context 'when the endpoint returns a server error' do
-      let(:endpoint) { "#{api_host}/v3/crn/#{crn}/tier/#{calculation_id}" }
-
       before do
-        stub_request(:get, endpoint).to_return(status: 500, body: 'Internal Server Error')
+        allow(client).to receive(:get).with(route, cache: false)
+          .and_raise(Faraday::ServerError.new(nil))
       end
 
       it 'returns an error hash' do
-        result = described_class.get_calculation(crn, calculation_id, version: 3)
+        result = described_class.get_calculation(crn, calculation_id, version:)
         expect(result).to eq(tier: nil, calculation_date: nil, error: Faraday::ServerError)
       end
 
       it 'logs the error' do
         allow(Rails.logger).to receive(:error)
-        described_class.get_calculation(crn, calculation_id, version: 3)
-        expect(Rails.logger).to have_received(:error).with(%r{event=tiering_get_calculation,route=/v3/crn/#{crn}/tier/#{calculation_id}})
+        described_class.get_calculation(crn, calculation_id, version:)
+        expect(Rails.logger).to have_received(:error).with(/event=tiering_get_calculation,route=#{route}/)
       end
     end
   end
