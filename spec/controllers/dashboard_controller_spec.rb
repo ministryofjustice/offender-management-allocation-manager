ed48535e53bef3a1d67786d119ebdd4bba48a4f1
Jesus Laiz <zheileman@users.noreply.github.com>
2026-08-04 16:01:35 +0100

    MO-2030 Add bulk realloc news banner and badge
    Ticket: https://dsdmoj.atlassian.net/browse/MO-2030
    
    Add bulk reallocation announcement banner and 'NEW' badge.
    
    It will only show to SPOs.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -96,6 +96,34 @@ RSpec.describe DashboardController, type: :controller do
         expect(response.body).to have_text('View all handover cases')
       end
 
+      describe 'bulk reallocation announcement' do
+        context 'when enabled' do
+          before do
+            stub_feature_flag(:bulk_reallocation, enabled: true)
+          end
+
+          it 'shows the homepage announcement and NEW badge on View all POMs' do
+            get :index, params: { prison_id: prison }
+
+            expect(response.body).to include('moj-alert--information')
+            expect(response.body).to include('moj-badge moj-badge--red')
+          end
+        end
+
+        context 'when disabled' do
+          before do
+            stub_feature_flag(:bulk_reallocation, enabled: false)
+          end
+
+          it 'shows the homepage announcement but hides the NEW badge' do
+            get :index, params: { prison_id: prison }
+
+            expect(response.body).to include('moj-alert--information')
+            expect(response.body).not_to include('moj-badge moj-badge--red')
+          end
+        end
+      end
+
       describe 'Make allocations tab' do
         context 'when the count is more than 0' do
           # create offenders with case_information so that they display as un-allocated
