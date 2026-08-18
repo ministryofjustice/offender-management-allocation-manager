53664ee5eb6487401eea6c06b2e3eafcb7e92713
Jesus Laiz <zheileman@users.noreply.github.com>
2026-08-18 15:50:04 +0100

    Update spec

--- spec/services/subject_access_request_template_service_spec.rb
+++ spec/services/subject_access_request_template_service_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe SubjectAccessRequestTemplateService do
 
   describe 'DB schema change guard' do
     # Keep this updated once any potential SAR drifting is asserted
-    let(:reviewed_version) { '20260813_173000' }
+    let(:reviewed_version) { '20260818_120000' }
 
     let(:current_version) { ActiveRecord::Base.connection_pool.migration_context.current_version }
     let(:message) do
